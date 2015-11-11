5071a016aa005565e671398856482f9a4a65083f
Edward Faulkner <ef@alum.mit.edu>
2015-11-11 14:43:34 -0500

    add html parser service and use it for dynamic titles

--- app/models/django-page.js
+++ app/models/django-page.js
@@ -1,5 +1,15 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  text: DS.attr()
+  htmlParser: Ember.inject.service(),
+  text: DS.attr(),
+
+  document: Ember.computed('text', function() {
+    return this.get('htmlParser').parse(this.get('text'));
+  }),
+
+  title: Ember.computed('document', function() {
+    return this.get('document').querySelector('title').innerHTML;
+  })
 });
