c52c86a3f564c636b954f28c666004c8f299286c
Edward Faulkner <ef@alum.mit.edu>
2015-11-12 01:09:04 -0500

    clone before append
    So that the document is reusable.

--- app/models/django-page.js
+++ app/models/django-page.js
@@ -19,20 +19,21 @@ export default DS.Model.extend({
   appendStyles($element) {
     let doc = this.get('document');
     let internalStyles = doc.querySelectorAll('head style');
-    let externalStyles = Array.from(doc.querySelectorAll('head link[rel=stylesheet]'));
-
-    externalStyles.forEach(s => {
+    let externalStyles = Array.from(doc.querySelectorAll('head link[rel=stylesheet]')).map(s => {
       let style = $(s);
       let href = style.attr('href');
       if (href) {
         href = href.replace(/^\/\//, location.protocol + '//');
         if (href.indexOf(config.wnycMediaURL) === 0) {
+          style = style.clone();
           style.attr('href', href.replace(config.wnycMediaURL, '/wnyc-media'));
         }
-        if (href.indexOf('http://cloud.typography.com') === 0) {
+        else if (href.indexOf('http://cloud.typography.com') === 0) {
+          style = style.clone();
           style.attr('href', href.replace('http://cloud.typography.com', '/cloud-typography'));
         }
       }
+      return style;
     });
 
     let stylesLoaded = externalStyles.map(s => loaded(s));
@@ -43,7 +44,7 @@ export default DS.Model.extend({
   appendTo($element) {
     let doc = this.get('document');
     return this.appendStyles($element).finally(() => {
-      $element.append(doc.querySelector('body').children);
+      $element.append($(doc).find('body').children().clone());
     });
   }
 });
