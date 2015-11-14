fc3e535a255c4c8c3ac2ecfb685cd3f07c1fe185
Edward Faulkner <ef@alum.mit.edu>
2015-11-14 15:30:34 -0500

    Using patched ember-cli
    See https://github.com/ember-cli/ember-cli/pull/5104. Without this we couldn't produce builds that correctly contain the babel runtime polyfill, which I'm relying on to support some old browsers.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    babel: { browserPolyfill: true }
+    babel: { includePolyfill: true }
   });
 
   // Use `app.import` to add additional libraries to the generated
