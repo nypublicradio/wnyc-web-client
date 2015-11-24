f48d994eefb7fbbae04c366c381d2b7a71c7ff43
Edward Faulkner <ef@alum.mit.edu>
2015-11-24 13:00:43 -0500

    cleanup & update to modern ember helper format

--- app/helpers/image-template.js
+++ app/helpers/image-template.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
 
-export default Ember.Handlebars.makeBoundHelper(function(template, x, y, crop) {
-    function replaceFn(originalString, base, X, Y, CROP, Q, path) {
-        // quality is not passed in from the templates, just use 80
-        X = x; Y = y; CROP = crop; Q = 99;
+export default Ember.Helper.helper(function([template, x, y, crop]) {
+  function replaceFn(originalString, base, X, Y, CROP, Q, path) {
+    // quality is not passed in from the templates, just use 80
+    X = x; Y = y; CROP = crop; Q = 99;
 
-        return [base, X, Y, CROP, Q, path].join('/')
-    }
+    return [base, X, Y, CROP, Q, path].join('/');
+  }
 
-    return template.replace(/(.*\/i)\/(%s)\/(%s)\/(%s)\/(%s)\/(.*)/, replaceFn)
-})
-
+  return template.replace(/(.*\/i)\/(%s)\/(%s)\/(%s)\/(%s)\/(.*)/, replaceFn);
+});
