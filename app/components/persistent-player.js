65c87d44ab88a540bfbd18e4f80098f2ae85f43e
Brian Whitton <me@brianwhitton.com>
2015-12-08 19:36:59 -0500

    updates persistent player to use audio service properties

--- app/components/persistent-player.js
+++ app/components/persistent-player.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import service from 'ember-service/inject';
 
 export default Ember.Component.extend({
-  audio: Ember.inject.service(),
+  audio: service(),
   classNames: ['persistent-player']
 });
