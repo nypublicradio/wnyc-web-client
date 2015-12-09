36c72a2302098a67b50cb17e4b61545ab4086760
Brian Whitton <me@brianwhitton.com>
2015-12-08 19:31:29 -0500

    big update to okra bridge
    simplifies okra bridge into 3 separate exported bridges. one for audio
    models, one for the audio service, and another with api methods that
    might go away.
    
    the idea is that the rest of the ember app should have no idea that
    there's a backbone layer. any direct access to backbone happens here, as
    proxied through standard ember objects like models and services.

--- app/lib/okra-bridge.js
+++ app/lib/okra-bridge.js
@@ -1,6 +1,17 @@
+/*global Okra, XDPlayer*/
 import Ember from 'ember';
+const {
+  Mixin,
+  computed,
+  set,
+  get
+} = Ember;
 const { Promise } = Ember.RSVP;
+const { bind } = Ember.run;
 
+// XDPlayer and Okra are globals provided by the underlying backbone source,
+// but we need to wait until all the deps are loaded before booting it up.
+// see services/legacy-loader for dependency list.
 export function installBridge() {
   XDPlayer.on('initialize:after', function() {
     Okra.start();
@@ -8,32 +19,115 @@ export function installBridge() {
   XDPlayer.start();
 }
 
-export function playOnDemand(pk) {
-  Okra.execute('playOnDemand', pk);
-}
-
-export default Ember.Object.extend({
-  audioService: Ember.computed(function() {
-    return new Promise(resolve => {
-      let interval = setInterval(() => {
-        if (typeof Okra !== 'undefined') {
-          clearInterval(interval);
-          Okra.on('initialize:after', function() {
-            resolve(Okra.request('audioService'));
-          });
-        }
-      }, 20);
-    });
-  }),
-  playerModel: Ember.computed('audioService', {
-    get() {
-      return this.get('audioService').then(a => a.playerModel);
+// the audio service provides useful events for a soundObject's life cycle.
+// instead of trying to smooth out all the rough edges now, we can use the existing
+// architecture.
+// from puppy/media/js/lib/marionette/xd_player/web_player_controller.js:
+// A Web Player Controller emits the following events:
+//     player:launchingRemoteFile     // Playback of a remote file is about to be attempted.
+//     player:launchingStream         // Playback of a stream is about to be attempted.
+//     player:loading                 // The Player Model has started to load a resource.
+//     player:played                  // The Player Model has begun playback of a sound.
+//     player:paused                  // The Player Model has paused playback of a sound.
+//     player:progress                // A sound has progressed its playback position.
+//     player:buffered                // Some portion of a sound has been buffered.
+//     player:finished                // A sound has completed playback.
+//     player:soundDestroyed          // A sound has been released from memory by the Player Model.
+//     player:switched                // Playback has moved from one piece of audio to another.
+//     player:error                   // The Player Model has published an error message.
+//     player:error:<ERROR_CONSTANT>  // The Player Model has published an error with a specific constant name.
+//
+// The following events are emitted to match APIs with the mobile app's
+// native bridge:
+//     audioStatusChanged             // The playback state of The Player Model has changed in some way.
+//     audioProgress                  // The audio being played by The Player Model has progressed its playback position.
+//
+// Note that Okra defines this internally as the `audioService`, but to avoid
+// confusing naming overlaps we'll refer to it as the WEB_PLAYER_CONTROLLER or
+// the playerController in our Ember context.
+const WEB_PLAYER_CONTROLLER = new Promise(resolve => {
+  let interval = setInterval(() => {
+    if (typeof Okra !== 'undefined') {
+      clearInterval(interval);
+      Okra.on('initialize:after', function() {
+        resolve(Okra.request('audioService'));
+      });
     }
-  }),
-  init() {
-    let aS = this.get('audioService');
-    let pM = this.get('playerModel');
-    aS.then(c => c.on('player:progress', m => this.set('position', m.normalised)));
-    pM.then(p => p.on('change:isPlaying', (m,e) => this.set('isPlaying', e)));
-  },
+  }, 20);
 });
+
+// a lower-level interface to the soundManger soundObject instance.
+// from puppy/media/js/lib/backbone/models/sound_manager_player.js
+//
+// A Player Model that fronts a SoundManager backend to be used in//{//}
+// a Marionette app. While this looks very similar to models/player.js,
+// it is used in a totally different way, so certain affordances such as
+// the loadingUpdate handler is not implemented in the same way.
+//
+// Plays one audio source at a time.
+const PLAYER_MODEL = WEB_PLAYER_CONTROLLER.then(c => c.playerModel);
+
+// the ServiceBridge is mixed into the audio-service to notify when the backbone
+// dependencies are loaded.
+export const ServiceBridge = Mixin.create({
+  playerController: WEB_PLAYER_CONTROLLER.then(c => c),
+  playerModel: PLAYER_MODEL.then(m => m),
+  isReady: computed.and('playerController', 'playerModel')
+});
+
+// the ModelBridge is mixed into the ondemand and stream models to provide a
+// simplifed interface to the required listeners, methods, and properties.
+// any interaction or data around the audio object itself should proxy to here
+// from other parts of the app usually through the audio services `currentAudio`
+// property, but also potentially via the queue property.
+//
+// certain properties need to be explicitly listened to via backbone KVOs. some
+// are found on the playerModel directly and some are found on the playerController.
+// the `setup` methods can init any listenrs required to update the ember-data model.
+export const ModelBridge = Mixin.create({
+  playerController: WEB_PLAYER_CONTROLLER.then(a => a),
+  playerModel: PLAYER_MODEL.then(p => p),
+  init() {
+    this._super(...arguments);
+    get(this, 'playerController').then(bind(this, this.setupPlayerController));
+    get(this, 'playerModel').then(bind(this, this.setupPlayerModel));
+  },
+  setupPlayerController(playerController) {
+    playerController.on('player:progress', m => set(this, 'position', m.normalised));
+  },
+  setupPlayerModel(playerModel) {
+     playerModel.on('change:isPlaying', (x, val) => set(this, 'isPlaying', val));
+  },
+  addToPlaylist(pkOrUrl, title) {
+    if (/^\d+/.test(pkOrUrl)) {
+      Okra.execute('addToPlaylistFromPK', pkOrUrl);
+    } else {
+      Okra.execute('addToPlaylistFromFile', pkOrUrl, title);
+    }
+  },
+  playStream(slug) {
+    Okra.execute('playStream', slug);
+  },
+  play() {
+    PLAYER_MODEL.then(p => p.play());
+  },
+  pause() {
+    PLAYER_MODEL.then(p => p.pause());
+  }
+});
+
+// the ApiBridge is imported into the ondemand and streaming adaptesr to provide
+// direct access to some underlying Okra methods which just make fetching audio
+// a smoother experience for now.
+export const ApiBridge = {
+  playerController: WEB_PLAYER_CONTROLLER.then(c => c),
+  playerModel: PLAYER_MODEL.then(m => m),
+  playOnDemand(pkOrUrl, title) {
+    if (/^\d+/.test(pkOrUrl)) {
+      Okra.execute('playOnDemand', pkOrUrl);
+    } else {
+      Okra.execute('playOnDemandFile', pkOrUrl, title);
+    }
+  },
+
+};
