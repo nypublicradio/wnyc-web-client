832ece5806e2b4057537990218d997e08efea6f4
Brian Whitton <me@brianwhitton.com>
2015-12-09 18:50:06 -0500

    adds defaults for model values

--- app/ondemand/model.js
+++ app/ondemand/model.js
@@ -8,8 +8,8 @@ import { ModelBridge } from '../lib/okra-bridge';
 export default DS.Model.extend(ModelBridge, {
   story: belongsTo('story'),
 
-  position: attr('number'),
-  duration: attr('number'),
-  isPlaying: attr('boolean'),
-  volume: attr('number')
+  position: attr('number', {defaultValue: 0}),
+  duration: attr('number', {defaultValue: 0}),
+  isPlaying: attr('boolean', {defaultValue: false}),
+  volume: attr('number', {defaultValue: 100})
 });
