1f2fd3f628220be4aeaa7906166f92292bcb923c
Brian Whitton <me@brianwhitton.com>
2015-12-09 20:36:12 -0500

    now all pieces returned by the backbone api facade have their show filled in
    also those objects are not getting run through to_jsondata_handlebars,
    so we don't have header information. until everything conforms, we'll
    just do a janky extract in the serializer

--- app/ondemand/serializer.js
+++ app/ondemand/serializer.js
@@ -1,7 +1,25 @@
 import DS from 'ember-data';
 
+function extractHeaders({ show }) {
+  if (!Object.keys(show).length) {
+    return {};
+  }
+
+  let path = show.show_url.match(/(?:http:\/\/.*wnyc.(?:net|org):\d+\/)(.*)\/$/);
+  path = path[1] ? path[1] : path;
+  return {
+    brand: {
+      path,
+      title: show.show_title
+    }
+  };
+}
+
 export default DS.JSONAPISerializer.extend({
   normalizeResponse(store, primaryModelClass, [story, {bbModel, soundObject}], id) {
+
+    story.headers = extractHeaders(story);
+
     return {
       data: {
         id,
