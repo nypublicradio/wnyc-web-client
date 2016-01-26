917d233994e3f0317621e30fc2564e625eba6349
Brian Whitton <me@brianwhitton.com>
2016-01-26 15:36:07 -0500

    adds GA key to environment .env config

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    googleAnalyticsKey: process.env.GOOGLE_ANALYTICS,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
