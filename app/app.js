e205c80c90572c248cfc5bd5b4e4029a572756e3
Brian Whitton <me@brianwhitton.com>
2016-01-26 15:39:12 -0500

    adds custom resolver to help break up listing routes

--- app/app.js
+++ app/app.js
@@ -4,13 +4,25 @@ import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
 let App;
+let RE = /^(?:route|template):(shows|series|articles|tags)/;
+
+let NYPRResolver = Resolver.extend({
+  parseName(fullName) {
+    if (RE.test(fullName)) {
+      let match = RE.exec(fullName);
+      return this._super(fullName.replace(match[1], 'channel'));
+    }
+    return this._super(fullName);
+  }
+});
+
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver: NYPRResolver
 });
 
 loadInitializers(App, config.modulePrefix);
