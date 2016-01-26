de97e6aed7083e8a0bb4c5839d665beede61861f
Brian Whitton <me@brianwhitton.com>
2016-01-26 15:38:21 -0500

    adds listings deprecation workflow messages to unity

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -7,6 +7,7 @@ window.deprecationWorkflow.config = {
     { handler: "silence", matchMessage: "Your custom serializer uses the old version of the Serializer API, with `extract` hooks. Please upgrade your serializers to the new Serializer API using `normalizeResponse` hooks instead." },
     { handler: "silence", matchMessage: "The default behavior of shouldReloadAll will change in Ember Data 2.0 to always return false when there is at least one \"comment\" record in the store. If you would like to preserve the current behavior please override shouldReloadAll in your adapter:application and return true." },
     { handler: "silence", matchMessage: "`RESTSerializer.normalizeHash` has been deprecated. Please use `serializer.normalize` to modify the payload of single resources." },
-    { handler: "silence", matchMessage: "Ember.keys is deprecated in favor of Object.keys" }
+    { handler: "silence", matchMessage: "Ember.keys is deprecated in favor of Object.keys" },
+    { handler: "silence", matchMessage: "Calling Ember.get without a target object has been deprecated, please specify a target object."  },
   ]
 };
