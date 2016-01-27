a2719d3ed02621a120c94d9fd9d3253c0f13f193
Brian Whitton <me@brianwhitton.com>
2016-01-27 11:13:54 -0500

    updates image template with string template

--- app/helpers/image-template.js
+++ app/helpers/image-template.js
@@ -5,7 +5,7 @@ export default Ember.Helper.helper(function([template, x, y, crop]) {
     // quality is not passed in from the templates, just use 80
     X = x; Y = y; CROP = crop; Q = 99;
 
-    return [base, X, Y, CROP, Q, path].join('/');
+    return `${base}/${X}/${Y}/${CROP}/${Q}/${path}`;
   }
 
   return template.replace(/(.*\/i)\/(%s)\/(%s)\/(%s)\/(%s)\/(.*)/, replaceFn);
