faa5fa23acab2ff5018b970d5c8706be08d774c4
Brian Whitton <me@brianwhitton.com>
2016-01-27 11:15:33 -0500

    adds metrics adapers to environment config

--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,7 @@ module.exports = function(environment) {
     baseURL: '/',
     locationType: 'auto',
     googleAnalyticsKey: process.env.GOOGLE_ANALYTICS,
+    metricsAdapters: [],
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
