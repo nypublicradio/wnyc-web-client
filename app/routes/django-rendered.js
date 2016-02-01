9ec56897e1313480f08e6a1907d2b28574179146
Brian Whitton <me@brianwhitton.com>
2016-01-31 23:31:39 -0500

    adds query params if present to upstream url

--- app/routes/django-rendered.js
+++ app/routes/django-rendered.js
@@ -1,10 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model({ upstream_url }) {
+  model({ upstream_url }, transition) {
     // This adds trailing slashes, because the server's redirect
     // doesn't otherwise work correctly due to the proxying I'm using
     // in development (which is neeeded due to CORs).
-    return this.store.find('django-page', upstream_url.replace(/\/*$/, '/'));
+    upstream_url = upstream_url.replace(/\/*$/, '/');
+
+    let queryParams = Object.keys(transition.queryParams).map(p => `${p}=${transition.queryParams[p]}`);
+    if (queryParams.length) {
+      upstream_url += `?${queryParams.join('&')}`;
+    }
+    return this.store.find('django-page', upstream_url);
   }
 });
