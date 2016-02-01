6b0a635ebb71731e56609b864c4191d2d9f99843
Brian Whitton <me@brianwhitton.com>
2016-02-01 14:41:26 -0500

    force queries for related stories to use JSONP for now

--- app/story/adapter.js
+++ app/story/adapter.js
@@ -1,12 +1,17 @@
 import ENV from '../config/environment';
 import DS from 'ember-data';
-import fetch from 'fetch';
+import Ember from 'ember';
+//import fetch from 'fetch';
 
 export default DS.JSONAPIAdapter.extend({
   host: ENV.wnycAPI,
   namespace: 'api/v2/related',
   query(store, type, query) {
     let url= [this.host, this.namespace, query.itemId, `?limit=${query.limit}`].join('/');
-    return fetch(url).then(response => response.json());
+    // Django isn't setup to honor XHR requests at the related stories endpoint,
+    // so just use the jQuery JSONp for now
+    // return fetch(url).then(response => response.json());
+    return Ember.$.ajax(url, {dataType: 'jsonp', jsonpCallback: 'RELATED', cache: true})
+      .then(d => d);
   }
 });
