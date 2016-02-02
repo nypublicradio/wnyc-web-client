17c3c7e723214ec719cab10998c6831b2570d9b3
Brian Whitton <me@brianwhitton.com>
2016-02-02 14:50:13 -0500

    don't need to use JSONAP with new endpoint

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,14 +3,6 @@ import ENV from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
     namespace: 'api/v3',
-    host: ENV.wnycAPI,
-    ajaxOptions(url) {
-      return {
-        url: url,
-        dataType: 'jsonp',
-        jsonpCallback: 'WNYC'
-      };
-    },
     // ember 2.0 deprecation
     shouldBackgroundReloadRecord() {
       return false;
