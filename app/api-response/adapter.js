c5a96d1274faa4957dfd32b41b67c28483510dc3
Brian Whitton <me@brianwhitton.com>
2016-02-02 14:51:21 -0500

    handle ember's attempt to encode our slashes

--- app/api-response/adapter.js
+++ app/api-response/adapter.js
@@ -1,5 +1,9 @@
 import ApplicationAdapter from '../adapters/application';
 
 export default ApplicationAdapter.extend({
-  pathForType() { return 'channel'; }
+  pathForType() { return 'channel'; },
+  buildURL() {
+    let url = this._super(...arguments);
+    return decodeURIComponent(url);
+  }
 });
