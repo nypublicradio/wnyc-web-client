fa598d45357dd977ce7f05818f692e05268bdbe9
Brian Whitton <me@brianwhitton.com>
2016-02-04 18:36:20 -0500

    whitespace fix in comments model

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    author: DS.attr('string'),
-    location: DS.attr('string'),
-    comment: DS.attr('string'),
-    publishDate: DS.attr('string')
+  author: DS.attr('string'),
+  location: DS.attr('string'),
+  comment: DS.attr('string'),
+  publishDate: DS.attr('string')
 });
