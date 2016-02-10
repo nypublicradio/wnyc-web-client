a353324f6ae5b9cfd37f4d268922e3011da4d8c3
Brian Whitton <me@brianwhitton.com>
2016-02-10 10:13:23 -0500

    updates ember-cli version

--- app/app.js
+++ app/app.js
@@ -1,27 +1,16 @@
 import Ember from 'ember';
-import Resolver from 'ember/resolver';
-import loadInitializers from 'ember/load-initializers';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 let App;
-let RE = /^(?:route|template):(shows|series|articles|tags|blogs)/;
-
-let NYPRResolver = Resolver.extend({
-  parseName(fullName) {
-    if (RE.test(fullName)) {
-      let match = RE.exec(fullName);
-      return this._super(fullName.replace(match[1], 'channel'));
-    }
-    return this._super(fullName);
-  }
-});
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: NYPRResolver
+  Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
