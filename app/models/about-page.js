e96317106e8ab443a65742ea64acacbd4540a5d2
Brian Whitton <me@brianwhitton.com>
2016-02-23 00:08:36 -0500

    adds escapedBody field to about model to preserve scripts
    the scripts will still need to be manually appended in order to run

--- app/models/about-page.js
+++ app/models/about-page.js
@@ -1,7 +1,15 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+const { computed, get } = Ember;
 
 export default DS.Model.extend({
   body: DS.attr('string'),
   people: DS.attr(),
-  social: DS.attr()
+  social: DS.attr(),
+  escapedBody: computed('body', {
+    get() {
+      let body = get(this, 'body');
+      return body.replace(/\\x3C\/script>/g, '</script>');
+    }
+  }),
 });
