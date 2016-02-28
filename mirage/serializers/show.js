6c83cad80aa5d29320567f57d2a8ca862100b258
Brian Whitton <me@brianwhitton.com>
2016-02-27 20:48:56 -0500

    uses the show serializer to create relationships
    shows and stories are related on the server using a DB config knonw as
    a LinkRoll. that relationship is represented in ember land using an
    ApiResponse model. This relationship is mimicked using the show
    serializer in mirage.

--- mirage/serializers/show.js
+++ mirage/serializers/show.js
@@ -1,4 +1,5 @@
 import ApplicationSerializer from './application';
+import serialize from 'overhaul/mirage/utils/serialize';
 
 export default ApplicationSerializer.extend({
   typeKeyForModel(model) {
@@ -8,6 +9,38 @@ export default ApplicationSerializer.extend({
       return 'channel';
     } else {
       return ApplicationSerializer.prototype.typeKeyForModel(model);
+    }
+  },
+  serialize({id, attrs}, response) {
+    const REGEX = /shows\/([^\/]+)-\d+/;
+    let type = id.match(REGEX)[1];
+    let apiResponseId = `${id}${attrs.linkroll[0].navSlug}/1`;
+
+    switch(type) {
+      case 'list':
+        let storyList = server.createList('story', 11);
+        let apiResponse = server.create('api-response', {
+          id: apiResponseId,
+          totalCount: 11,
+          teaseList: server.schema.story.find(storyList.mapBy('id'))
+        });
+        break;
+      case 'story':
+        let story = server.create('story');
+        break;
+      case 'about':
+        break;
+    }
+
+    let apiResponseModel = server.schema.apiResponse.find(apiResponse.id);
+    let {data, included} = serialize(apiResponseModel);
+    return {
+      data: {
+        type: 'channel',
+        id,
+        attributes: attrs
+      },
+      included: included.concat(data)
     }
   }
 });
