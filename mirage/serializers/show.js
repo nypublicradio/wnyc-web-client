0fa162ead10ed65eccff8913fb4c83b3fc17724a
Brian Whitton <me@brianwhitton.com>
2016-02-29 18:27:02 -0500

    updates show serializer to return channels wtih diferent included data

--- mirage/serializers/show.js
+++ mirage/serializers/show.js
@@ -15,32 +15,40 @@ export default ApplicationSerializer.extend({
     const REGEX = /shows\/([^\/]+)-\d+/;
     let type = id.match(REGEX)[1];
     let apiResponseId = `${id}${attrs.linkroll[0].navSlug}/1`;
+    let apiResponse;
+    let data = {
+      type: 'channel',
+      id,
+      attributes: attrs
+    };
 
     switch(type) {
       case 'list':
-        let storyList = server.createList('story', 11);
-        let apiResponse = server.create('api-response', {
+        let storyList = server.createList('story', 50);
+        apiResponse = server.create('api-response', {
           id: apiResponseId,
-          totalCount: 11,
+          totalCount: 1000,
           teaseList: server.schema.story.find(storyList.mapBy('id'))
         });
         break;
       case 'story':
         let story = server.create('story');
+        apiResponse = server.create('api-response', {
+          id: apiResponseId,
+          totalCount: 1,
+          story: server.schema.story.find(story.id)
+        });
         break;
       case 'about':
+        return { data };
         break;
     }
 
     let apiResponseModel = server.schema.apiResponse.find(apiResponse.id);
-    let {data, included} = serialize(apiResponseModel);
+    let {data:apiResponseJSON, included} = serialize(apiResponseModel);
     return {
-      data: {
-        type: 'channel',
-        id,
-        attributes: attrs
-      },
-      included: included.concat(data)
+      data,
+      included: included.concat(apiResponseJSON)
     }
   }
 });
