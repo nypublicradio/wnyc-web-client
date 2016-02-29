f0a86f20b1fe6d44d27074b99f87df3e5dc29d1c
Brian Whitton <me@brianwhitton.com>
2016-02-29 18:34:54 -0500

    adds pagination tests and some required story fields

--- tests/acceptance/paginating-show-listings-test.js
+++ tests/acceptance/paginating-show-listings-test.js
@@ -25,5 +25,64 @@ test('showing pagination for a list of episodes', function(assert) {
   });
 });
 
+test('showing no pagination on about pages', function(assert) {
+  let show = server.create('show', {firstPage: 'about'});
+  showPage
+    .bootstrap(show)
+    .visit(show);
 
+  andThen(function() {
+    assert.equal(find('#pagefooter').length, 0, 'is not showing pagination');
+  });
+});
 
+test('showing no pagination on story detail listing pages', function(assert) {
+  let show = server.create('show', {firstPage: 'story'});
+  showPage
+    .bootstrap(show)
+    .visit(show);
+
+  andThen(function() {
+    assert.equal(find('#pagefooter').length, 0, 'is not showing pagination');
+  });
+});
+
+test('can go back and forward', function(assert) {
+  let show = server.create('show');
+  let firstStoryTitle;
+
+  showPage
+    .bootstrap(show)
+    .visit(show);
+
+  andThen(function() {
+    firstStoryTitle = showPage.storyTitles()[0];
+    showPage.clickNext();
+  });
+  andThen(function() {
+    assert.notEqual(firstStoryTitle, showPage.storyTitles()[0]);
+    assert.equal(currentURL(), `/${show.id}${show.linkroll[0].navSlug}/2`, 'uses nav slug when paginating');
+    showPage.clickBack();
+  });
+  andThen(function() {
+    assert.equal(currentURL(), `/${show.id}${show.linkroll[0].navSlug}/1`, 'uses nav slug when paginating');
+    assert.equal(firstStoryTitle, showPage.storyTitles()[0]);
+  });
+});
+
+test('can navigate to a specified page of results', function(assert) {
+  let show = server.create('show');
+  let firstStoryTitle;
+
+  showPage
+    .bootstrap(show)
+    .visit(show);
+
+  andThen(function() {
+    showPage.clickPage(5);
+  });
+  andThen(function() {
+    assert.notEqual(firstStoryTitle, showPage.storyTitles()[0]);
+    assert.equal(currentURL(), `/${show.id}${show.linkroll[0].navSlug}/5`, 'uses nav slug when paginating');
+  });
+});
