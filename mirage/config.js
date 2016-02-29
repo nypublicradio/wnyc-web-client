3a6562080b3683dd21c22110b27b2587dd9a8a01
Brian Whitton <me@brianwhitton.com>
2016-02-29 18:26:00 -0500

    updates mirage config to return an api-response with stories

--- mirage/config.js
+++ mirage/config.js
@@ -3,8 +3,23 @@ import config from 'overhaul/config/environment';
 export default function() {
   this.namespace = config.wnycURL;
 
-  this.get('/api/v3/channel/shows/:showId/:navSlug/1', function(schema, request) {
-    let { showId, navSlug } = request.params;
-    return schema.apiResponse.find(`shows/${showId}/${navSlug}/1`);
+  this.get('/api/v3/channel/shows/:showId/:navSlug/:pageNumber', function(schema, request) {
+    let { showId, navSlug, pageNumber } = request.params;
+    let id = `shows/${showId}/${navSlug}/${pageNumber}`;
+    let apiResponse = schema.apiResponse.find(id);
+
+    if (!apiResponse) {
+      let attrs;
+      let storyList = server.createList('story', 50);
+      if (/list/.test(showId)) {
+        attrs = {
+          id,
+          totalCount: 1000,
+          teaseList: server.schema.story.find(storyList.mapBy('id'))
+        }
+      }
+      return schema.apiResponse.create(attrs);
+    }
+    return apiResponse;
   });
 }
