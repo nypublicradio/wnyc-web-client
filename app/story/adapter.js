e69b7860702b07fdfd04b766bfbf25f95be49395
Brian Whitton <me@brianwhitton.com>
2016-03-01 19:39:55 -0500

    Force XMLHttpRequests in test mode
    Pretender.js only overwrites window.XMLHttpRequest and ignores JSONP
    requests, so we need to ensure any network calls use the mocked object

--- app/story/adapter.js
+++ app/story/adapter.js
@@ -7,11 +7,16 @@ export default DS.JSONAPIAdapter.extend({
   host: ENV.wnycAPI,
   namespace: 'api/v2/related',
   query(store, type, query) {
-    let url= [this.host, this.namespace, query.itemId, `?limit=${query.limit}`].join('/');
+    let url = [this.host, this.namespace, query.itemId, `?limit=${query.limit}`].join('/');
     // Django isn't setup to honor XHR requests at the related stories endpoint,
     // so just use the jQuery JSONp for now
     // return fetch(url).then(response => response.json());
-    return Ember.$.ajax(url, {dataType: 'jsonp', jsonpCallback: 'RELATED', cache: true})
-      .then(d => d);
+    if (ENV.environment === 'test') {
+      // Pretender.js only intercepts XML requests, not JSONP or native Fetch
+      return Ember.$.ajax(url).then(d => d);
+    } else {
+      return Ember.$.ajax(url, {dataType: 'jsonp', jsonpCallback: 'RELATED', cache: true})
+        .then(d => d);
+    }
   }
 });
