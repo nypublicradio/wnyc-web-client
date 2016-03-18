a2560379a4aa5bd17800403f5389377baadab9db
Brian Whitton <me@brianwhitton.com>
2016-03-18 13:33:47 -0400

    adds "None" as fallback as per NPR spec
    also adds 3 more slots to default array

--- app/metrics-adapters/google-analytics.js
+++ app/metrics-adapters/google-analytics.js
@@ -1,7 +1,7 @@
 import GoogleAnalytics from 'ember-metrics/metrics-adapters/google-analytics';
 import {siteName} from 'overhaul/config/environment';
 
-const DEFAULT_NPR_VALS = ['NYPR', ...Array(7), siteName, null, document.title];
+const DEFAULT_NPR_VALS = ['NYPR', ...Array(7), siteName, null, document.title, Array(3)];
 
 export default GoogleAnalytics.extend({
   identify({isAuthenticated}) {
@@ -13,7 +13,7 @@ export default GoogleAnalytics.extend({
   nprDimensions({nprVals = DEFAULT_NPR_VALS}) {
     for (let i = 0; i < nprVals.length; i++) {
       // NPR Dimensions begin at slot 6
-      window.ga('set', `dimension${i + 6}`, nprVals[i]);
+      window.ga('set', `dimension${i + 6}`, nprVals[i] || 'None');
     }
   }
 });
