ac6b1cef285ab80f72b78aacda937e11d16b41d6
Brian Whitton <me@brianwhitton.com>
2016-03-18 17:26:47 -0400

    bundle legacy deps during testing

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -34,7 +34,7 @@ module.exports = function(defaults) {
         'WNYC_URL',
       ],
     },
-    storeConfigInMeta: EmberApp.env() !== 'development',
+    storeConfigInMeta: env !== 'development',
 
     // This project's in-repo addon configuration confuses Mirage and makes it
     // think we are distributing this project as an addon. Because of this it assumes
@@ -65,6 +65,18 @@ module.exports = function(defaults) {
   app.import('vendor/wnyc-legacy/lib/jquery/jquery.ba-postmessage.js');
   app.import('vendor/wnyc-legacy/lib/jquery/jquery.xdr.js');
   app.import('vendor/wnyc-legacy/lib/jquery/browserWarn.js');
+
+  if (env === 'test') {
+    app.import('vendor/wnyc-bootstrap/index.js');
+    app.import('vendor/wnyc-legacy/util.js');
+    app.import('vendor/wnyc-legacy/lib/wnyc/listening.js');
+    app.import('vendor/wnyc-legacy/lib/wnyc/namespace.js');
+    app.import('vendor/wnyc-legacy/overhaul/story/namespace_ext.js');
+    app.import('vendor/wnyc-legacy/lib/wnyc/jquery.js');
+    app.import('vendor/wnyc-legacy/lib/jquery/jquery.ba-postmessage.js');
+    app.import('vendor/wnyc-legacy/lib/jquery/jquery.xdr.js');
+    app.import('vendor/wnyc-legacy/lib/jquery/browserWarn.js');
+  }
 
   return app.toTree();
 };
