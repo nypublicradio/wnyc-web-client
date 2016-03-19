552ae3b1adc09333697c3fca99653c46e26e99be
Brian Whitton <me@brianwhitton.com>
2016-03-19 14:53:53 -0400

    simplifies session-manager to use current-user and browser-id models

--- app/adapters/current-user.js
+++ app/adapters/current-user.js
@@ -1,13 +1,19 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 import fetch from 'fetch';
 import ENV from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  findRecord() {
-    return fetch(ENV.wnycAccountRoot + `/api/v1/is_logged_in/?bust_cache=${Math.random()}`, {
-      credentials: 'include'
-    }).then(checkStatus)
-      .then(response => response.json());
+  findRecord(store) {
+    let user = store.peekRecord('current_user', 'current');
+    if (!user) {
+      return fetch(ENV.wnycAccountRoot + `/api/v1/is_logged_in/?bust_cache=${Math.random()}`, {
+        credentials: 'include'
+      }).then(checkStatus)
+        .then(response => response.json());
+    } else {
+      return Ember.RSVP.Promise.resolve({ user });
+    }
   }
 });
 
