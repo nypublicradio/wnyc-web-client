9eb72f884e2f2385ac58d052f6ae5e55b7303966
Brian Whitton <me@brianwhitton.com>
2016-03-21 09:29:35 -0400

    calling _trackManagedItemView on story routes

--- app/story/route.js
+++ app/story/route.js
@@ -25,6 +25,7 @@ export default Ember.Route.extend({
     let {gaAction:action, gaLabel:label} = get(model, 'story.analytics');
 
     metrics.trackEvent({
+      eventName: 'viewedStory',
       category: 'Viewed Story',
       action,
       label,
