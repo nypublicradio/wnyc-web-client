376d90127de60899e795fb757de2ddb3524a1e6f
Brian Whitton <me@brianwhitton.com>
2016-04-06 20:41:54 -0400

    adds a serializer for django pages
    we want to return only the text property of the server response, as it
    it were actualy coming back from a legacy server

--- mirage/serializers/django-page.js
+++ mirage/serializers/django-page.js
@@ -1,3 +1,13 @@
-import ApplicationSerializer from './application';
+import { Serializer  } from 'ember-cli-mirage';
 
-export default ApplicationSerializer;
+export default Serializer.extend({
+  // we're actually returning HTML from our server, so we can't have a rootKey
+  root: false,
+  // and mirage expects that one of these two props to be true, even though we
+  // aren't embedding any records with this request. it just needs to be set
+  // to true to work.
+  embed: true,
+  serialize(response, request) {
+    return response.text;
+  }
+});
