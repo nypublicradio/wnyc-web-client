nypublicradio/nypr-publisher-lib

set action to NPR only if it doesn\t belong to any thing else AND it is an npr article

app/story/model.js
@@ -66,8 +66,8 @@ export default Model.extend({
           return `Series: ${c.join('+')}`;
         }
       }).compact().join(' | ');
-      if (isnpr) {
-        gaAction = gaAction ? `NPR | ${gaAction}` : 'NPR';
+      if (isnpr && !gaAction) {
+        gaAction = 'NPR';
       }
       return {
         gaAction,

nypublicradio/wnyc-web-client

app/story/model.js
@@ -67,8 +67,8 @@ export default Model.extend({
           return `Series: ${c.join('+')}`;
         }
       }).compact().join(' | ');
-      if (isnpr) {
-        containers = containers ? `NPR | ${containers}` : 'NPR';
+      if (isnpr && !containers) {
+        containers = 'NPR';
       }
       return {
         containers,