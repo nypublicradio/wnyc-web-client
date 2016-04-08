nypublicradio/nypr-publisher-lib

better npr article testing: look at model char

app/story/model.js
@@ -55,7 +55,7 @@ export default Model.extend({
   analytics: computed('analyticsCode', {
     get() {
       let analyticsCode = get(this, 'analyticsCode');
-      let {channeltitle, showtitle, seriestitles, isnpr} = parseAnalyticsCode(analyticsCode);
+      let {channeltitle, showtitle, seriestitles, modelchar} = parseAnalyticsCode(analyticsCode);
       // compact first to guard against returned undefineds
       let gaAction = [channeltitle, showtitle, seriestitles].compact().map((c, i) => {
         if (i === 0 && c) {
@@ -66,7 +66,7 @@ export default Model.extend({
           return `Series: ${c.join('+')}`;
         }
       }).compact().join(' | ');
-      if (isnpr && !gaAction) {
+      if (modelchar === 'n' && !gaAction) {
         gaAction = 'NPR';
       }
       return {

nypublicradio/wnyc-web-client

app/story/model.js
@@ -56,7 +56,7 @@ export default Model.extend({
   analytics: computed('analyticsCode', {
     get() {
       let analyticsCode = get(this, 'analyticsCode');
-      let {channeltitle, showtitle, seriestitles, isblog, isnpr} = parseAnalyticsCode(analyticsCode);
+      let {channeltitle, showtitle, seriestitles, modelchar, isblog} = parseAnalyticsCode(analyticsCode);
       // compact first to guard against returned undefineds
       let containers = [channeltitle, showtitle, seriestitles].compact().map((c, i) => {
         if (i === 0 && c) {
@@ -67,7 +67,7 @@ export default Model.extend({
           return `Series: ${c.join('+')}`;
         }
       }).compact().join(' | ');
-      if (isnpr && !containers) {
+      if (modelchar === 'n' && !containers) {
         containers = 'NPR';
       }
       return {