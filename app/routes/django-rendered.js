805f3230ede540116c1161e0bd34a485bc45e242
Brian Whitton <me@brianwhitton.com>
2016-04-09 00:36:31 -0400

    fixes search queries with a space
    encode queryParams so they match what the server generates

--- app/routes/django-rendered.js
+++ app/routes/django-rendered.js
@@ -6,15 +6,15 @@ export default Ember.Route.extend({
       refreshModel: true
     }
   },
-  model({ upstream_url }, transition) {
+  model({ upstream_url }, { queryParams }) {
     // This adds trailing slashes, because the server's redirect
     // doesn't otherwise work correctly due to the proxying I'm using
     // in development (which is neeeded due to CORs).
     upstream_url = upstream_url.replace(/\/*$/, '/');
 
-    let queryParams = Object.keys(transition.queryParams).map(p => `${p}=${transition.queryParams[p]}`);
-    if (queryParams.length) {
-      upstream_url += `?${queryParams.join('&')}`;
+    let qp = Object.keys(queryParams).map(p => `${p}=${encodeURIComponent(queryParams[p])}`);
+    if (qp.length) {
+      upstream_url += `?${qp.join('&')}`;
     }
     return this.store.find('django-page', upstream_url);
   }
