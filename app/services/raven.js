d9b3fafb1347577e5a12d0be05e768461c88f222
Brian Whitton <me@brianwhitton.com>
2016-04-12 15:45:29 -0400

    override raven's request to go directly to our dsn endpoint

--- app/services/raven.js
+++ app/services/raven.js
@@ -1,6 +1,19 @@
+import Ember from 'ember';
+import config from 'overhaul/config/environment';
 import RavenLogger from 'ember-cli-sentry/services/raven';
 
 export default RavenLogger.extend({
+  init() {
+    /*global Raven*/
+    this._super(...arguments);
+    Raven.setTransport(({data, onError, onSuccess}) => {
+      Ember.$.ajax(config.sentry.dsn, {
+          data,
+          xhrFields: {withCredentials: true},
+          type: 'POST'})
+        .success(onSuccess).error(onError);
+    });
+  },
 
   unhandledPromiseErrorMessage: '',
 
