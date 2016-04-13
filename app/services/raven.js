4382556e3cbd86bf9d7f5f74400eb365ce4a8900
Brian Whitton <me@brianwhitton.com>
2016-04-13 12:31:22 -0400

    raven update in prep for sentry upgrade

--- app/services/raven.js
+++ app/services/raven.js
@@ -1,20 +1,6 @@
-import Ember from 'ember';
-import config from 'overhaul/config/environment';
 import RavenLogger from 'ember-cli-sentry/services/raven';
 
 export default RavenLogger.extend({
-  init() {
-    /*global Raven*/
-    this._super(...arguments);
-    Raven.setTransport(({data, onError, onSuccess}) => {
-      Ember.$.ajax(config.sentry.dsn, {
-          data,
-          xhrFields: {withCredentials: true},
-          type: 'POST'})
-        .success(onSuccess).error(onError);
-    });
-  },
-
   unhandledPromiseErrorMessage: '',
 
   captureException(/* error */) {
