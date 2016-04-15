629151818034b4edd378f90ed927a00d6ae486ea
Brian Whitton <me@brianwhitton.com>
2016-04-15 16:23:07 -0400

    implements global ajaxError handler for network and CORS failures
    (cherry picked from commit 8b6b26fbeb5de78adf110d1187c862ac63a46a6c)

--- app/services/raven.js
+++ app/services/raven.js
@@ -1,6 +1,26 @@
 import RavenLogger from 'ember-cli-sentry/services/raven';
+import Ember from 'ember';
 
 export default RavenLogger.extend({
+  init() {
+    Ember.$(document).ajaxError((event, jqXHR, ajaxSettings, thrownError) => {
+      // The details of CORS errors are protected by browsers, so we don't get
+      // additional metadata. The jQuery default for these errors are 'error',
+      // which isn't helpful.
+      let fallbackMessage = jqXHR.statusText === 'error' ? "Possibly a CORS error. Extra details are protected." : jqXHR.statusText;
+      // if a thrownError is actually though, use that. otherwise use whatever we got from above.
+      this.captureMessage(thrownError || fallbackMessage, {
+        extra: {
+            type: ajaxSettings.type,
+            url: ajaxSettings.url,
+            data: ajaxSettings.data,
+            status: jqXHR.status,
+            error: thrownError || jqXHR.statusText,
+            response: jqXHR.responseText.substring(0, 100)
+        }
+      });
+    });
+  },
 
   unhandledPromiseErrorMessage: '',
 
@@ -16,7 +36,11 @@ export default RavenLogger.extend({
     return this._super(...arguments);
   },
 
-  ignoreError() {
+  ignoreError(error) {
+    if (error.promise && error.status === 0) {
+      // let global ajaxError handler take this
+      return true;
+    }
     return this._super();
   },
 
