04bbf1a0422a71f8ae475366c0e8636783531398
Brian Whitton <me@brianwhitton.com>
2016-04-15 15:04:57 -0400

    only replace the prefix if the deploy target is both not demo and not production

--- config/deploy.js
+++ config/deploy.js
@@ -39,7 +39,7 @@ module.exports = function(deployTarget) {
     }
   };
 
-  if (deployTarget === 'navigation') {
+  if (deployTarget !== 'production' && deployTarget !== 'demo') {
     ENV.redis.keyPrefix = deployTarget;
   }
 
