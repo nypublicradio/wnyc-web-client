eb186f9706e4487b8a5687eeccebd796291013c6
Brian Whitton <me@brianwhitton.com>
2016-04-20 12:10:46 -0400

    adds support for legacy listen/queue buttons
    move overhaul story template audio handling to ember

--- app/mixins/legacy-support.js
+++ app/mixins/legacy-support.js
@@ -3,12 +3,33 @@ import Ember from 'ember';
 const {
   Mixin,
   inject,
-  get
+  get,
+  $
 } = Ember;
+
+const LEGACY_EVENT_SELECTORS = ['.js-listen', '.js-queue'];
+
+function legacySelector(target) {
+  return $(target).closest(LEGACY_EVENT_SELECTORS.join(','));
+}
 
 export default Mixin.create({
   legacyServices: inject.service(),
 
+  isLegacyEvent(event) {
+    return !!legacySelector(event.target).length;
+  },
+  fireLegacyEvent(target) {
+    let legacy = get(this, 'legacyServices');
+    let legacyTarget = legacySelector(target);
+    let { itemPK, itemTitle } = legacyTarget.data('ember-args');
+    if (legacyTarget.hasClass('js-listen')) {
+      return legacy.listen(itemPK, itemTitle);
+    }
+    if (legacyTarget.hasClass('js-queue')) {
+      return legacy.queue(itemPK);
+    }
+  },
   actions: {
     listen(model, streamSlug) {
       const id = get(model, 'id');
