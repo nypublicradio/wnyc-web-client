b06afe6731cad6674c53f1707262d56aa9a9b42a
Jeff Keen <jeff@keen.me>
2016-05-09 23:03:24 -0500

    Add some select all / select none tests for discover topic list component

--- tests/integration/components/discover-topic-list/component-test.js
+++ tests/integration/components/discover-topic-list/component-test.js
@@ -18,3 +18,39 @@ test('it renders', function(assert) {
   this.render(hbs`{{discover-topic-list topics=topics}}`);
   assert.equal(this.$('.discover-topic').length, 20);
 });
+
+test('select all selects all topics', function(assert) {
+  this.set('selectedTopics', []);
+  this.set('topics', server.createList('discover-topic', 20));
+  this.render(hbs`{{discover-topic-list topics=topics selectedTopics=selectedTopics}}`);
+
+  this.$('a:contains("Select All")').click();
+  assert.equal(this.get('selectedTopics').length, this.get('topics').length);
+});
+
+test('select none selects none', function(assert) {
+  this.set('selectedTopics', []);
+  this.set('topics', server.createList('discover-topic', 20));
+  this.render(hbs`{{discover-topic-list topics=topics selectedTopics=selectedTopics}}`);
+
+  this.$('a:contains("Select All")').click();
+  this.$('a:contains("Select None")').click();
+  assert.equal(this.get('selectedTopics').length, 0);
+});
+
+test('select none only shows up when all are selected', function(assert) {
+  this.set('selectedTopics', []);
+  this.set('topics', server.createList('discover-topic', 20));
+  this.render(hbs`{{discover-topic-list topics=topics selectedTopics=selectedTopics}}`);
+
+  var _this = this;
+  this.$('input[type=checkbox]').each(function(i, c) {
+    $(c).click();
+    if (i === 19) {
+      assert.equal(_this.$('a:contains("Select None")').length, 1, "Should be 'Select None' when all are selected");
+    }
+    else {
+      assert.equal(_this.$('a:contains("Select None")').length, 0, "Should be 'Select All' when not all are selected");
+    }
+  });
+});
