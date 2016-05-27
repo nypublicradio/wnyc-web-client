c2d32f2ca48e5628222974c4d4f974801229153f
Brian Whitton <me@brianwhitton.com>
2016-05-27 13:38:13 -0400

    get show data from the local store

--- app/services/legacy-services.js
+++ app/services/legacy-services.js
@@ -9,6 +9,7 @@ const {
 
 export default Service.extend({
   metrics: service(),
+  store: service(),
 
   streamStation(streamSlug) {
     if (!streamSlug) {
@@ -17,20 +18,24 @@ export default Service.extend({
       wnyc.xdPlayer.playStream(streamSlug);
     }
   },
-  listen(id, title, show) {
+  listen(id, title) {
     const metrics = get(this, 'metrics');
+    const store = get(this, 'store');
+    const story = store.peekRecord('story', id);
     try {
       wnyc.xdPlayer.playOnDemand(id);
     } catch(e) {
       console.warn('Cross Domain Player does not exist');
     }
 
-    metrics.trackEvent({
-      category: 'Cross-Domain Player',
-      action: `Played OnDemand Show "${show}"`,
-      label: title,
-      model: {cmsPK: id}  // conform to how the data-warehouse metrics adapter expects
-    });
+    if (story) {
+      metrics.trackEvent({
+        category: 'Cross-Domain Player',
+        action: `Played OnDemand Show "${story.get('headers.brand.title')}"`,
+        label: title,
+        model: {cmsPK: id}  // conform to how the data-warehouse metrics adapter expects
+      });
+    }
   },
   queue(id) {
     try {
