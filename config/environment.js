60a1612ef25e9b77731e218b85b366bde1e2736d
Brian Whitton <me@brianwhitton.com>
2016-06-01 18:17:33 -0400

    another item for error blacklist

--- config/environment.js
+++ config/environment.js
@@ -52,7 +52,8 @@ module.exports = function(environment) {
           /^chrome:\/\//i,
         ],
         ignoreErrors: [
-          'adsafeprotected'
+          'adsafeprotected',
+          'sascdn'
         ]
       }
     },
