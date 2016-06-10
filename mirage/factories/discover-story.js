eb1b890f08ff57352527d0ac6e14f9268dc5752f
Jeff Keen <jeff@keen.me>
2016-06-10 16:59:54 -0500

    Some more mods for testing/playing audio in dev/test

--- mirage/factories/discover-story.js
+++ mirage/factories/discover-story.js
@@ -3,13 +3,17 @@ import { Factory, faker } from 'ember-cli-mirage';
 /* This is representing the response we're getting from the API. This is different than the discover/story model format we're using in the discover feature. Data gets transformed a bit by the serializer */
 
 export default Factory.extend({
-  pk()    { return faker.random.number(10); },
+  pk()    { return faker.random.number(1000000); },
   // this needs to be only numbers, or the audio service will think this it's a 'stream' instead of 'onDemand'.
 
   title() { return faker.lorem.sentence(); },
   tease() { return faker.lorem.sentence(); },
   url()   { return `/story/${this.pk}`; },
-  audio() { return `/assets/test-audio.mp3`; },
+  audio() { return `assets/test-audio.mp3?${this.pk}`; },
+  // The audio URL is tightly coupled, assumed to be unique, and somewhere
+  // down deep in an okra bridge there's some logic that updates the current
+  // audio. If this isn't unique, play status gets totally borked
+
   type(i) { return faker.list.random('nprarticle', 'article')(i); },
   newsdate() { return faker.date.recent(); },
   detail_api_url() { return `/api/story/${this.pk}/`; },
