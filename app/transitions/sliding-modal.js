fb428195389da309944eca485c1c8707919540a9
walsh9 <walsh9@gmail.com>
2016-07-08 13:44:52 -0400

    Add webkit overflow scrolling to liquid container

--- app/transitions/sliding-modal.js
+++ app/transitions/sliding-modal.js
@@ -28,6 +28,7 @@ export default function() {
     width: '100%',
     height: '100%',
     top: 0,
+    '-webkit-overflow-scrolling': 'touch'
   });
 
   return animate(el, {translateY}, animationOptions)
