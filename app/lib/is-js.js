f533ff54f92f01cc345876243c86898ab96dae92
Brian Whitton <me@brianwhitton.com>
2016-07-12 11:36:56 -0400

    updates isJavascript regex match

--- app/lib/is-js.js
+++ app/lib/is-js.js
@@ -2,5 +2,5 @@ export default function isJavascript(scriptTag) {
   // TODO: add a console warning if the script tag doesn't have an attribute? 
   // seems like it's required for some parts of ember consumption
   let type = scriptTag.attributes.type ? scriptTag.attributes.type.value : 'text/javascript';
-  return /(?:application|text)\/javascript/i.test(type);
+  return /(?:application|text)\/(deferred-)?javascript/i.test(type);
 }
