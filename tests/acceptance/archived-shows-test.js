98dc8f16b12a89c0182c72e295d27f0a58d0aabd
Diana Parrington <diana.parrington@gmail.com>
2016-07-19 11:26:46 -0400

    add a simpleLayout option to show-tease, and test it

---
+++ tests/acceptance/archived-shows-test.js
@@ -0,0 +1,17 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'overhaul/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | archived-shows');
+
+test('visiting /archived-shows', function(assert) {
+  server.createList('show', 25);
+  visit('/archived-shows');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/archived-shows');
+
+    //all 25 shows are listed, per test data
+    assert.equal( $('.shows-list ul li').length, 25, "twenty five shows are listed" );
+
+  });
+});
--- tests/integration/components/show-tease/component-test.js
+++ tests/integration/components/show-tease/component-test.js
@@ -32,8 +32,9 @@ test('it renders', function(assert) {
 
   this.set("testShow", testShow);
   this.set("featured", false);
+  this.set("simpleLayout", false);
 
-  this.render(hbs`{{show-tease show=testShow featuredSpot=featured}}`);
+  this.render(hbs`{{show-tease show=testShow featuredSpot=featured trackingCategory="Test Category" simpleLayout=simpleLayout}}`);
   
   assert.equal(this.$(".flag").hasClass("flag--med"), true, "Regular Show Tease has class flag-med");
   assert.equal(this.$('.flag-body .text h4').text().trim(), 'WNYC and NPR', 'Producing orgs should render');
@@ -41,6 +42,8 @@ test('it renders', function(assert) {
   assert.equal(this.$('.flag-body .text h2').text().trim(), 'All Things Considered', 'Title should render correctly');
   assert.equal(this.$('.flag-body .text .text-body--nopad').text().trim(), "A wrap-up of the day's news, with features and interviews.", 'Description should render correctly');
   assert.equal(this.$('.flag-image img').attr("src"), "http://www.wnyc.org/i/90/90/l/99/1/2DQ_1400X1400_NoWNYCSTUDIOS_2.png", "Image is populated");
+  assert.equal(this.$('.flag-body .text h2 a').attr("data-tracking-category"), "Test Category", "Tracking Category should render on title link");
+  assert.equal(this.$('a.flag-image').attr("data-tracking-category"), "Test Category", "Tracking Category should render on image link" );
 
 
   // check for edits when it is a featured item
@@ -52,6 +55,18 @@ test('it renders', function(assert) {
   assert.equal(this.$('.flag-body .text h2').text().trim(), 'All Things Considered', 'Title should render correctly');
   assert.equal(this.$('.flag-body .text .h3').text().trim(), "A wrap-up of the day's news, with features and interviews.", 'Description should render correctly');
   assert.equal(this.$('.flag-image img').attr("src"), "http://www.wnyc.org/i/150/150/l/99/1/2DQ_1400X1400_NoWNYCSTUDIOS_2.png", "Image is populated");
+  assert.equal(this.$('.flag-body .text h2 a').attr("data-tracking-category"), "Test Category", "Tracking Category should render on title link");
+  assert.equal(this.$('a.flag-image').attr("data-tracking-category"), "Test Category", "Tracking Category should render on image link" );
 
+  // check for edits when using simple layout - no producing org or tease text
+  this.set("featured", false);
+  this.set("simpleLayout", true);
+  assert.equal(this.$(".flag").hasClass("flag--med"), true, "Regular Show Tease has class flag-med");
+  assert.equal(this.$('.flag-body .text h4').length, 0, 'Producing orgs should not render');
+  assert.equal(this.$('.flag-body .text h2').text().trim(), 'All Things Considered', 'Title should render correctly');
+  assert.equal(this.$('.flag-body .text .text-body--nopad').length, 0, 'Description not render');
+  assert.equal(this.$('.flag-image img').attr("src"), "http://www.wnyc.org/i/135/135/l/99/1/2DQ_1400X1400_NoWNYCSTUDIOS_2.png", "Image is populated");
+  assert.equal(this.$('.flag-body .text h2 a').attr("data-tracking-category"), "Test Category", "Tracking Category should render on title link");
+  assert.equal(this.$('a.flag-image').attr("data-tracking-category"), "Test Category", "Tracking Category should render on image link" );
 
 });
