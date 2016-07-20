64a7cb48128affaaa49d0f6cdf369568ea006a9d
walsh9 <walsh9@gmail.com>
2016-07-20 17:44:01 -0400

    Fix numeric-duration helper

--- app/helpers/numeric-duration.js
+++ app/helpers/numeric-duration.js
@@ -1,11 +1,16 @@
 import moment from 'moment';
 import { helper } from 'ember-helper';
 
-export default helper(function([time]) {
-  let hours = moment.duration(time).get('hours');
-  let minutes = moment.duration(time).get('minutes');
-  let seconds = moment.duration(time).get('seconds');
+export function numericDuration([milliseconds]) {
+  let hours = moment.duration(milliseconds).get('hours');
+  let minutes = moment.duration(milliseconds).get('minutes');
+  if (hours > 1) {
+    minutes = String(minutes).length === 1 ? `0${minutes}` : minutes;
+  }
+  let seconds = moment.duration(milliseconds).get('seconds');
   seconds = String(seconds).length === 1 ? `0${seconds}` : seconds;
 
   return `${hours > 0 ? `${hours}:` : ''}${minutes}:${seconds}`;
-});
+}
+
+export default helper(numericDuration);
