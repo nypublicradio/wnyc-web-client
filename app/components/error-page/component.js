40c87a18ed6f6a12a202249b397a5fa48d027977
Diana Parrington <diana.parrington@gmail.com>
2016-07-20 09:32:41 -0400

    typo on equals

--- app/components/error-page/component.js
+++ app/components/error-page/component.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   errorType: Ember.computed.readOnly('error.response.status'),
-  pageNotFound: Ember.computed.equals('errorType', 404),
-  serverError: Ember.computed.equals('errorType', 500)
+  pageNotFound: Ember.computed.equal('errorType', 404),
+  serverError: Ember.computed.equal('errorType', 500)
 });
