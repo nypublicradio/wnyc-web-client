16a8c5ccf366bcf42bb2d2b72314266b7ff254b6
Diana Parrington <diana.parrington@gmail.com>
2016-07-25 10:32:04 -0400

    active link param edits

--- app/helpers/active-link.js
+++ app/helpers/active-link.js
@@ -1,10 +1,8 @@
 import { helper } from 'ember-helper';
 
-export function activeLink(params) {
-  let [linkPath, currentUrl] = params;
+export function activeLink([ linkPath, currentUrl ]) {
   let regex = new RegExp("^" + linkPath, "i");
- // if the first part of the pathname matches the url, set active
-  if (currentUrl.match(regex)) {
+  if (currentUrl && currentUrl.match(regex)) {
     return "active";  
   }
 }
