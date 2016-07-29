8b4425a1f89760c5eae81b606795b6db93a9c247
walsh9 <walsh9@gmail.com>
2016-07-29 17:39:45 -0400

    Animate sign up links with player

--- app/transitions/player-reveal.js
+++ app/transitions/player-reveal.js
@@ -6,9 +6,11 @@ export default function() {
     let player = this.newElement.find('.persistent-player');
     let floatingQueueButton = this.oldElement.find('.persistent-queuebutton');
     player.css({ transform: 'translateY(80px)' });
+    let playerAnimationOptions = {easing: [0.17, 0.89, 0.39, 1.25], duration: 350}
     return Promise.all([
       animate(floatingQueueButton, {opacity: [0, 1]}, {duration: 125}),
-      animate(player, {translateY: [0, '80px']}, {easing: [0.17, 0.89, 0.39, 1.25], duration: 350})
+      animate($('nav .l-bottom'), {translateY: ['-80px', 0]}, playerAnimationOptions),
+      animate(player, {translateY: [0, '80px']}, playerAnimationOptions)
     ]);
   }
 }
