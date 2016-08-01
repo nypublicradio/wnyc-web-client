681d2d77c92e05f1eedd6107e19ff33c94053a5a
walsh9 <walsh9@gmail.com>
2016-08-01 09:43:13 -0400

    fix jshint errors

--- app/transitions/player-reveal.js
+++ app/transitions/player-reveal.js
@@ -1,4 +1,6 @@
 import { animate, Promise } from 'liquid-fire';
+import Ember from 'ember';
+const { $ } = Ember;
 
 export default function() {
   this.newElement.css('visibility', '');
@@ -6,7 +8,7 @@ export default function() {
     let player = this.newElement.find('.persistent-player');
     let floatingQueueButton = this.oldElement.find('.persistent-queuebutton');
     player.css({ transform: 'translateY(80px)' });
-    let playerAnimationOptions = {easing: [0.17, 0.89, 0.39, 1.25], duration: 350}
+    let playerAnimationOptions = {easing: [0.17, 0.89, 0.39, 1.25], duration: 350};
     return Promise.all([
       animate(floatingQueueButton, {opacity: [0, 1]}, {duration: 125}),
       animate($('nav .l-bottom'), {translateY: ['-80px', 0]}, playerAnimationOptions),
