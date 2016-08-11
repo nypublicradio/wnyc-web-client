f191bbc87c8ec02061ca2d194dee35897a9a430c
Diana Parrington <diana.parrington@gmail.com>
2016-08-11 14:33:35 -0400

    wip add files

---
+++ app/components/forgot-pw/component.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import ENV from 'overhaul/config/environment';
+
+export default Ember.Component.extend({
+  ENV: ENV
+});
---
+++ app/routes/forgot-password.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+import ENV from 'overhaul/config/environment';
+
+export default Ember.Route.extend({
+});
