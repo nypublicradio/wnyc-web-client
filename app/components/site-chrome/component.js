8c746e441c5c31c8dabb175ddb455ddf0bf853ec
Brian Whitton <me@brianwhitton.com>
2016-08-18 11:32:20 -0400

    cleans up site-chrome component JS

--- app/components/site-chrome/component.js
+++ app/components/site-chrome/component.js
@@ -1,23 +1,18 @@
-import Ember from 'ember';
+import Component from 'ember-component';
 import service from 'ember-service/inject';
-import config from 'overhaul/config/environment';
 import BetaActionsMixin from 'overhaul/mixins/beta-actions';
 
-export default Ember.Component.extend(BetaActionsMixin, {
+export default Component.extend(BetaActionsMixin, {
   session: service(),
   metrics: service(),
   router: service('wnyc-routing'),
-  // we load SVGs via the <use> element, which requires the asset loaded
-  // on the same domain
-  svgURL: config.wnycSvgURL,
  
-  click: function(e){
-    if (e.target.tagName === "A"){
+  click: function({target}){
+    if (target.tagName === "A"){
       //send tracking
-      let text =  e.target.text;
       this.get('metrics').trackEvent({
         category: 'WNYC Menu',
-        action: "Clicked " + text,
+        action: "Clicked " + target.text,
       });
     }
   },
