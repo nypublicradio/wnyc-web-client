c6e3c58d1b57cb9e0966729034c049ba359f39bb
Brian Whitton <me@brianwhitton.com>
2016-08-18 11:31:39 -0400

    removes scrolling callback for site-chrome
    we're doing the sticky header using pure CSS thanks to the position:
    -webkit-sticky, so no need for all this JS

--- app/components/site-chrome/component.js
+++ app/components/site-chrome/component.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 import service from 'ember-service/inject';
 import config from 'overhaul/config/environment';
 import BetaActionsMixin from 'overhaul/mixins/beta-actions';
-import { bind } from 'ember-runloop';
-
-var $window = Ember.$(window),
-  $document = Ember.$(document);
 
 export default Ember.Component.extend(BetaActionsMixin, {
   session: service(),
@@ -14,29 +10,7 @@ export default Ember.Component.extend(BetaActionsMixin, {
   // we load SVGs via the <use> element, which requires the asset loaded
   // on the same domain
   svgURL: config.wnycSvgURL,
-  donateURL: config.wnycDonateURL,
-  fixedNavOffset: 0,
-  classNameBindings: ["fixed-nav"],
-
-  didInsertElement: function() {
-    this._super(...arguments);
-    if (!this.get("media.isLargeAndUp")) { //only on mobile
-      this.set('fixedNavOffset',  this.$('.sitechrome-nav').offset().top); //get default top offset
-      $window.on('scroll.fixednav', bind(this, this.checkOffset));
-    }
-  },
  
-  willDestroyElement: function() {
-    this._super(...arguments);
-    if (!this.get("media.isLargeAndUp")) {
-      $window.off('scroll.fixednav');
-    }
-  },
-
-  checkOffset: function() {
-    this.set("fixed-nav", $document.scrollTop() >= this.get('fixedNavOffset') );
-  },
-
   click: function(e){
     if (e.target.tagName === "A"){
       //send tracking
