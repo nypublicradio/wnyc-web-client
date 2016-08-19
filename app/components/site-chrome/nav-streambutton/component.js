dab2fa44ad4082068b9977ac92ebc8806a331780
walsh9 <walsh9@gmail.com>
2016-08-19 12:08:58 -0400

    Make nav-streambutton more decoupled from nav template

--- app/components/site-chrome/nav-streambutton/component.js
+++ app/components/site-chrome/nav-streambutton/component.js
@@ -1,12 +1,19 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
+import computed from 'ember-computed';
 import service from 'ember-service/inject';
 
 export default Component.extend({
   audio: service(),
-  tagName: 'li',
-  classNames: ['list-item', 'list-item--short'],
-  stationSlug: 'wnyc-fm939',
+  store: service(),
+  tagName: '',
+  slug: null,
+  stream: computed('slug', function() {
+    let slug = get(this, 'slug');
+    if (slug) {
+      return get(this, 'store').findRecord('stream', slug);
+    }
+  }),
   actions: {
     listenLive(slug) {
       get(this, 'audio').playStream(slug);
