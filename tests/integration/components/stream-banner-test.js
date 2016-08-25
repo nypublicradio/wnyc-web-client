45659d096eedb26028491336e721b65805966834
walsh9 <walsh9@gmail.com>
2016-08-25 14:44:27 -0400

    Update stream-banner tests

--- tests/integration/components/stream-banner-test.js
+++ tests/integration/components/stream-banner-test.js
@@ -1,7 +1,5 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { setBreakpointForIntegrationTest } from 'overhaul/tests/helpers/responsive';
-
 
 moduleForComponent('stream-banner', 'Integration | Component | stream banner', {
   integration: true
@@ -69,51 +67,42 @@ const dummyStreamShowOnly = {
   }
 };
 
-test('it renders on smallOnly breakpoint', function(assert) {
-  setBreakpointForIntegrationTest(this, 'smallOnly');
-  this.render(hbs`{{stream-banner media=media}}`);
-  assert.equal(this.$('.streambanner-header').text().trim(), 'ON AIR NOW', 'it should render on small breakpoint');
-});
+const streamTitleSelector = '.streambanner-station:first-child';
+const streamDetailsSelector = '.streambanner-details';
 
-test('it does not render on mediumAndUp breakpoint', function(assert) {
-  setBreakpointForIntegrationTest(this, 'mediumAndUp');
-  this.render(hbs`{{stream-banner media=media}}`);
-  assert.notOk(this.$('.streambanner-header').text().trim(), 'ON AIR NOW', 'it should not render on the mediumAndUp breakpoint');
+test('it renders', function(assert) {
+  this.render(hbs`{{stream-banner}}`);
+  assert.equal(this.$('.streambanner-header').text().trim(), 'ON AIR NOW', 'it should render');
 });
 
 test('it displays the show title', function(assert) {
-  setBreakpointForIntegrationTest(this, 'smallOnly');
   this.set('stream', dummyStreamShowOnly);
-  this.render(hbs`{{stream-banner media=media stream=stream}}`);
-  assert.equal(this.$('.streambanner-station:first:child').text().trim(), 'Show Title', 'it should display the show title');
+  this.render(hbs`{{stream-banner stream=stream}}`);
+  assert.equal(this.$(streamTitleSelector).text().trim(), 'Show Title', 'it should display the show title');
 });
 
 test('it displays the show title for streams with episodes', function(assert) {
-  setBreakpointForIntegrationTest(this, 'smallOnly');
   this.set('stream', dummyStreamShowWithEpisode);
-  this.render(hbs`{{stream-banner media=media stream=stream}}`);
-  assert.equal(this.$('.streambanner-station:first:child').text().trim(), 'Show Title', 'it should display the show title');
+  this.render(hbs`{{stream-banner stream=stream}}`);
+  assert.equal(this.$(streamTitleSelector).text().trim(), 'Show Title', 'it should display the show title');
 });
 
 test('it displays the show title for streams with music', function(assert) {
-  setBreakpointForIntegrationTest(this, 'smallOnly');
   this.set('stream', dummyStreamShowWithMusic);
-  this.render(hbs`{{stream-banner media=media stream=stream}}`);
-  assert.equal(this.$('.streambanner-station:first:child').text().trim(), 'Show Title', 'it should display the show title');
+  this.render(hbs`{{stream-banner stream=stream}}`);
+  assert.equal(this.$(streamTitleSelector).text().trim(), 'Show Title', 'it should display the show title');
 });
 
 test('it displays the episode title for streams with episodes', function(assert) {
-  setBreakpointForIntegrationTest(this, 'smallOnly');
   this.set('stream', dummyStreamShowWithEpisode);
-  this.render(hbs`{{stream-banner media=media stream=stream}}`);
-  assert.equal(this.$('.streambanner-details').text().trim(), 'Episode Title', 'it should display the episode title');
+  this.render(hbs`{{stream-banner stream=stream}}`);
+  assert.equal(this.$(streamDetailsSelector).text().trim(), 'Episode Title', 'it should display the episode title');
 });
 
 test('it displays the song details for streams with music', function(assert) {
-  setBreakpointForIntegrationTest(this, 'smallOnly');
   this.set('stream', dummyStreamShowWithMusic);
-  this.render(hbs`{{stream-banner media=media stream=stream}}`);
-  assert.equal(this.$('.streambanner-details').text().trim(), 'Song Title', 'it should display the song details');
+  this.render(hbs`{{stream-banner stream=stream}}`);
+  assert.equal(this.$(streamDetailsSelector).text().trim(), 'Song Title, Composer Name, Musician Name (Instrument)', 'it should display the song details');
 });
 
 
