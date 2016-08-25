0ddb6ea40f57e9a00433c5090205e9964cb393f3
walsh9 <walsh9@gmail.com>
2016-08-25 13:05:37 -0400

    Make song-metadata helper testable

--- app/helpers/song-metadata.js
+++ app/helpers/song-metadata.js
@@ -1,13 +1,14 @@
-import Ember from 'ember';
-const { helper } = Ember.Helper;
+import { helper } from 'ember-helper';
 
-export default helper(function([ catalogEntry ]){
-    let metadata = `${catalogEntry.title}, ${catalogEntry.composer.name}`;
-    catalogEntry.soloists.forEach(function(soloist) {
-      metadata += `, ${soloist.musician.name}`;
-      if (soloist.instruments.length > 0) {
-         metadata += ` (${soloist.instruments[0]})`;
-      }
-    });
-    return metadata;
-});
+export function songMetadata([ catalogEntry ]){
+  let metadata = `${catalogEntry.title}, ${catalogEntry.composer.name}`;
+  catalogEntry.soloists.forEach(function(soloist) {
+    metadata += `, ${soloist.musician.name}`;
+    if (soloist.instruments.length > 0) {
+       metadata += ` (${soloist.instruments[0]})`;
+    }
+  });
+  return metadata;
+}
+
+export default helper(songMetadata);
