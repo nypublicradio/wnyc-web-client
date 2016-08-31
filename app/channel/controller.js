5c67faf438832954ad5fdb5e011ca4a51c7bb84f
Brian Whitton <me@brianwhitton.com>
2016-08-31 09:49:04 -0400

    updates channel pod to comply with new page_parms

--- app/channel/controller.js
+++ app/channel/controller.js
@@ -2,8 +2,8 @@ import Controller from 'ember-controller';
 
 export default Controller.extend({
   actions: {
-    navSlugTransition(navSlug) {
-      this.transitionToRoute(`${this.channelType}.well`, navSlug);
+    navSlugTransition(page_params) {
+      this.transitionToRoute(`${this.channelType}.page`, page_params);
     }
   }
 });
--- app/channel/index/route.js
+++ app/channel/index/route.js
@@ -1,20 +1,26 @@
+import Ember from 'ember';
 import Route from 'ember-route';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import ListingRouteMixin from 'overhaul/mixins/listing-route';
+const { Inflector } = Ember;
+
+const inflect = new Inflector(Inflector.defaultRules);
 
 export default Route.extend(ListingRouteMixin, {
   model() {
-    let page = 1;
     let channelType = get(this, 'channelType');
-    let id = this.buildId(channelType, page);
+    let { slug } = this.paramsFor(channelType);
+    let navSlug = this._getNavSlug(channelType);
+    
+    let id = `${inflect.pluralize(channelType)}/${slug}/${navSlug || 'recent_stories'}/${1}`;
 
     set(this, 'pageNumbers.totalPages', 0);
 
     return this.store.findRecord('api-response', id)
       .then(m => {
         // wait until models are loaded to keep UI consistent
-        set(this, 'pageNumbers.page', page);
+        set(this, 'pageNumbers.page', 1);
         set(this, 'pageNumbers.totalPages', get(m, 'totalPages'));
 
         return m;
