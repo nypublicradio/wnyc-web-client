545bcd21d1b7971c29656b88a7081743e4ab0bc5
Brian Whitton <me@brianwhitton.com>
2016-08-31 09:47:16 -0400

    replaces well routes with wildcard page route

--- app/channel/page/route.js
+++ app/channel/page/route.js
@@ -1,20 +1,24 @@
 import Route from 'ember-route';
+import Ember from 'ember';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
+const { Inflector } = Ember;
+
+const inflect = new Inflector(Inflector.defaultRules);
 import ListingRouteMixin from 'overhaul/mixins/listing-route';
 
 export default Route.extend(ListingRouteMixin, {
-  model() {
-    let routes = this.routeName.split('.');
-    let { navSlug } = this.paramsFor(`${routes[0]}.${routes[1]}`);
-    let page = /^\d+$/.test(navSlug) ? navSlug : 1;
-    let id = this.buildId(get(this, 'channelType'), page);
+  model({ page_params }) {
+    let channelType = get(this, 'channelType');
+    let { slug } = this.paramsFor(channelType);
+    let [navSlug, page] = page_params.split('/');
+    let id = `${inflect.pluralize(channelType)}/${slug}/${navSlug}/${page || 1}`;
     set(this, 'pageNumbers.totalPages', 0);
 
     return this.store.findRecord('api-response', id)
       .then(m => {
         // wait until models are loaded to keep UI consistent
-        set(this, 'pageNumbers.page', page);
+        set(this, 'pageNumbers.page', page || 1);
         set(this, 'pageNumbers.totalPages', get(m, 'totalPages'));
 
         return m;
