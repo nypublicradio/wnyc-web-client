18fe58b56cb41fea3e84798642a08c91ca9fad72
Brian Whitton <me@brianwhitton.com>
2016-08-31 16:56:12 -0400

    replace ember-cli-pagination with homegrown version
    the add-on did not properly export its component so we couldn't extend
    it. this is just a stripped down, cribbed version that basically does
    what we need without forcing us to battle with the other author's
    decisions.

--- app/components/page-numbers/component.js
+++ app/components/page-numbers/component.js
@@ -1,21 +1,61 @@
-import Ember from 'ember';
-import PageNumberAddon from 'ember-cli-pagination/components/page-numbers';
+import Component from'ember-component';
+import computed from 'ember-computed';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
 
-const {
-  computed,
-  get
-} = Ember;
-
-export default  PageNumberAddon.extend({
-  currentPageClass: computed('currentPage', {
-    get() {
-      const currentPage = get(this, 'currentPage');
-
-      if(Number(currentPage) === 1) {
-        return 'page-one';
-      } else {
-        return 'pagination';
-      }
+export default Component.extend({
+  
+  hasPages: computed.gt('totalPages', 0),
+  
+  pageItems: computed('currentPage', 'totalPages', function() {
+    let currentPage = get(this, 'currentPage');
+    let totalPages = get(this, 'totalPages');
+    let pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push({
+        dots: i > 10,
+        page: i,
+        current: i === currentPage
+      });
     }
-  })
+    return pages;
+  }),
+
+  currentPageClass: computed('currentPage', function(){
+    let currentPage = get(this, 'currentPage');
+
+    if(Number(currentPage) === 1) {
+      return 'page-one';
+    } else {
+      return 'pagination';
+    }
+  }),
+  
+  canStepForward: computed('currentPage', 'totalPages', function() {
+    let page = Number(get(this, 'currentPage'));
+    let totalPages = Number(get(this, 'totalPages'));
+    return page < totalPages;
+  }),
+  
+  canStepBackward: computed('currentPage', 'totalPages', function() {
+    let page = Number(get(this, 'currentPage'));
+    return page > 1;
+  }),
+  
+  actions: {
+    pageClicked(num) {
+      set(this, 'currentPage', num);
+      this.sendAction('action', num);
+    },
+    incrementPage(num) {
+      let currentPage = get(this, 'currentPage');
+      let totalPages = get(this, 'totalPages');
+      
+      if (currentPage === totalPages && num === 1) { return false; }
+      if (currentPage <= 1 && num === -1) { return false; }
+      this.incrementProperty('currentPage', num);
+      let newPage = this.get('currentPage');
+      this.sendAction('action', newPage);
+    }
+  }
 });
