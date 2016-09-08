230f1c2cdd14e62f29891a2e3d8da2026b8a4b9a
Brian Whitton <me@brianwhitton.com>
2016-09-08 14:52:52 -0400

    clean up page numbers service and api-response factory

--- mirage/factories/api-response.js
+++ mirage/factories/api-response.js
@@ -7,14 +7,4 @@ export default Factory.extend({
   totalCount() {
     return this.teaseList ? this.teaseList.length : 0;
   },
-  //teaseList() {
-  //  if (this.type === 'list') {
-  //    return server.createList('story', 50);
-  //  }
-  //},
-  //story() {
-  //  if (this.type === 'story') {
-  //    return server.create('story');
-  //  }
-  //},
 });
