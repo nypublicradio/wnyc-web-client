25f82c433ad07aa088acb0b51368dd7d050b88ed
Matt Long <matthewhl@gmail.com>
2016-09-08 15:57:01 -0400

    add text

--- app/components/persistent-player/component.js
+++ app/components/persistent-player/component.js
@@ -1,15 +1,24 @@
+import { inject } from 'ember';
 import Component from 'ember-component';
 import service from 'ember-service/inject';
-import { reads, equal } from 'ember-computed';
+import { reads, equal, computed } from 'ember-computed';
 import get from 'ember-metal/get';
 
 export default Component.extend({
   audio: service(),
+  session: inject(),
   currentAudio: reads('audio.currentAudio'),
   isPlaying: equal('audio.playState', 'is-playing'),
   classNames: ['persistent-player', 'l-flexcontent', 'l-highlight--blur'],
   classNameBindings: ['isAudiostream'],
   isAudiostream: equal('currentAudio.audioType', 'stream'),
+  isPlayingAudioCue: equal('currentAudio.id', 1),
+  sessionSettings: computed('session.data.userPrefs', function(){
+    var prefs = this.get('session.data.userPrefs');
+    return {
+      disableAutoPlay: prefs.activePref === 'Do Not Autoplay',
+    };
+  }),
   actions: {
     playOrPause() {
       if (get(this, 'isPlaying')) {
--- app/components/persistent-player/notification/component.js
+++ app/components/persistent-player/notification/component.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  secondsRemaining: Ember.computed('position', function(){
+    return (this.get('position') / 1000) - 15; // ms -> s
+  })
 });
