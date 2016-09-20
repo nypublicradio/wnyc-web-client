dc0aae444fb718e135f9c5677e4d9ece19ea53cc
walsh9 <walsh9@gmail.com>
2016-09-20 18:06:01 -0400

    Get show list with query instead of findAll

--- app/discover/edit/shows/route.js
+++ app/discover/edit/shows/route.js
@@ -7,7 +7,7 @@ export default Ember.Route.extend({
   model() {
     let prefs = this.get('discoverPrefs');
 
-    return this.store.findAll('shows', {
+    return this.store.query('shows', {
       discover_station: ENV.discoverStation,
       api_key: ENV.discoverAPIKey
     }).then((shows) => {
