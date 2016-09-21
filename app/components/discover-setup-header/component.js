b05cb416b4a1ed4aa11c6c412182565458aac2df
Brian Whitton <me@brianwhitton.com>
2016-09-21 00:29:40 -0400

    adds spinner to initial flow steps 1 & 2

--- app/components/discover-setup-header/component.js
+++ app/components/discover-setup-header/component.js
@@ -4,9 +4,11 @@ export default Ember.Component.extend({
   classNames: ['discover-setup-header-container'],
   actions: {
     next() {
+      this.set('isLoadingNext', true);
       this.sendAction('onNext');
     },
     back() {
+      this.set('isLoadingBack', true);
       this.sendAction('onBack');
     }
   }
