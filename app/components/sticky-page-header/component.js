0c5ca989ae3fe97c3d2a166a5943f19c55e8afdc
Brian Whitton <me@brianwhitton.com>
2016-09-23 17:25:23 -0400

    ok. just set the offset from the context
    use the pre-rendered height to determine the spacerheight

--- app/components/sticky-page-header/component.js
+++ app/components/sticky-page-header/component.js
@@ -1,22 +1,20 @@
 import Component from 'ember-component';
 import { htmlSafe } from 'ember-string';
 import computed from 'ember-computed';
-import { schedule } from 'ember-runloop';
 
 export default Component.extend({
-  offset: 0,
   spacerHeight: 0,
   
   spacerStyle: computed('spacerHeight', function() {
     return htmlSafe(`height: ${this.get('spacerHeight')}px;`);
   }),
   
-  didRender() {
+  willUpdate() {
     let height = this.$('.sticky-page-header').height();
     if (!this.get('sticky')) {
       height = 0;
     }
-    schedule('afterRender', this, () => this.set('spacerHeight', height));
+    this.set('spacerHeight', height);
   },
   
   actions: {
