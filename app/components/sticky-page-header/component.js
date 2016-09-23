b69075c7716b3958115492b2ead4bff39e57b360
Brian Whitton <me@brianwhitton.com>
2016-09-23 12:41:12 -0400

    ok, don't use magic numbers
    this lets the test pass and makes it more reusable
    * update spacerHeight afterRender
    * don't need an offset

--- app/components/sticky-page-header/component.js
+++ app/components/sticky-page-header/component.js
@@ -1,24 +1,22 @@
 import Component from 'ember-component';
 import { htmlSafe } from 'ember-string';
 import computed from 'ember-computed';
-
+import { schedule } from 'ember-runloop';
 
 export default Component.extend({
+  offset: 0,
   spacerHeight: 0,
   
   spacerStyle: computed('spacerHeight', function() {
     return htmlSafe(`height: ${this.get('spacerHeight')}px;`);
   }),
-  offset: computed('sticky', function() {
-    return this.get('sticky') ? -100 : -285;
-  }),
   
-  willUpdate() {
-    let height = this.$('.sticky-page-header').height() + -this.get('offset');
+  didRender() {
+    let height = this.$('.sticky-page-header').height();
     if (!this.get('sticky')) {
       height = 0;
     }
-    this.set('spacerHeight', height);
+    schedule('afterRender', this, () => this.set('spacerHeight', height));
   },
   
   actions: {
