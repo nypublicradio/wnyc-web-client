66c417b250b280ad06046891ecf39a4729e85760
walsh9 <walsh9@gmail.com>
2016-10-17 17:06:38 -0400

    These loading templates had titles too

---
+++ app/discover/edit/shows-loading/route.js
@@ -0,0 +1,5 @@
+import Route from 'ember-route';
+
+export default Route.extend({
+  titleToken: 'Loading Shows...',
+});
---
+++ app/discover/index-loading/route.js
@@ -0,0 +1,5 @@
+import Route from 'ember-route';
+
+export default Route.extend({
+  titleToken: 'Loading Your Discover Playlist',
+});
