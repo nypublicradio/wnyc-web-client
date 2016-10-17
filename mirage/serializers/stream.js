3ec3fefeeb144e3bc64d6cb56be3a737c6149052
Brian Whitton <me@brianwhitton.com>
2016-10-17 14:00:59 -0400

    updates mirage serializer for streams to better handle request params

--- mirage/serializers/stream.js
+++ mirage/serializers/stream.js
@@ -1,14 +1,15 @@
 import { Serializer } from 'ember-cli-mirage';
 
 export default Serializer.extend({
-  serialize() {
+  serialize(streams, {params}) {
     let json = Serializer.prototype.serialize.apply(this, arguments);
-    if (json.streams.length === 1) {
+    if (params.slug) {
       return json.streams[0];
+    } else {
+      return {
+        results: json.streams,
+        count: json.streams.length
+      };
     }
-    return {
-      results: json.streams,
-      count: json.streams.length
-    };
   }
 });
