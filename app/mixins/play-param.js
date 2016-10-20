2dc3e400354b62a1691f63920dd7e0c8b6cde9e1
Brian Whitton <me@brianwhitton.com>
2016-10-20 18:53:13 -0400

    updates to use ember-query-method :)

--- app/mixins/play-param.js
+++ app/mixins/play-param.js
@@ -1,26 +1,6 @@
-import Ember from 'ember';
-import service from 'ember-service/inject';
+import QueryMethodMixin from 'ember-query-method/mixins/query-method';
 
-export default Ember.Mixin.create({
-  audio: service(),
-  
-  // check return value of super objects and respect if they don't
-  //  want the event to bubble
-  actions: {
-    didTransition() {
-      let play = this.controllerFor('application').get('play');
-      if (play) {
-        this.get('audio').play(play);
-      }
-      
-      let ret = this._super(...arguments);
-      return ret === false ? ret : true;
-    },
-    willTransition() {
-      this.controllerFor('application').set('play', null);
-      
-      let ret = this._super(...arguments);
-      return ret === false ? ret : true;
-    }
-  }
+export default QueryMethodMixin.reopen({
+  factory: 'service:audio',
+  method: 'play'
 });
