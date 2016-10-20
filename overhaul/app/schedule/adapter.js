f8b793c588cc524c6d98027703f3802d7e91747d
Michael Hearn <git@mikehearn.net>
2016-10-20 18:52:36 -0400

    Trailing slash on API to avoid CORS error

--- overhaul/app/schedule/adapter.js
+++ overhaul/app/schedule/adapter.js
@@ -4,5 +4,9 @@ export default ApplicationAdapter.extend({
   pathForType: function(type) {
     return type;
   },
+  buildURL() {
+    let url = this._super(...arguments);
+    return url.replace(/([^\/])$/, '$1/');
+  },
 });
 
