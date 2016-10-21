f6f7c1f9478e662f5668a5d000485c525d8f6058
Michael Hearn <git@mikehearn.net>
2016-10-21 13:05:13 -0400

    Update redirect code to always hit wnyc-fm939

--- overhaul/app/routes/schedule-redirect.js
+++ overhaul/app/routes/schedule-redirect.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Route.extend({
-  redirect(model, { targetName, queryParams }) {
+  redirect() {
     const year = moment().tz('America/New_York').format('YYYY');
     const month = moment().tz('America/New_York').format('MMM').toLowerCase();
     const day = moment().tz('America/New_York').format('DD');
     this.replaceWith('schedule', year, month, day, {
       queryParams: {
-        scheduleStation: queryParams.scheduleStation || 'wnyc-fm939'
+        scheduleStation: 'wnyc-fm939',
       },
     });
   },
