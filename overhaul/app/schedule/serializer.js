c169e307ce483a769cf42662eaec9667bebdeb95
Michael Hearn <git@mikehearn.net>
2016-10-25 17:46:20 -0400

    Minor code removal, refactor

--- overhaul/app/schedule/serializer.js
+++ overhaul/app/schedule/serializer.js
@@ -1,6 +1,5 @@
-import Ember from 'ember';
 import JSONAPISerializer from 'ember-data/serializers/json-api';
 
 export default JSONAPISerializer.extend({
-  keyForAttribute: key => Ember.String.camelize(key),
+  keyForAttribute: key => key.camelize(),
 });
