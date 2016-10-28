de32ed584d8f257fe1d5508714cac15e4d8a226d
walsh9 <walsh9@gmail.com>
2016-10-28 17:52:54 -0400

    Add support notices component

---
+++ overhaul/app/components/support-notices/component.js
@@ -0,0 +1,18 @@
+import Component from 'ember-component';
+import { or, reads } from 'ember-computed';
+import bowser from 'ember-bowser';
+import moment from 'moment';
+
+export default Component.extend({
+  unsupportedBrowser: or('old.{chrome,firefox,ie,safari,mobileSafari}'),
+  unsupportedMobileBrowser: reads('old.mobileSafari'),
+  old: {
+    chrome:    bowser.chrome   && bowser.version < 15,
+    firefox:   bowser.firefox  && bowser.version < 3.6,
+    ie:        bowser.msie     && bowser.version < 11,
+    safari:    bowser.safari   && bowser.version < 6.9,
+    mobileSafari: bowser.ios && bowser.safari && bowser.osversion < 9,
+  },
+  tomorrow: moment().add(1, 'days').toDate(),
+  nextWeek: moment().add(7, 'days').toDate(),
+});
