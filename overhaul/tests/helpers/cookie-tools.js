4c3a147046f17aabd3027cc34bf704321aaa5ef1
walsh9 <walsh9@gmail.com>
2016-11-02 11:34:01 -0400

    Update tests

---
+++ overhaul/tests/helpers/cookie-tools.js
@@ -0,0 +1,19 @@
+export function deleteCookie(name) {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
+
+export function readCookies() {
+  let cookies = {};
+  document.cookie.split(';').forEach(function(cookie) {
+    let key = cookie.split('=')[0].replace(/^ +/, "");
+    let value = cookie.split('=')[1];
+    cookies[key] = value;
+  });
+  return cookies;
+};
+
+export function clearCookies() {
+  Object.keys(readCookies()).forEach(function(key) {
+    deleteCookie(key);
+  });
+};
