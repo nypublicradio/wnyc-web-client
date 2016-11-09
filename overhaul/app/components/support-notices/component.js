0b7116bd5acbbfbb1ba8209b1329ce96aaa93866
walsh9 <walsh9@gmail.com>
2016-11-09 13:47:57 -0500

    Make version checks work

--- overhaul/app/components/support-notices/component.js
+++ overhaul/app/components/support-notices/component.js
@@ -4,14 +4,14 @@ import bowser from 'ember-bowser';
 import moment from 'moment';
 
 export default Component.extend({
-  unsupportedBrowser: or('old.{chrome,firefox,ie,safari,mobileSafari}'),
-  unsupportedMobileBrowser: reads('old.mobileSafari'),
+  unsupportedBrowser: or('old.{chrome,firefox,ie,safari,ios}'),
+  unsupportedMobileBrowser: reads('old.ios'),
   old: {
-    chrome:    bowser.chrome   && bowser.version < 15,
-    firefox:   bowser.firefox  && bowser.version < 3.6,
-    ie:        bowser.msie     && bowser.version < 11,
-    safari:    bowser.safari   && bowser.version < 6.9,
-    mobileSafari: bowser.ios && bowser.safari && bowser.osversion < 9,
+    chrome:    bowser.chrome   && !bowser.check({chrome: "15"}),
+    firefox:   bowser.firefox  && !bowser.check({firefox: "3.6"}),
+    ie:        bowser.msie     && !bowser.check({msie: "11"}),
+    safari:    bowser.safari   && !bowser.check({safari: "6.9"}),
+    ios:       bowser.ios      && !bowser.check({osversion: "9"}),
   },
   tomorrow: moment().add(1, 'days').toDate(),
   twoWeeks: moment().add(14, 'days').toDate(),
