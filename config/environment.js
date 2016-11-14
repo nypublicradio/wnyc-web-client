504e81335e5776b39aff66d8c1fb7f1fd3a91383
Brian Whitton <me@brianwhitton.com>
2016-11-14 12:02:01 -0500

    upgrade compat
    don't need ember qunit notifications
    don't extend Date
    root root testem.js

--- config/environment.js
+++ config/environment.js
@@ -26,6 +26,9 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        Date: false
       }
     },
 
