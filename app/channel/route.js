6c14cd24b549f62820df7678ea8094e469cf13a4
Brian Whitton <me@brianwhitton.com>
2016-11-17 12:09:59 -0500

    adds datapipeline item view reporting to channel route

--- app/channel/route.js
+++ app/channel/route.js
@@ -13,8 +13,9 @@ import PlayParamMixin from 'wnyc-web-client/mixins/play-param';
 import config from 'wnyc-web-client/config/environment';
 
 export default Route.extend(PlayParamMixin, {
-  session: service(),
-  metrics: service(),
+  session:      service(),
+  metrics:      service(),
+  dataPipeline: service(),
 
   model(params) {
     const channelType = this.routeName;
@@ -31,10 +32,11 @@ export default Route.extend(PlayParamMixin, {
     .catch(e => retryFromServer(e, listingSlug.replace(/\/*$/, '/')));
   },
   afterModel({ channel }, transition) {
-    const channelTitle = get(channel, 'title');
-    const metrics = get(this, 'metrics');
-    const nprVals = get(channel, 'nprAnalyticsDimensions');
-
+    let channelTitle = get(channel, 'title');
+    let metrics = get(this, 'metrics');
+    let dataPipeline = get(this, 'dataPipeline');
+    let nprVals = get(channel, 'nprAnalyticsDimensions');
+    
     if (channel.get('headerDonateChunk')) {
       transition.send('updateDonateChunk', channel.get('headerDonateChunk'));
     }
@@ -42,18 +44,25 @@ export default Route.extend(PlayParamMixin, {
       transition.send('setMiniChrome', true);
     }
 
+    // google analytics
     metrics.trackEvent({
       category: `Viewed ${get(channel, 'listingObjectType').capitalize()}`,
       action: channelTitle,
-      id: channel.get('cmsPK'),
-      type: channel.get('listingObjectType')
     });
 
+    // NPR
     metrics.invoke('trackPage', 'NprAnalytics', {
       page: `/${get(this, 'listingSlug')}`,
       title: channelTitle,
       nprVals,
       isNpr: true
+    });
+    
+    // data pipeline
+    dataPipeline.reportItemView({
+      cms_id: channel.get('cmsPK'),
+      item_type: channel.get('listingObjectType'),
+      site_id: channel.get('siteId')
     });
   },
   setupController(controller, model) {
