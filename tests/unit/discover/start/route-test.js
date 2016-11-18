0e689f2e735c97d3c35ebf2d7a5f790e8f40b4fd
Brian Whitton <me@brianwhitton.com>
2016-11-18 18:14:30 -0500

    adds data pipeline to needs in failing unit tests

--- tests/unit/discover/start/route-test.js
+++ tests/unit/discover/start/route-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:discover/start', 'Unit | Route | discover/start', {
   // Specify the other units that are required for this test.
-  needs: ['service:metrics', 'service:session']
+  needs: ['service:metrics', 'service:session', 'service:data-pipeline']
 });
 
 test('it exists', function(assert) {
--- tests/unit/routes/profile-test.js
+++ tests/unit/routes/profile-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:profile', 'Unit | Route | profile', {
   // Specify the other units that are required for this test.
-  needs: ['service:metrics', 'service:session']
+  needs: ['service:metrics', 'service:session', 'service:data-pipeline']
 });
 
 test('it exists', function(assert) {
