2b37d7d2bbf70a25b9ce966187f04ebb244127c0
Brian Whitton <me@brianwhitton.com>
2016-11-18 09:52:38 -0500

    unit tests need data-pipeline service injected

--- tests/unit/discover/edit/shows/route-test.js
+++ tests/unit/discover/edit/shows/route-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:discover/edit/shows', 'Unit | Route | discover/edit/shows', {
   // Specify the other units that are required for this test.
-  needs: ['service:metrics', 'service:session']
+  needs: ['service:metrics', 'service:session', 'service:data-pipeline']
 });
 
 test('it exists', function(assert) {
--- tests/unit/shows/controller-test.js
+++ tests/unit/shows/controller-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('controller:shows', 'Unit | Controller | shows', {
   // Specify the other units that are required for this test.
-  needs: ['service:metrics', 'service:session']
+  needs: ['service:metrics', 'service:session', 'service:data-pipeline']
 });
 
 // Replace this with your real tests.
