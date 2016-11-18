738c3fa9ace1b8b47dbd51c73258ac3c6989521c
Brian Whitton <me@brianwhitton.com>
2016-11-18 18:17:49 -0500

    updates audio service to use data pipeline to send listen actions

--- app/services/audio.js
+++ app/services/audio.js
@@ -7,10 +7,10 @@ import { bind } from 'ember-runloop';
 import RSVP from 'rsvp';
 import { classify as upperCamelize } from 'ember-string';
 import Ember from 'ember';
+import config from 'overhaul/config/environment';
 
 const FIFTEEN_SECONDS = 1000 * 15;
 const TWO_MINUTES     = 1000 * 60 * 2;
-const PLATFORM        = 'NYPR_Web';
 
 export default Service.extend({
   poll:             service(),
@@ -21,7 +21,7 @@ export default Service.extend({
   bumperState:      service(),
   listens:          service('listen-history'),
   queue:            service('listen-queue'),
-  listenActions:    service(),
+  dataPipeline:      service(),
 
   hifi:             service(),
   isReady:          readOnly('hifi.isReady'),
@@ -108,6 +108,8 @@ export default Service.extend({
     return this.get('hifi').play(urlPromise).then(({sound, failures}) => {
       if (newStoryPlaying) {
         this._trackOnDemandPlay(story, context);
+      } else {
+        this._trackResume(story);
       }
 
       // independent of context, if this item is already the first item in your
@@ -338,16 +340,48 @@ export default Service.extend({
     this.get('listens').addListen(story);
   },
 
-  sendCompleteListenAction(pk) {
-    this.get('listenActions').sendComplete(pk, PLATFORM);
+  sendCompleteListenAction({id, itemType, audioType}) {
+    let data = {
+      audio_type: audioType,
+      cms_id: id,
+      site_id: config.siteId,
+      item_type: itemType,
+      current_position: this.get('position')
+    };
+    this.get('dataPipeline').reportListenAction('finish', data);
   },
 
-  sendPlayListenAction(pk) {
-    this.get('listenActions').sendPlay(pk, PLATFORM);
+  sendPlayListenAction({id, itemType, audioType}) {
+    let data = {
+      audio_type: audioType,
+      cms_id: id,
+      site_id: config.siteId,
+      item_type: itemType,
+      current_position: this.get('position')
+    };
+    this.get('dataPipeline').reportListenAction('start', data);
+  },
+  
+  sendResumeListenAction({id, itemType, audioType}) {
+    let data = {
+      audio_type: audioType,
+      cms_id: id,
+      site_id: config.siteId,
+      item_type: itemType,
+      current_position: this.get('position')
+    };
+    this.get('dataPipeline').reportListenAction('resume', data);
   },
 
-  sendPauseListenAction(pk) {
-    this.get('listenActions').sendPause(pk, PLATFORM, get(this, 'position'));
+  sendPauseListenAction({id, itemType, audioType}) {
+    let data = {
+      audio_type: audioType,
+      cms_id: id,
+      site_id: config.siteId,
+      item_type: itemType,
+      current_position: this.get('position')
+    };
+    this.get('dataPipeline').reportListenAction('pause', data);
   },
   
   _trackPlayerEvent(options) {
@@ -420,7 +454,7 @@ export default Service.extend({
       action: 'On_demand_audio_play',
       label: get(story, 'audio')
     });
-    this.sendPlayListenAction(get(story, 'id'));
+    this.sendPlayListenAction(story);
 
     if (context === 'queue' || context === 'history') {
       this._trackPlayerEvent({
@@ -471,6 +505,10 @@ export default Service.extend({
       label: 'Audio Bumper',
     });
   },
+  
+  _trackResume(audio) {
+    this.sendResumeListenAction(audio);
+  },
 
   _trackAutoplayQueue() {
     this._trackPlayerEvent({
@@ -512,7 +550,7 @@ export default Service.extend({
 
     if (audio && get(audio, 'audioType') !== 'stream') {
       // we're not set up to handle pause listen actions from streams atm
-      this.sendPauseListenAction(audio.id);
+      this.sendPauseListenAction(audio);
     }
   },
 
@@ -524,7 +562,7 @@ export default Service.extend({
       region: upperCamelize(context),
     });
 
-    this.sendCompleteListenAction(story.id);
+    this.sendCompleteListenAction(story);
   },
   
   // TODO: would like to move this and the rest of the above
