a19528f9e107571d4a01a2c1327abde9aed8ef95
walsh9 <walsh9@gmail.com>
2016-11-21 11:21:33 -0500

    Add login and signup forms

--- app/routes/application.js
+++ app/routes/application.js
@@ -58,6 +58,12 @@ export default Route.extend(ApplicationRouteMixin, {
     },
     setMiniChrome(val) {
       this.controllerFor('application').set('miniChrome', val);
+    },
+    disableChrome() {
+      this.controllerFor('application').set('chromeDisabled', true);
+    },
+    enableChrome() {
+      this.controllerFor('application').set('chromeDisabled', false);
     }
   },
 
