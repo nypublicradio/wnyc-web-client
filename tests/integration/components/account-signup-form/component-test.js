d5a0d139b8867257e67eeb61a340ddc4b221138a
walsh9 <walsh9@gmail.com>
2016-11-28 16:14:28 -0500

    update

--- tests/integration/components/account-signup-form/component-test.js
+++ tests/integration/components/account-signup-form/component-test.js
@@ -6,19 +6,6 @@ moduleForComponent('account-signup-form', 'Integration | Component | account sig
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{account-signup-form}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#account-signup-form}}
-      template block text
-    {{/account-signup-form}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.render(hbs`{{account-login-form}}`);
+  assert.equal(this.$('.account-form').length(), 1);
 });
