ddb815c94ac8b896385b09ad72fd30dc5b9d17b2
walsh9 <walsh9@gmail.com>
2016-11-28 16:14:28 -0500

    update

--- tests/integration/components/account-login-form/component-test.js
+++ tests/integration/components/account-login-form/component-test.js
@@ -6,19 +6,6 @@ moduleForComponent('account-login-form', 'Integration | Component | account logi
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{account-login-form}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#account-login-form}}
-      template block text
-    {{/account-login-form}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.account-form').length(), 1);
 });
