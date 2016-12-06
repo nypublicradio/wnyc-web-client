800db340e55e979dd9796ca5c1e3c936a815c1a2
walsh9 <walsh9@gmail.com>
2016-12-06 18:10:17 -0500

    Make user navigation tests pass

--- app/components/user-navigation/component.js
+++ app/components/user-navigation/component.js
@@ -1,12 +1,13 @@
 import Component from 'ember-component';
-import set from 'ember-metal/set';
 
 export default Component.extend({
   isPopupOpen: false,
   classNameBindings: ['isPopupOpen'],
   actions: {
     closePopup() {
-      set(this, 'isPopupOpen', false);
+      if ( !(this.get('isDestroyed') || this.get('isDestroying')) ) {
+        this.set('isPopupOpen', false);
+      }
     },
     togglePopup() {
       this.toggleProperty('isPopupOpen');
