f27709a9c78dc526f2e0ec473d3ac25538dad98f
walsh9 <walsh9@gmail.com>
2016-12-09 15:47:25 -0500

    Getting closer to working signup

--- app/authenticators/nypr.js
+++ app/authenticators/nypr.js
@@ -1,7 +1,6 @@
 import config from 'overhaul/config/environment';
 import OAuth2PasswordGrantAuthenticator from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
-
 export default OAuth2PasswordGrantAuthenticator.extend({
   serverTokenEndpoint: `${config.wnycAuthAPI}/session`
 });
