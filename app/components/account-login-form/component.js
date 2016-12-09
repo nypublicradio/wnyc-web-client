485a5b335ebeaf6ea27968632c964b6e8372216d
walsh9 <walsh9@gmail.com>
2016-12-09 15:47:25 -0500

    Getting closer to working signup

--- app/components/account-login-form/component.js
+++ app/components/account-login-form/component.js
@@ -1,23 +1,47 @@
 import Component from 'ember-component';
 import set from 'ember-metal/set';
+import get from 'ember-metal/get';
 import Changeset from 'ember-changeset';
 import LoginValidations from 'overhaul/validations/login';
 import lookupValidator from 'ember-changeset-validations';
+import service from 'ember-service/inject';
 
 export default Component.extend({
+  session: service(),
   isProcessing: false,
   changeset: null,
   init() {
     this._super(...arguments);
-    let fields = {
+    set(this, 'fields', {
       email: '',
       password: ''
-    };
-    set(this, 'changeset', new Changeset(fields, lookupValidator(LoginValidations), LoginValidations));
+    });
+    set(this, 'changeset', new Changeset(get(this, 'fields'), lookupValidator(LoginValidations), LoginValidations));
   },
   actions: {
     login() {
-
+      let changeset = get(this, 'changeset');
+      let fields = get(this, 'fields');
+      let snapshot = changeset.snapshot();
+      return changeset
+      .cast(Object.keys(fields))
+      .validate()
+      .then(() => {
+        set(this, 'isProcessing', true);
+        if (get(changeset, 'isValid')) {
+          console.log('ATTEMPTING AUTHENTICATION', fields, changeset.get('errors'), changeset);
+          changeset.execute();
+          return get(this, 'session').authenticate('authenticator:nypr', fields.email, fields.password);
+        }
+        changeset.restore(snapshot);
+        set(this, 'isProcessing', false);
+        console.log('VALIDATION FAILED', fields, changeset.get('errors'), changeset);
+        return false;
+      }).catch((e) => {
+        set(this, 'isProcessing', false);
+        console.log('LOGIN FAILED', fields);
+        console.log('what went wrong:', e);
+      });
     },
   }
 });
--- app/components/account-signup-form/component.js
+++ app/components/account-signup-form/component.js
@@ -4,8 +4,10 @@ import set from 'ember-metal/set';
 import Changeset from 'ember-changeset';
 import SignupValidations from 'overhaul/validations/signup';
 import lookupValidator from 'ember-changeset-validations';
+import service from 'ember-service/inject';
 
 export default Component.extend({
+  store: service(),
   isProcessing: false,
   changeset: null,
   init() {
@@ -22,22 +24,46 @@ export default Component.extend({
     signUp() {
       let changeset = get(this, 'changeset');
       let snapshot = changeset.snapshot();
+      let fields = get(this, 'fields');
+      let newUser;
       return changeset
-      .cast(Object.keys(get(this, 'fields')))
+      .cast(Object.keys(fields))
       .validate()
       .then(() => {
         if (get(changeset, 'isValid')) {
           changeset.save()
           .then(() => {
-            console.log('SIGNING UP WITH', get(this, 'fields'));
-            set(this, 'emailSent', true);
+            console.log('SIGNING UP WITH', fields);
+            set(this, 'isProcessing', true);
+            newUser = this.get('store').createRecord('user', {
+              email: fields.email,
+              firstName: fields.firstName,
+              lastName: fields.lastName
+            });
+            // typedPassword will be available to send to our new user api endpoint but won't be persisted to the store
+            newUser.set('typedPassword', fields.password);
+            newUser.save().then(function() {
+
+            }).catch(e => {
+              set(this, 'isProcessing', false);
+              set(this, 'loginFailed', true);
+              console.log('LOGIN FAILED', fields, changeset.get('errors'), changeset);
+              console.log('what went wrong', e);
+              console.log('model errors', newUser.get('errors'));
+            });
           });
         }
+        set(this, 'isProcessing', false);
+        set(this, 'loginFailed', true);
+        changeset.restore(snapshot);
+        console.log('LOGIN FAILED', fields, changeset.get('errors'), changeset);
       }).catch(() => {
-        console.log('UPDATE FAILED', get(this, 'fields'), changeset.get('errors'), changeset);
+        set(this, 'isProcessing', false);
+        set(this, 'loginFailed', true);
+        console.log('LOGIN FAILED', fields, changeset.get('errors'), changeset);
         changeset.restore(snapshot);
       });
-    },
+    }
   }
 
 });
