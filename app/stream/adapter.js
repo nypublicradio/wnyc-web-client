eb2da3a0eff027bd4e37f648279b17f26ce6d001
Brian Whitton <me@brianwhitton.com>
2016-12-11 21:45:19 -0500

    updates other adapters for auth compat
    adds data dapter mixin and call super in any ajax options to include the
    additions from ESA

--- app/stream/adapter.js
+++ app/stream/adapter.js
@@ -1,25 +1,35 @@
 import config from 'wnyc-web-client/config/environment';
 import DS from 'ember-data';
 import Ember from 'ember';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 const { hash } = Ember.RSVP;
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:nypr',
   host: config.environment === 'development' ? '' : config.wnycAPI, // use proxy to wnyc
   namespace: 'api/v1',
   findAll() {
     let streams, whatsOn;
     let streamUrl = [this.host, this.namespace, 'list/streams'].join('/') + '/';
     let whatsOnUrl = [this.host, this.namespace, 'whats_on'].join('/') + '/';
-    streams = Ember.$.get(streamUrl);
-    whatsOn = Ember.$.get(whatsOnUrl);
+    
+    let options = this.ajaxOptions(streamUrl, 'GET', {});
+    streams = Ember.$.get(options);
+    
+    options = this.ajaxOptions(whatsOnUrl, 'GET', {});
+    whatsOn = Ember.$.get(options);
     return hash({streams, whatsOn});
   },
   findRecord(store, type, id/*, snapshot*/) {
     let stream, whatsOn;
     let streamUrl = [this.host, this.namespace, 'list/streams', id].join('/') + '/';
     let whatsOnUrl = [this.host, this.namespace, 'whats_on', id].join('/') + '/';
-    stream = Ember.$.get(streamUrl);
-    whatsOn = Ember.$.get(whatsOnUrl);
+    
+    let options = this.ajaxOptions(streamUrl, 'GET', {});
+    stream = Ember.$.get(options);
+    
+    options = this.ajaxOptions(whatsOnUrl, 'GET', {});
+    whatsOn = Ember.$.get(options);
     return hash({stream, whatsOn});
   }
 });
