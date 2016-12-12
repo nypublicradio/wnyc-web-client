31b2267d64b786db3179c3fe45d63550ec3d19a7
Brian Whitton <me@brianwhitton.com>
2016-12-11 21:41:43 -0500

    adds data dapter mixin to user adapter
    will decorate outgoing requests with the auth token
    
    (cherry picked from commit 83c10a7238fffd2f210002fd1123436f6c6c49e5)

--- app/user/adapter.js
+++ app/user/adapter.js
@@ -1,7 +1,9 @@
 import ENV from '../config/environment';
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.RESTAdapter.extend({
+export default DS.RESTAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:nypr',
   host: ENV.wnycAuthAPI,
   // pathForType: () => 'user',
   buildURL() {
