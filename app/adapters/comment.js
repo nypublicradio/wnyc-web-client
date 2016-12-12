88ac8f8fe23ae21ad3773a57a2487e4c9a937771
Brian Whitton <me@brianwhitton.com>
2016-12-11 21:45:19 -0500

    updates other adapters for auth compat
    adds data dapter mixin and call super in any ajax options to include the
    additions from ESA

--- app/adapters/comment.js
+++ app/adapters/comment.js
@@ -2,17 +2,21 @@ import ENV from '../config/environment';
 import Ember from 'ember';
 import DS from 'ember-data';
 import fetch from 'fetch';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:nypr',
   host: ENV.wnycAPI,
   namespace: `api/v1/list/comments`,
+  
   query(store, type, query) {
     let url = [this.host, this.namespace, query.itemTypeId, query.itemId, ''].join('/');
+    let options = this.ajaxOptions(url, 'GET', {});
     if (!Ember.testing) {
-      return fetch(url).then(response => response.json());
+      return fetch(options).then(response => response.json());
     } else {
       // Pretender.js only intercepts XML requests, not JSONP or native Fetch
-      return Ember.$.ajax(url).then(d => d);
+      return Ember.$.ajax(options).then(d => d);
     }
   }
 });
--- app/discover/topics/adapter.js
+++ app/discover/topics/adapter.js
@@ -1,7 +1,9 @@
 import ENV from '../../config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import DS from 'ember-data';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:nypr',
   host: ENV.wnycAPI,
   namespace: `api/v1/discover/topics/`,
   buildURL() {
