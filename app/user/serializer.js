adbc52bca36b1d66a1929da201b1eb9921da9805
Brian Whitton <me@brianwhitton.com>
2016-12-12 18:13:27 -0500

    logic to pull out the `attributes` key on outgoing user payloads
    implements description from previous commit

--- app/user/serializer.js
+++ app/user/serializer.js
@@ -3,11 +3,15 @@ import DS from 'ember-data';
 export default DS.JSONAPISerializer.extend({
   keyForAttribute: attr => attr.underscore(),
   serialize(snapshot, options) {
-    var json = this._super(snapshot, options);
-    json = Object.assign(json, { password: snapshot.record.get('typedPassword') });
+    let { data : { attributes:json } } = this._super(snapshot, options);
+    let typedPassword = snapshot.record.get('typedPassword');
+    
+    if (typedPassword) {
+      json = Object.assign(json, { password: snapshot.record.get('typedPassword') });
+    }
     return json;
   },
   serializeIntoHash(hash, typeClass, snapshot, options) {
     Object.assign(hash, this.serialize(snapshot, options));
-  }
+  },
 });
