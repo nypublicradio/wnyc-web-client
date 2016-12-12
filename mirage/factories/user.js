fb760ab261c94308cc98c7a51bbd8412ed985988
Brian Whitton <me@brianwhitton.com>
2016-12-12 18:11:39 -0500

    update user model with cognito attrs
    the auth service will return User models in JSONAPI format, but for now
    only accepts plain JSON objects. the serializer will strip the outgoing
    JSONAPI formatting and only send along the `attributes` key.
    
    this updates the serializer to transform the attribute key to underscore
    from camelcase when the keys are going out to the network.

--- mirage/factories/user.js
+++ mirage/factories/user.js
@@ -4,5 +4,6 @@ export default Factory.extend({
   username: faker.internet.domainWord,
   adminURL: "/admin",
   email: faker.internet.email,
-  name: faker.findName,
+  name: faker.name.firstName,
+  family_name: faker.name.lastName
 });
