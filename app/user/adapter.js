f5e6e8ba2cd7a0b8ed20f1b24585e2167246c259
Brian Whitton <me@brianwhitton.com>
2016-12-12 18:14:14 -0500

    make user adapter a JSONAPI adapter

--- app/user/adapter.js
+++ app/user/adapter.js
@@ -2,7 +2,7 @@ import ENV from '../config/environment';
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.RESTAdapter.extend(DataAdapterMixin, {
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:nypr',
   host: ENV.wnycAuthAPI,
   // pathForType: () => 'user',
