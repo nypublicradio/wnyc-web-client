9298d1d4387810196863c48d32a7d16d963ed164
Brian Whitton <me@brianwhitton.com>
2016-12-11 21:47:43 -0500

    whitespace
    (cherry picked from commit 3755a7a7ced45f09fd9c0c9334a81e8e760611de)

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,11 +4,11 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-    authorizer: 'authorizer:nypr',
-    host: ENV.wnycAPI,
-    namespace: 'api/v3',
-    // ember 2.0 deprecation
-    shouldBackgroundReloadRecord() {
-      return false;
-    }
+  authorizer: 'authorizer:nypr',
+  host: ENV.wnycAPI,
+  namespace: 'api/v3',
+  // ember 2.0 deprecation
+  shouldBackgroundReloadRecord() {
+    return false;
+  }
 });
