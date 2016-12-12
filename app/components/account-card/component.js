01d7d005fecf125fb2ea3464299e878ebed17c65
Brian Whitton <me@brianwhitton.com>
2016-12-12 16:37:43 -0500

    add accounts pod and account-card component w/tests

---
+++ app/components/account-card/component.js
@@ -0,0 +1,7 @@
+import Component from 'ember-component';
+// import BasicInfoValidations from 'nypr-account-settings/validators/basic-info';
+// import PasswordValidations from 'nypr-account-settings/validators/password';
+
+export default Component.extend({
+  password: {}
+});
