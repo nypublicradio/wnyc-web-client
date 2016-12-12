e4ec2821b4f50297ff5f305f36645997c299ce90
Brian Whitton <me@brianwhitton.com>
2016-12-12 18:11:39 -0500

    update user model with cognito attrs
    the auth service will return User models in JSONAPI format, but for now
    only accepts plain JSON objects. the serializer will strip the outgoing
    JSONAPI formatting and only send along the `attributes` key.
    
    this updates the serializer to transform the attribute key to underscore
    from camelcase when the keys are going out to the network.

--- app/user/model.js
+++ app/user/model.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string')
+  name: DS.attr('string'),
+  familyName: DS.attr('string'),
+  username: DS.attr('string')
 });
