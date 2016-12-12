cb3c8b4fee4228d88c2c6ca27091ea3a634aed8e
Brian Whitton <me@brianwhitton.com>
2016-12-12 18:02:54 -0500

    add nypr authorizer and mixes it into application adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,10 @@
 import DS from 'ember-data';
 import ENV from '../config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
+
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+    authorizer: 'authorizer:nypr',
     host: ENV.wnycAPI,
     namespace: 'api/v3',
     // ember 2.0 deprecation
