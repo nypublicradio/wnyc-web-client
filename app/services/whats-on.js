88e6323ed4f8e8392a95289efe391e232450b160
Brian Whitton <me@brianwhitton.com>
2016-12-12 16:33:44 -0500

    update adpaters to use Ember.$.ajax for XHRs
    due to using DataAdapterMixin, which adds auth headers in the
    ajax.beforeSend hook. if the request is called using fetch, that hook
    won't run.
    
    after updating adapters to use $.ajax, it turned out that jQuery's
    resolution wasn't a true promise, which caused acceptance tests to fail.
    
    This patch adds a `wrap-ajax` module to manually resolve the $.ajax
    calls.
    
    We could update fetch calls to use ESA's `authorize` call to add the
    headers manually. Maybe we should. this works for now.

--- app/services/whats-on.js
+++ app/services/whats-on.js
@@ -1,5 +1,5 @@
 import ENV from 'overhaul/config/environment';
-import $ from 'jquery';
+import wrapAjax from 'overhaul/lib/wrap-ajax';
 import Service from 'ember-service';
 import get from 'ember-metal/get';
 import { canonicalize } from 'overhaul/services/script-loader';
@@ -13,7 +13,7 @@ export default Service.extend({
     let endPoint = get(this, 'endPoint');
     let url = `${wnycURL}${endPoint}`;
 
-    return $.ajax(url).then(d => this._extractStatus(d, pk));
+    return wrapAjax(url).then(d => this._extractStatus(d, pk));
   },
 
   _extractStatus(data, pk) {
