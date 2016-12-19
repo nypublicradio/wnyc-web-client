26b97d1e59599699b411fe9ef73bb80d6fb01c0f
walsh9 <walsh9@gmail.com>
2016-12-19 12:24:17 -0500

    Add form component, forgot, reset password forms, tests

--- tests/integration/components/account-signup-form/component-test.js
+++ tests/integration/components/account-signup-form/component-test.js
@@ -9,3 +9,37 @@ test('it renders', function(assert) {
   this.render(hbs`{{account-login-form}}`);
   assert.equal(this.$('.account-form').length, 1);
 });
+
+test('submitting the form tries to save values on a new user model', function(assert) {
+  let save = sinon.stub().returns(Promise.reject({}));
+  let fakeUser = {save};
+  let createRecord = sinon.stub().returns(fakeUser);
+  let store = {createRecord};
+  this.set('store', store);
+  this.render(hbs`{{account-signup-form store=store}}`);
+
+  let testFirstName = 'Test';
+  let testLastName = 'User';
+  let testEmail = 'test@email.com';
+  let testPassword = 'password123';
+
+  this.$('label:contains(First Name) + input').val(testFirstName);
+  this.$('label:contains(First Name) + input').change();
+  this.$('label:contains(Last Name) + input').val(testLastName);
+  this.$('label:contains(Last Name) + input').change();
+  this.$('label:contains(Email) + input').val(testEmail);
+  this.$('label:contains(Email) + input').change();
+  this.$('label:contains(Password) + input').val(testPassword);
+  this.$('label:contains(Password) + input').change();
+  this.$('button:contains(Sign up)').click();
+
+  delete fakeUser.save;
+  assert.ok(createRecord.calledOnce);
+  assert.ok(save.calledOnce);
+  assert.deepEqual(fakeUser, {
+    givenName: testFirstName,
+    familyName: testLastName,
+    email: testEmail,
+    typedPassword: testPassword
+  });
+});
