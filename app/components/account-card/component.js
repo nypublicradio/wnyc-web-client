add924e3b5a25f3fd5cee3fd585fd2cea8db2d00
Brian Whitton <me@brianwhitton.com>
2016-12-21 08:14:00 -0500

    delete account-card component
    components from nypr-account-settings are self contained

--- app/components/account-card/component.js
+++
@@ -1,7 +0,0 @@
-import Component from 'ember-component';
-// import BasicInfoValidations from 'nypr-account-settings/validators/basic-info';
-// import PasswordValidations from 'nypr-account-settings/validators/password';
-
-export default Component.extend({
-  password: {}
-});
