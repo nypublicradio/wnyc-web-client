c771292b472e5f4c13b67b3f0b6fa388bb327a2b
Brian Whitton <me@brianwhitton.com>
2016-12-21 08:47:57 -0500

    fills in accounts template
    basic-card and password-card components with contextual actions
    * changePassword
    * authenticate for email updates

--- app/accounts/controller.js
+++ app/accounts/controller.js
@@ -1,7 +1,28 @@
 import Controller from 'ember-controller';
+import service from 'ember-service/inject';
+import config from 'overhaul/config/environment';
 
 export default Controller.extend({
-  isEditing: false,
+  session: service(),
+  
+  authenticate(password) {
+    let email = this.get('model.email');
+    return this.get('session').authenticate('authenticator:nypr', email, password);
+  },
+  
+  changePassword(changeset) {
+    let old_password = changeset.get('currentPassword');
+    let new_password = changeset.get('newPassword');
+    this.get('session').authorize('authorizer:nypr', (header, value) => {
+      fetch(`${config.wnycAuthAPI}/v1/password`, {
+        method: 'POST',
+        headers: {
+          Authorization: value
+        },
+        body: JSON.stringify({old_password, new_password})
+      });
+    });
+  },
   
   actions: {
     edit() {
