1de74c2345e6e98c772a7dca6cf470271784c1bc
Brian Whitton <me@brianwhitton.com>
2016-12-22 09:46:10 -0500

    moves accounts to my-account for demo compat

--- tests/unit/my-account/controller-test.js
+++ tests/unit/my-account/controller-test.js
@@ -1,11 +1,12 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:accounts', 'Unit | Route | accounts', {
+moduleFor('controller:my-account', 'Unit | Controller | my-account', {
   // Specify the other units that are required for this test.
   needs: ['service:metrics', 'service:session']
 });
 
+// Replace this with your real tests.
 test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+  let controller = this.subject();
+  assert.ok(controller);
 });
--- tests/unit/my-account/route-test.js
+++ tests/unit/my-account/route-test.js
@@ -1,12 +1,11 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('controller:accounts', 'Unit | Controller | accounts', {
+moduleFor('route:my-account', 'Unit | Route | my-account', {
   // Specify the other units that are required for this test.
   needs: ['service:metrics', 'service:session']
 });
 
-// Replace this with your real tests.
 test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
+  let route = this.subject();
+  assert.ok(route);
 });
