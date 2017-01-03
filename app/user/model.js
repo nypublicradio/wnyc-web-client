c718da272ecff451810d3e5593f72f5d32a5da98
Brian Whitton <me@brianwhitton.com>
2017-01-03 10:40:32 -0500

    adds isStaff attr to user model

--- app/user/model.js
+++ app/user/model.js
@@ -1,4 +1,7 @@
 import DS from 'ember-data';
+import computed from 'ember-computed';
+import fetch from 'fetch';
+import config from 'overhaul/config/environment';
 const {
   Model,
   attr
@@ -8,5 +11,16 @@ export default Model.extend({
   email:              attr('string'),
   givenName:          attr('string'),
   familyName:         attr('string'),
-  preferredUsername:  attr('string')
+  preferredUsername:  attr('string'),
+  isStaff:            computed(function() {
+    fetch(`${config.wnycAccountRoot}/api/v1/is_logged_in/`, {
+      credentials: 'include'
+    })
+    .then(r => r.json())
+    .then(({is_staff}) => {
+      if (is_staff) {
+        this.set('isStaff', true);
+      }
+    });
+  })
 });
