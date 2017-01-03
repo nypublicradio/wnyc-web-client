3b87bdadd7a738f18771d0cc528d9e82d846ffa7
Brian Whitton <me@brianwhitton.com>
2017-01-03 10:12:11 -0500

    fix auth urls
    use proper path for server token, reset pw, and resend confirm
    
    also remove dupe auth key in config

--- app/authenticators/nypr.js
+++ app/authenticators/nypr.js
@@ -2,5 +2,5 @@ import config from 'overhaul/config/environment';
 import OAuth2PasswordGrantAuthenticator from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrantAuthenticator.extend({
-  serverTokenEndpoint: `${config.wnycAuthAPI}/session`
+  serverTokenEndpoint: `${config.wnycAuthAPI}/v1/session`
 });
