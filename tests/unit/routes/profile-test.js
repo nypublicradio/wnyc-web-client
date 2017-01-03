1f81e51f6f03bedcee478d91c99a6916f472dc1d
Brian Whitton <me@brianwhitton.com>
2017-01-03 13:48:23 -0500

    renames my-account to profile and don't use pods for this one

--- tests/unit/routes/profile-test.js
+++ tests/unit/routes/profile-test.js
@@ -1,12 +1,11 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('controller:my-account', 'Unit | Controller | my-account', {
+moduleFor('route:profile', 'Unit | Route | profile', {
   // Specify the other units that are required for this test.
   needs: ['service:metrics', 'service:session']
 });
 
-// Replace this with your real tests.
 test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
+  let route = this.subject();
+  assert.ok(route);
 });
