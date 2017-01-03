dd8afb61368fb309cd4b40d1460224f8018df0b2
walsh9 <walsh9@gmail.com>
2017-01-03 16:46:25 -0500

    Flatten  routes

--- app/routes/reset.js
+++ app/routes/reset.js
@@ -1,9 +1,6 @@
 import Route from 'ember-route';
 
 export default Route.extend({
-  queryParams: ['email', 'confirmation'],
-  email: null,
-  confirmation: null,
   actions: {
     didTransition() {
       this.send('disableChrome');
