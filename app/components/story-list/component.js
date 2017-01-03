843929470e62d021552064cd7511cc834cc7721c
Brian Whitton <me@brianwhitton.com>
2017-01-03 10:42:13 -0500

    use currentUser as user in story-list component

--- app/components/story-list/component.js
+++ app/components/story-list/component.js
@@ -2,6 +2,6 @@ import Component from 'ember-component';
 import service from 'ember-service/inject';
 
 export default Component.extend({
-  session: service(),
+  currentUser: service(),
   tagName: 'section',
 });
