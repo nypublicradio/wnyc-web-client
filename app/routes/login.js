149829e014c03024982250e09f594eb7af69aa5e
walsh9 <walsh9@gmail.com>
2017-01-04 14:06:25 -0500

    disable chrome in didTransition instead of setupController

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   titleToken: 'Sign In',
-  setupController() {
-    this.send('disableChrome');
-  },
   actions: {
+    didTransition() {
+      this.send('disableChrome');
+    },
     willTransition() {
       this.send('enableChrome');
     }
