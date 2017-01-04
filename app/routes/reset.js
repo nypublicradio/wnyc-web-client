43d050681c3408116b167625ce075c2acd3c3b7d
walsh9 <walsh9@gmail.com>
2017-01-04 10:51:38 -0500

    update reset params

--- app/routes/reset.js
+++ app/routes/reset.js
@@ -1,6 +1,9 @@
 import Route from 'ember-route';
 
 export default Route.extend({
+  queryParams: ['email', 'confirmation'],
+  email: null,
+  confirmation: null,
   setupController() {
     this.send('disableChrome');
   },
