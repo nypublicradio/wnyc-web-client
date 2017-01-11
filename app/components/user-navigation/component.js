bedf566f5b6c35d755881b9586969deacd12b718
walsh9 <walsh9@gmail.com>
2017-01-11 15:57:28 -0500

    Use mut helper

--- app/components/user-navigation/component.js
+++ app/components/user-navigation/component.js
@@ -7,11 +7,6 @@ export default Component.extend({
   isPopupOpen: false,
   classNameBindings: ['isPopupOpen'],
   actions: {
-    closePopup() {
-      if ( !(this.get('isDestroyed') || this.get('isDestroying')) ) {
-        this.set('isPopupOpen', false);
-      }
-    },
     togglePopup() {
       this.toggleProperty('isPopupOpen');
     },
