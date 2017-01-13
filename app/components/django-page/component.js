c57bdd54e1f5c1d7cc5ee5efaa3b0dfb167d8c8d
Brian Whitton <me@brianwhitton.com>
2017-01-12 22:30:25 -0500

    reveal staff links method; add it to django-page component

--- app/components/django-page/component.js
+++ app/components/django-page/component.js
@@ -9,6 +9,7 @@ import {
   embeddedComponentSetup,
   clearAlienDom,
 } from '../../lib/alien-dom';
+import config from 'wnyc-web-client/config/environment';
 
 const { get, computed, run } = Ember;
 let { wnycURL } = ENV;
@@ -86,7 +87,9 @@ export default Ember.Component.extend(LegacySupportMixin, BetaActionsMixin, {
           doRefresh();
         }
 
-        this.revealStaffLinks(this.get('session'));
+        if (this.get('session.data.isStaff')) {
+          this.revealStaffLinks(this.$(), config.wnycAccountRoot);
+        }
         this.$().imagesLoaded().progress((i, image) => {
           Ember.run(() => {
             image.img.classList.add('is-loaded');
