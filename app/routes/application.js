8a77eb91c2990ce58f8d7cd52ae49ebe54358e8d
Brian Whitton <me@brianwhitton.com>
2017-01-12 22:29:42 -0500

    simplify account route method calls

--- app/routes/application.js
+++ app/routes/application.js
@@ -27,7 +27,7 @@ export default Route.extend(ApplicationRouteMixin, {
 
     let metrics = get(this, 'metrics');
 
-    this._syncBrowserId();
+    get(this, 'session').syncBrowserId();
     get(this, 'currentUser').load();
 
     metrics.identify('GoogleAnalytics', {isAuthenticated: false});
@@ -39,8 +39,7 @@ export default Route.extend(ApplicationRouteMixin, {
 
     window.WNYC_LEGACY_LOADER = get(this, 'legacyLoader');
 
-    let store = get(this, 'store');
-    let pollFunction = () => store.findAll('stream');
+    let pollFunction = () => get(this, 'store').findAll('stream');
     get(this, 'poll').addPoll({interval: 60 * 1000, callback: pollFunction});
   },
 
@@ -73,9 +72,5 @@ export default Route.extend(ApplicationRouteMixin, {
     this._super(...arguments);
     get(this, 'metrics').identify('GoogleAnalytics', {isAuthenticated: true});
     get(this, 'currentUser').load();
-  },
-
-  _syncBrowserId() {
-    return get(this, 'session').syncBrowserId();
   },
 });
