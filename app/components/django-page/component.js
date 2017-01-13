b871cdc8a46c7bc0e54d644fda952779a8047b51
Brian Whitton <me@brianwhitton.com>
2017-01-12 22:30:25 -0500

    reveal staff links method; add it to django-page component

--- app/components/django-page/component.js
+++ app/components/django-page/component.js
@@ -9,6 +9,7 @@ import {
   embeddedComponentSetup,
   clearAlienDom,
 } from '../../lib/alien-dom';
+import config from 'wnyc-web-client/config/environment';
 
 const { get, computed } = Ember;
 let { wnycURL, wnycAdminRoot } = ENV;
