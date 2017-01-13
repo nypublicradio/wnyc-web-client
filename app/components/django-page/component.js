36c1226a8c8d1d4251d480c97ab487c32ff32424
Brian Whitton <me@brianwhitton.com>
2017-01-13 09:52:10 -0500

    pass adminRoot to revealStaffLinks
    so that the correct edit URLs can be constructed. also cleans up
    duplicate import from config/environment.js

--- app/components/django-page/component.js
+++ app/components/django-page/component.js
@@ -9,10 +9,9 @@ import {
   embeddedComponentSetup,
   clearAlienDom,
 } from '../../lib/alien-dom';
-import config from 'wnyc-web-client/config/environment';
 
 const { get, computed, run } = Ember;
-let { wnycURL } = ENV;
+let { wnycURL, wnycAdminRoot, renderGoogleAds } = ENV;
 wnycURL = canonicalize(wnycURL);
 
 function doRefresh() {
@@ -83,12 +82,12 @@ export default Ember.Component.extend(LegacySupportMixin, BetaActionsMixin, {
         // re-enable any overlaid content so that it can wormhole
         // itself into the server-rendered DOM.
         this.set('showingOverlay', true);
-        if (ENV.renderGoogleAds) {
+        if (renderGoogleAds) {
           doRefresh();
         }
 
         if (this.get('session.data.isStaff')) {
-          this.revealStaffLinks(this.$(), config.wnycAccountRoot);
+          this.revealStaffLinks(this.$(), wnycAdminRoot);
         }
         this.$().imagesLoaded().progress((i, image) => {
           Ember.run(() => {
