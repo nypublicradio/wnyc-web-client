33b2fa20aad468bfdafc2b71ebf49dea0a841707
walsh9 <walsh9@gmail.com>
2017-01-20 11:36:53 -0500

    I think we might not need this

--- app/user/adapter.js
+++ app/user/adapter.js
@@ -33,18 +33,4 @@ export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
 
     return this.ajax(url, 'PATCH', { data: data });
   },
-
-  normalizeErrorResponse(status, headers, payload) {
-    if (payload && typeof payload === 'object' && payload.error) {
-      return payload.error;
-    } else {
-      return [
-        {
-          status: `${status}`,
-          title: "The backend responded with an error",
-          detail: `${payload}`
-        }
-      ];
-    }
-  },
 });
