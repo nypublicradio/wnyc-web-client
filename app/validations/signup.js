b7ddea993d38ebdbf2591cc50c36712455fe1665
walsh9 <walsh9@gmail.com>
2017-01-20 15:57:16 -0500

    Add character limits to first and last name

--- app/validations/signup.js
+++ app/validations/signup.js
@@ -2,15 +2,18 @@ import {
   validatePresence,
   validateFormat,
   validateConfirmation,
+  validateLength
 } from 'ember-changeset-validations/validators';
 import messages from './custom-messages';
 
 export default {
   givenName: [
-    validatePresence({ presence: true, message: messages.firstNameRequired })
+    validatePresence({ presence: true, message: messages.firstNameRequired }),
+    validateLength({ max: 20, message: messages.firstNameMaxLength })
     ],
   familyName: [
-    validatePresence({ presence: true, message: messages.lastNameRequired })
+    validatePresence({ presence: true, message: messages.lastNameRequired }),
+    validateLength({ max: 40, message: messages.lastNameMaxLength })
     ],
   email: [
     validateFormat({ type: 'email', allowBlank: true, message: messages.emailFormat }),
