97fee7c1d1a8b9cc5aff43b7b2c2cee62506d805
Brian Whitton <me@brianwhitton.com>
2017-01-26 19:44:42 -0500

    no longer need to cleanup after google tags

--- app/lib/compat-hooks.js
+++ app/lib/compat-hooks.js
@@ -31,12 +31,6 @@ export function homepageCleanup(element = document.body) {
 // This gets run by the django-page component right before tearing
 // down the content.
 export function beforeTeardown(/* element, page */) {
-  // we must destroy any presently loaded google ad slots so the next page can
-  // reliably render theirs
-  if (window.googletag && window.googletag.destroySlots) {
-    window.googletag.destroySlots();
-  }
-
   // player.js listens for a storage event with a handler defined on the wnyc object,
   // which is triggered by logic outside of Ember; unbind to avoid throwing errors
   $(window).off('unload storage');
@@ -132,9 +126,6 @@ export function serializeInlineDoc(inlineDoc) {
   toClean.push(inlineDoc.querySelector('script[src*="assets/wnyc-web-client"]'));
   toClean.push(inlineDoc.querySelector('link[href*="assets/vendor"]'));
   toClean.push(inlineDoc.querySelector('link[href*="assets/wnyc-web-client"]'));
-  // any included google ad scripts have also already run, so clean them out so
-  // see errors from intializing ads in occupied divs
-  toClean.push(...inlineDoc.querySelectorAll('.google-ads'));
 
   toClean.forEach(n => n && n.parentNode.removeChild(n));
 
