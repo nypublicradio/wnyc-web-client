9600c0b045f37b956e5e0f21cbd929fe27bd0502
walsh9 <walsh9@gmail.com>
2017-01-31 12:29:34 -0500

    Add validations for leading and trailing spaces on names

--- app/validations/signup.js
+++ app/validations/signup.js
@@ -9,11 +9,15 @@ import messages from './custom-messages';
 export default {
   givenName: [
     validatePresence({ presence: true, message: messages.firstNameRequired }),
-    validateLength({ max: 20, message: messages.firstNameMaxLength })
+    validateLength({ max: 20, message: messages.firstNameMaxLength }),
+    validateFormat({ regex: /^\S.*$/, allowBlank: true, message: messages.noLeadingSpace }),
+    validateFormat({ regex: /^.*\S$/, allowBlank: true, message: messages.noTrailingSpace })
     ],
   familyName: [
     validatePresence({ presence: true, message: messages.lastNameRequired }),
-    validateLength({ max: 40, message: messages.lastNameMaxLength })
+    validateLength({ max: 40, message: messages.lastNameMaxLength }),
+    validateFormat({ regex: /^\S.*$/, allowBlank: true, message: messages.noLeadingSpace }),
+    validateFormat({ regex: /^.*\S$/, allowBlank: true, message: messages.noTrailingSpace })
     ],
   email: [
     validateFormat({ type: 'email', allowBlank: true, message: messages.emailFormat }),
