8e533439f66aa0515ae4071e740b9d435a6c4155
Brian Whitton <me@brianwhitton.com>
2017-02-01 15:24:14 -0500

    user disabled error messages for login and sign up
    adds new custom message and show it
    also manually rollback changest to clear errors so they don't stack up

--- app/validations/custom-messages.js
+++ app/validations/custom-messages.js
@@ -9,4 +9,5 @@ export default {
   emailFormat:           'oops! looks like an invalid email address',
   emailConfirmation:     "oops! this email doesn't match the other one",
   passwordRules:         'must be at least 8 characters and 1 number',
+  userDisabled:          'This email is associated with a disabled account. If you would like to re-enable it, please contact listener services at <a href="http://www.wnyc.org/feedback" target="_blank">http://www.wnyc.org/feedback</a>.'
 };
