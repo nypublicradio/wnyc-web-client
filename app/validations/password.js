89a425f5fa25572fdba9b1354ca941172ce8bc47
walsh9 <walsh9@users.noreply.github.com>
2017-02-02 10:37:15 -0500

    don't allow spaces in passwords (reset page)

--- app/validations/password.js
+++ app/validations/password.js
@@ -6,7 +6,7 @@ import messages from './custom-messages';
 
 export default {
   password: [
-    validateFormat({regex: /^(?=.*?[0-9]).{8,}$/, allowBlank: true, message: messages.passwordRules }),
+    validateFormat({regex: /^(?=[\S]*?[0-9]).{8,}$/, allowBlank: true, message: messages.passwordRules }),
     validatePresence({ presence: true, message: messages.passwordRequired })
     ]
 };
