fc124ae20e3f02cbccf8bcefd74a782ca270866d
Brian Whitton <me@brianwhitton.com>
2017-02-21 12:46:24 -0500

    sign up with facebook action
    this is currently on the signup route, but should probably be moved to
    the account sign up componente per @walsh9's prerogative. Here's the
    logic:
    
    * use the torii provider directly to go through facebook auth, instead
    of using the session#authenticate method. this prevents the global
    sessionAuthenticated event from firing, which has undesired consequences.
    * once authenticated, make a call against the FB api using the FB sdk in
    order to gather the required fields for a new user
    * create the user, save the user
    * now use the session's authenticate method so that it triggers the
    global sessionAuthenticated event, which will load up the current user
    
    *NOTE*: if we end up refactoring this to so that the current user is
    **not** loaded during the sessionAuthenticated, then the logic can
    change so that first we authenticate using the session, and in the
    callback create the user. if this occurs, then the work in 1451399a
    can be taken out, since the headers added in that commit will
    automatically get added since the session will be authenticated.

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -1,12 +1,43 @@
+import Ember from 'ember';
 import Route from 'ember-route';
 
 export default Route.extend({
+  session: Ember.inject.service(),
+  store: Ember.inject.service(),
+  
   actions: {
     didTransition() {
       this.send('disableChrome');
     },
     willTransition() {
       this.send('enableChrome');
+    },
+
+    signupWithFb() {
+      let authenticator = Ember.getOwner(this).lookup('authenticator:torii');
+      authenticator.authenticate('facebook-connect').then(({access_token}) => {
+        window.FB.api('/me', {fields: 'first_name,last_name,email,picture.width(500)'}, data => {
+          // collect user attrs from FB api and send to auth
+          let attrs = {
+            providerToken: access_token,
+            givenName: data.first_name,
+            familyName: data.last_name,
+            email: data.email,
+            picture: data.picture.data.url,
+            facebookId: data.id
+          };
+          let user = this.get('store').createRecord('user', attrs);
+          user.save({adapterOptions: {provider: 'facebook-connect'}}).then(() => {
+            // TODO: this opens a pop up twice, but how else to get an access token
+            // and create a user withouth first triggering the sessionAuthenticated
+            // event? if we authenticate first, the sessionAuthenticated event fires
+            // which tries to load a user before we've had a chance to send the token
+            // to the back end
+            this.get('session').authenticate('authenticator:torii', 'facebook-connect');
+          });
+        });
+      })
+      .catch(() => {});
     }
   }
 });
