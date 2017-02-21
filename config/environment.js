9d109e1a103c205d6c4a56e62ca370903c652cb0
Brian Whitton <me@brianwhitton.com>
2017-02-21 12:30:17 -0500

    adds torii provider and authenticator
    torii is used to authenticate against third-party auth providers like
    facebook. it requires torii is added as a dependency and that the
    FacebookConnect provider is implemented with a bit of boilerplate for
    ember-simple-auth's uses.
    
    the other bit this changes is overriding the ember simple auth Torii
    authenticator's authenticate method to return an authenticated data
    object that conforms to how the rest of the app needs it.
    
    one thing this does is include a value for the `provider` key, which
    will be passed to the auth service at the `X-Provider` header. the
    service uses that value to determine how to validate the provided access
    token which is added at the Authorization header, as normal.

--- config/environment.js
+++ config/environment.js
@@ -139,6 +139,15 @@ module.exports = function(environment) {
       'object-src': "'self' *.wnyc.net:* *.wnyc.org *.moatads.com *.googlesyndication.com",
       'font-src': "'self' data: fonts.gstatic.com",
       'frame-src': "'self' *"
+    },
+    torii: {
+      providers: {
+        'facebook-connect': {
+          appId: process.env.FB_APP || '1399025636795196',
+          scope: 'user_friends,email',
+          version: 'v2.8'
+        }
+      }
     }
   };
 
