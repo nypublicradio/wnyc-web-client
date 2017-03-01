3a4ae94675e0eeea5e08fbcbc922325246340257
walsh9 <walsh9@gmail.com>
2017-03-01 12:58:18 -0500

    Remove unused services from signup route

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -1,10 +1,6 @@
-import Ember from 'ember';
 import Route from 'ember-route';
 
 export default Route.extend({
-  session: Ember.inject.service(),
-  store: Ember.inject.service(),
-
   actions: {
     didTransition() {
       this.send('disableChrome');
