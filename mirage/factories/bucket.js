c1a65c51a1df57d0125a974b6b4455e7f31edccd
Marine Boudeau <m14868@gmail.com>
2017-03-03 14:31:34 -0500

    Add mirage setup for the first set of stories on the hp

--- mirage/factories/bucket.js
+++ mirage/factories/bucket.js
@@ -2,10 +2,24 @@ import { Factory, faker } from 'ember-cli-mirage';
 
 function bucketItems() {
   var res = [];
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 20; i++) {
     res.push({
-      slug: faker.lorem.words().join('-'),
-      producingOrganizations: []
+      attributes: {
+        slug: faker.lorem.words().join('-'),
+        title: faker.lorem.words().join(' '),
+        imageMain: {
+          url: faker.image.image()
+        },
+        headers: {
+          brand: {
+            title: faker.lorem.words().join(' '),
+            url: 'http://marineboudeau.com'
+          }
+        },
+        template: faker.random.arrayElement(['story_default', 'story_video']),
+        url: 'http://marineboudeau.com'
+      },
+      type: faker.random.arrayElement(['event', 'story'])
     });
   }
   return res;
