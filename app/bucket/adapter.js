d0f0d5327646c12de31368960e89b0ed56cc3f00
Marine Boudeau <m14868@gmail.com>
2017-03-06 17:57:06 -0500

    Fix bucket API call

--- app/bucket/adapter.js
+++ app/bucket/adapter.js
@@ -2,7 +2,6 @@ import ApplicationAdapter from 'wqxr-web-client/adapters/application';
 import config from 'wqxr-web-client/config/environment';
 
 export default ApplicationAdapter.extend({
-  pathForType: 'bucket',
   buildURL(modelName, id, snapshot, requestType, query) {
     if (requestType !== 'findRecord') {
       return this._super(...arguments);
