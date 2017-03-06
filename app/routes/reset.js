fff6652bd2ff0a49a154be2f4e2be494304e10be
walsh9 <walsh9@gmail.com>
2017-03-06 12:13:53 -0500

    Pass session to account forms

--- app/routes/reset.js
+++ app/routes/reset.js
@@ -1,10 +1,13 @@
 import Route from 'ember-route';
+import service from 'ember-service/inject';
 import config from 'wnyc-web-client/config/environment';
 
 export default Route.extend({
   config,
+  session: service(),
   setupController(controller) {
     controller.set('config', this.get('config'));
+    controller.set('session', this.get('session'));
     return this._super(...arguments);
   },
   beforeModel(transition) {
