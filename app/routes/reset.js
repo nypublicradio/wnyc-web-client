cc2006886ac86b5a8d48ac70a5d38b8a3c6638c5
walsh9 <walsh9@gmail.com>
2017-03-06 12:13:19 -0500

    Handle redirect in route instead of component

--- app/routes/reset.js
+++ app/routes/reset.js
@@ -7,6 +7,14 @@ export default Route.extend({
     controller.set('config', this.get('config'));
     return this._super(...arguments);
   },
+  beforeModel(transition) {
+    if ( !transition.queryParams.email || !transition.queryParams.confirmation ) {
+      // if we got here with missing url parameters,
+      // (i.e. typing url, bad copy/paste)
+      // send the user to the 'forgot' page so they can request a reset email
+      this.transitionTo('forgot');
+    }
+  },
   actions: {
     didTransition() {
       this.send('disableChrome');
