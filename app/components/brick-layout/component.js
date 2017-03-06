141fced50e126c92bd0a7e3e44f3f1af8c97a371
Marine Boudeau <m14868@gmail.com>
2017-03-06 13:49:44 -0500

    Add missing stories to the homepage

--- app/components/brick-layout/component.js
+++ app/components/brick-layout/component.js
@@ -13,4 +13,22 @@ export default Ember.Component.extend({
   largeStoryRow2: Ember.computed('items', function(){
     return this.get('items').slice(5,6);
   }),
+  cardsRow1: Ember.computed('items', function(){
+    return this.get('items').slice(6,8);
+  }),
+  largeStoryRow3: Ember.computed('items', function(){
+    return this.get('items').slice(8,9);
+  }),
+  colStoriesRow3: Ember.computed('items', function(){
+    return this.get('items').slice(9,11);
+  }),
+  colStoriesRow4: Ember.computed('items', function(){
+    return this.get('items').slice(11,13);
+  }),
+  largeStoryRow4: Ember.computed('items', function(){
+    return this.get('items').slice(13,14);
+  }),
+  cardsRow2: Ember.computed('items', function(){
+    return this.get('items').slice(14,16);
+  }),
 });
