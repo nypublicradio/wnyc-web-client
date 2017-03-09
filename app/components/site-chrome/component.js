eb2740587f0ffed8cdcb353673e4a2d2a56019c7
Diana Parrington <diana.parrington@gmail.com>
2017-03-09 14:21:03 -0500

    sitechrome needs to know when player is open, as wnyc

--- app/components/site-chrome/component.js
+++ app/components/site-chrome/component.js
@@ -11,6 +11,7 @@ export default Component.extend(BetaActionsMixin, {
   router: service('wnyc-routing'),
   currentUser: service(),
   donateURL: config.wnycDonateURL,
+  classNameBindings: ['showPlayer:sitechrome--player-open'],
   defaultStream:  {slug: 'wqxr', name: 'WQXR New York'},
   preferredStream: or('session.data.user-prefs-active-stream', 'defaultStream'),
 
