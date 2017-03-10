a911d3858a1c45ac994cf34ab199a375126b2c23
Marine Boudeau <m14868@gmail.com>
2017-03-10 12:58:45 -0500

    Move brick component over to nypr-ui

--- app/components/brick-card-row/component.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  tagName: 'ul',
-  classNames: 'brick_row brick_row--cards',
-  group: Ember.computed('items', function() {
-    return this.get('items');
-  })
-});
--- app/components/brick-item/component.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  tagName: '',
-  item: Ember.computed('items', function(){
-    return this.get('items');
-  }),
-});
