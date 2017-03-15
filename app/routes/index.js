f4b345045179cd6a5abfb203b72c7341c6fdd99c
Brian Whitton <me@brianwhitton.com>
2017-03-15 19:01:07 -0400

    split returned stories into two groups for layout components

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,11 @@ export default Route.extend({
   classNames: ['home'],
 
   model() {
-    return this.get('store').findRecord('bucket', 'wqxr-home');
+    return this.get('store').findRecord('bucket', 'wqxr-home').then(bucket => {
+      return {
+        featuredItems: bucket.get('bucketItems').slice(0, 8),
+        otherItems: bucket.get('bucketItems').slice(8)
+      };
+    });
   }
 });
