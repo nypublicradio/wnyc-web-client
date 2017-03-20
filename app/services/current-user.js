77222d3ffc8367e45bf6d91416229e9854e9c242
walsh9 <walsh9@gmail.com>
2017-03-20 12:47:11 -0400

    Move third-party user creation to session

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -1,5 +1,6 @@
 import Service from 'ember-service';
 import service from 'ember-service/inject';
+import RSVP from 'rsvp';
 
 export default Service.extend({
   session: service('session'),
@@ -7,12 +8,23 @@ export default Service.extend({
 
   load() {
     if (this.get('session.isAuthenticated')) {
-      let user = this.get('store').queryRecord('user', {me: true});
-      this.set('user', user);
-      user.catch(() => {
+      let user;
+      if (this.get('session.data.isSigningUpWithThirdParty') === true) {
+        this.get('session').set('data.isSigningUpWithThirdParty', false);
+        user = this.get('session').createUserForAuthenticatedProvider();
+      } else {
+        user = this.get('store').queryRecord('user', {me: true});
+      }
+
+      return user.then((user) => {
+        this.set('user', user);
+      })
+      .catch(() => {
         // this access token has since been revoked
         this.get('session').invalidate();
       });
+    } else {
+      return RSVP.resolve();
     }
   }
 });
--- app/services/session.js
+++ app/services/session.js
@@ -1,10 +1,13 @@
 import SessionService from 'ember-simple-auth/services/session';
 import config from 'wnyc-web-client/config/environment';
 import RSVP from 'rsvp';
+import service from 'ember-service/inject';
 import fetch from 'fetch';
 import getOwner from 'ember-owner/get';
 
 export default SessionService.extend({
+  store: service(),
+
   syncBrowserId(report = true) {
     let legacyId;
     try {
@@ -47,10 +50,36 @@ export default SessionService.extend({
       });
     });
   },
-  
+
   verify(email, password) {
     let authenticator = getOwner(this).lookup('authenticator:nypr');
     return authenticator.authenticate(email, password);
+  },
+
+  createUserForAuthenticatedProvider() {
+    let provider = this.get('data.authenticated.provider');
+    let access_token = this.get('data.authenticated.access_token');
+    if (provider === 'facebook-connect') {
+      return new RSVP.Promise((resolve, reject) => {
+        window.FB.api('/me', {fields: 'first_name,last_name,email,picture.width(500)'}, response => {
+          if (!response || response.error) {
+            reject(response);
+          }
+          // collect user attrs from FB api and send to auth
+          let attrs = {
+            providerToken: access_token,
+            givenName: response.first_name,
+            familyName: response.last_name,
+            email: response.email,
+            picture: response.picture.data.url,
+            facebookId: response.id
+          };
+          let user = this.get('store').createRecord('user', attrs);
+          user.save({adapterOptions: {provider: 'facebook-connect'}});
+          resolve(user);
+        });
+      });
+    }
   }
 });
 
