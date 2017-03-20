a38125e9961821cc5677f18a274d544d8db05278
Diana Parrington <diana.parrington@gmail.com>
2017-03-20 12:10:17 -0400

    add route for events to differentiate active state

---
+++ tests/acceptance/events-test.js
@@ -0,0 +1,49 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'wqxr-web-client/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | events', {
+  beforeEach() {
+    server.create('stream');
+  }
+});
+
+test('visiting /events', function(assert) {
+  server.create('django-page', {id: `events`});
+  visit('/events');
+
+  andThen(function() {
+    assert.equal(currentURL(), `/events`);
+  });
+});
+
+test('clicking on /events', function(assert) {
+  server.create('django-page', {id: '/'});
+  server.create('django-page', {id: `events`});
+  
+  visit('/');
+  
+  andThen(function() {
+    click('a[href="/events"]');
+  });
+  
+  andThen(function() {
+    assert.equal(currentURL(), `/events`);
+  });
+});
+
+test('transitioning to a specific event', function(assert) {
+  server.create('django-page', {
+    id: '/',
+    testMarkup: `
+    <a href="/events/wqxr-media-sponsorship/2016/jan/29/ecstatic-music-festival-2016/" id="foo">foo</a>
+    `
+  });
+  server.create('django-page', {id: `events/wqxr-media-sponsorship/2016/jan/29/ecstatic-music-festival-2016/`});
+  
+  visit('/');
+  click('#foo');
+  
+  andThen(function() {
+    assert.equal(currentURL(), `/events/wqxr-media-sponsorship/2016/jan/29/ecstatic-music-festival-2016/`);
+  });
+});
