f898446b1873b741b00b8e159eba493fbfce0733
Michael Hearn <git@mikehearn.net>
2017-03-21 00:42:27 -0400

    Removed unused DataAdapter mixin

--- app/adapters/order.js
+++ app/adapters/order.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
 import ENV from '../config/environment';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend({
   host: ENV.wnycMembershipAPI,
