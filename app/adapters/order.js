bb185ae1346ac7fc72296c93671640f4a3be9a6e
Michael Hearn <git@mikehearn.net>
2017-03-21 13:24:51 -0400

    Removed membership env var, using wnycAPI val

--- app/adapters/order.js
+++ app/adapters/order.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 import ENV from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  host: ENV.wnycMembershipAPI,
-  namespace: 'v1',
+  host: ENV.wnycAPI,
+  namespace: 'membership/v1',
   buildURL(...args) {
     let url = this._super(...args);
     return `${url}/`;
