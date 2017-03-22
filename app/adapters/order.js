5d6968b2f93cfbdbea71cc82444f4ca579c9a281
Michael Hearn <git@mikehearn.net>
2017-03-22 12:36:55 -0400

    Add authorizer to order adapter

--- app/adapters/order.js
+++ app/adapters/order.js
@@ -4,6 +4,7 @@ import ENV from '../config/environment';
 export default DS.JSONAPIAdapter.extend({
   host: ENV.wnycAPI,
   namespace: 'membership/v1',
+  authorizer: 'authorizer:nypr',
   buildURL(...args) {
     let url = this._super(...args);
     return `${url}/`;
