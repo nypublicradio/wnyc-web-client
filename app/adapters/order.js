32e190d9cf92a092cb001f9ad77a549732c6313a
Michael Hearn <git@mikehearn.net>
2017-03-22 12:54:12 -0400

    Add DataAdapterMixin to order adapter

--- app/adapters/order.js
+++ app/adapters/order.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
 import ENV from '../config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   host: ENV.wnycAPI,
   namespace: 'membership/v1',
   authorizer: 'authorizer:nypr',
