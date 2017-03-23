71eaca3c44c9661323d6547c214882bc8faeea73
Michael Hearn <git@mikehearn.net>
2017-03-23 13:28:24 +0000

    Update GTM prod & demo IDs to WQXR's container IDs

--- config/environment.js
+++ config/environment.js
@@ -32,7 +32,7 @@ module.exports = function(environment) {
     }, {
       name: 'GoogleTagManager',
       config: {
-        id: process.env.GOOGLE_TAG_MANAGER_ID || (environment === 'production' ? 'GTM-PM94N2' : 'GTM-KJZRH7H')
+        id: process.env.GOOGLE_TAG_MANAGER_ID || (environment === 'production' ? 'GTM-MZ2S75K' : 'GTM-5NR8N5P')
       },
       environments: ['production']
     }],
