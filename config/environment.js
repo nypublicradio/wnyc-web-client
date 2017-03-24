7bf3682b17c91524cb38cf3b0975a1ade35754a8
Diana Parrington <diana.parrington@gmail.com>
2017-03-24 14:26:26 -0400

    new donate link

--- config/environment.js
+++ config/environment.js
@@ -119,7 +119,7 @@ module.exports = function(environment) {
     wnycStaticURL: process.env.WQXR_STATIC_URL,
     wnycURL: process.env.WQXR_URL,
     platformEventsAPI: process.env.PLATFORM_EVENTS_SERVICE,
-    wnycDonateURL: 'https://pledge3.wqxr.org/donate/wqxr/main/onestep/?utm_source=wqxr&utm_medium=wqxr-support-300x75&utm_campaign=pledge&utm_content=pledge',
+    wnycDonateURL: 'http://www.wqxr.org/epledge/main?utm_source=wqxr&utm_medium=wqxr-85x39&utm_campaign=pledge&utm_content=donate',
     wnycSvgURL: '/media/svg/',
     // put beta host at the root so it can be overridden by Django
     wnycBetaURL: process.env.WNYC_BETA_URL,
