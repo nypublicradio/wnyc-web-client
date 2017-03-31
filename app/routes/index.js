ec2a246da8d2c6641c2c9533df112801b5d09a47
Brian Whitton <me@brianwhitton.com>
2017-03-31 19:14:35 -0400

    adds playparam mixin back to index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,10 @@
 import Route from 'ember-route';
 import service from 'ember-service/inject';
+import PlayParamMixin from 'wqxr-web-client/mixins/play-param';
 
 const carouselBg = 'https://images.unsplash.com/photo-1481462585914-9f695507135e?dpr=2&auto=format&fit=crop&w=1500&h=1500&q=80&cs=tinysrgb';
 
-export default Route.extend({
+export default Route.extend(PlayParamMixin, {
   audio:      service(),
   classNames: ['home'],
 
