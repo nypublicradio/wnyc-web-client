3e26c027fe3c0a6fd2386f811b250b49c265b42a
Michael Hearn <git@mikehearn.net>
2017-04-04 18:03:42 -0400

    Update web client member center to async

--- app/adapters/order.js
+++ app/adapters/order.js
@@ -3,7 +3,7 @@ import ENV from '../config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  host: ENV.wnycAPI,
+  host: ENV.wnycAPIGateway,
   namespace: 'membership/v1',
   authorizer: 'authorizer:nypr',
   buildURL(...args) {
--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,15 +1,15 @@
 import Route from 'ember-route';
 import service from 'ember-service/inject';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import RSVP from 'rsvp';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   currentUser: service(),
-
   model() {
-    return RSVP.hash({
-      user: this.get('currentUser.user'),
-      order: this.store.findAll('order')
+    return this.get('currentUser.user').then(result => {
+      return {
+        user: result,
+        order: this.store.findAll('order')
+      };
     });
   }
 });
