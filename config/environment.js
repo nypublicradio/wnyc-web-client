f463bd86ce7903b3e07a4416764715fc7eb2667c
Michael Hearn <git@mikehearn.net>
2017-04-04 18:03:42 -0400

    Update web client member center to async

--- config/environment.js
+++ config/environment.js
@@ -117,6 +117,7 @@ module.exports = function(environment) {
     moreShowsAPIKey: "mammoth",
     wnycAdminRoot: process.env.WNYC_ADMIN_ROOT,
     wnycAuthAPI: process.env.AUTH_SERVICE,
+    wnycAPIGateway: process.env.API_GATEWAY,
     wnycAccountRoot: process.env.WNYC_ACCOUNT_ROOT,
     wnycEtagAPI: process.env.WNYC_ETAG_API,
     wnycStaticURL: process.env.WNYC_STATIC_URL,
--- mirage/config.js
+++ mirage/config.js
@@ -234,5 +234,5 @@ export default function() {
   /*-------------------------------------------------------------
   membership microservice
   ---------------------------------------------------------------*/
-  this.get(`${config.wnycAPI}/membership/v1/orders/`, 'orders');
+  this.get(`${config.wnycAPIGateway}/membership/v1/orders/`, 'orders');
 }
