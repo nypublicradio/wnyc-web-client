537d6bc5224231f0bbc96c921ebbc106f95dcb43
Michael Hearn <git@mikehearn.net>
2017-04-05 22:36:26 -0400

    Update user model to be returned as promise

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,14 +1,17 @@
 import Route from 'ember-route';
 import service from 'ember-service/inject';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import RSVP from 'rsvp';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   currentUser: service(),
   model() {
-    return this.get('currentUser.user');
+    return RSVP.hash({
+      user: this.get('currentUser.user'),
+    });
   },
   setupController(controller, model) {
-    this._super(controller, model);
     controller.set('orders', this.store.findAll('order'));
+    return this._super(controller, model);
   }
 });
