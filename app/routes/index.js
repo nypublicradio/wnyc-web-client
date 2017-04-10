fb6cac93668f4a4dc1a3ac2ef87bd298770dadef
Diana Parrington <diana.parrington@gmail.com>
2017-04-10 15:41:16 -0400

    use new brick blowout

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,8 @@
 import Route from 'ember-route';
 import service from 'ember-service/inject';
-<<<<<<< HEAD
 import PlayParamMixin from 'wqxr-web-client/mixins/play-param';
 import DS from 'ember-data';
-=======
 import RSVP from 'rsvp';
->>>>>>> membership chunk rendering
 
 const carouselBg = 'https://images.unsplash.com/photo-1481462585914-9f695507135e?dpr=2&auto=format&fit=crop&w=1500&h=1500&q=80&cs=tinysrgb';
 
@@ -19,9 +16,7 @@ export default Route.extend(PlayParamMixin, {
       membershipChunk: this.store.findRecord('chunk', 'wqxr-membership-home').catch(() => ''),
       gridItems: this.store.findRecord('bucket', 'wqxr-home').then(b => b.get('bucketItems'))
     });
-
   },
-
 
   setupController(controller, model) {
     this._super(...arguments);
