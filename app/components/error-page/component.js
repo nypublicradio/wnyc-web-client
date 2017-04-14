eef3297322a137e5d8998d7a7d17b7f01e2a8642
Brian Whitton <me@brianwhitton.com>
2017-04-14 16:57:54 -0400

    refactors error page component for wqxr layout

--- app/components/error-page/component.js
+++ app/components/error-page/component.js
@@ -1,7 +1,37 @@
-import Ember from 'ember';
+import Component from 'ember-component';
+import computed, { readOnly } from 'ember-computed';
+import { htmlSafe } from 'ember-string';
 
-export default Ember.Component.extend({
-  errorType: Ember.computed.readOnly('error.response.status'),
-  pageNotFound: Ember.computed.equal('errorType', 404),
-  serverError: Ember.computed.equal('errorType', 500)
+const ASSET_MAP = {
+  '404': '/assets/img/error/violinist.jpg'
+};
+
+const TEXT_MAP = {
+  '404': {
+    headline: "Something's missing",
+    sub: "(404) We can't find the page you're looking for. Please check the url and try again."
+  }
+};
+
+export default Component.extend({
+  classNames: ['error-page'],
+  attributeBindings: ['style'],
+  
+  style: computed('errorCode', function() {
+    let imagePath = ASSET_MAP[this.get('errorCode')];
+    return htmlSafe(`background-image: url(${imagePath});`);
+  }),
+  headline: computed('errorCode', function() {
+    let text = TEXT_MAP[this.get('errorCode')];
+    if (text) {
+      return text.headline;
+    }
+  }),
+  sub: computed('errorType', function() {
+    let text = TEXT_MAP[this.get('errorCode')];
+    if (text) {
+      return text.sub;
+    }
+  }),
+  errorCode: readOnly('error.response.status'),
 });
