7b0746625f9bb9072eaec93daa714fd63119f7a6
Brian Whitton <me@brianwhitton.com>
2017-04-16 17:43:24 -0400

    empty string for staticHost in testing

--- config/environment.js
+++ config/environment.js
@@ -191,7 +191,7 @@ module.exports = function(environment) {
     ENV.wnycURL = '//example.com';
     ENV.wnycBetaURL = 'http://example.com';
     ENV.wnycAuthAPI = 'http://example.com';
-    ENV.staticHost = '/';
+    ENV.staticHost = '';
   }
 
   if (environment === 'production') {
