880a7d8a62d210d5bf9063b8bf0a2861beba550c
walsh9 <walsh9@gmail.com>
2017-04-17 14:39:08 -0400

    Let ember handle user promise resolution

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -9,9 +9,7 @@ export default Service.extend({
     if (this.get('session.isAuthenticated')) {
       let user = this.get('store').queryRecord('user', {me: true});
       this.set('user', user);
-      return user.then((user) => {
-        this.set('user', user);
-      })
+      return user
       .catch(() => {
         // this access token has since been revoked
         this.get('session').invalidate();
