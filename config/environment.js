be535e1294f75c24b2fde2324e85562ad8ad68f5
Brian Whitton <me@brianwhitton.com>
2017-04-16 22:49:54 -0400

    turns out filepaths are fingerprinted in JS files
    don't need staticHost

--- config/environment.js
+++ config/environment.js
@@ -163,9 +163,6 @@ module.exports = function(environment) {
       // passed in --proxy to the command line
       enabled: mirageEnabled
     };
-    
-    ENV.staticHost = 'http://localhost:4200';
-
   }
 
   if (environment === 'test') {
@@ -191,15 +188,10 @@ module.exports = function(environment) {
     ENV.wnycURL = '//example.com';
     ENV.wnycBetaURL = 'http://example.com';
     ENV.wnycAuthAPI = 'http://example.com';
-    ENV.staticHost = '';
   }
 
   if (environment === 'production') {
 
-  }
-  
-  if (['production', 'demo'].includes(process.env.DEPLOY_TARGET)) {
-    ENV.staticHost = process.env.FINGERPRINT_PREPEND_URL;
   }
 
   return ENV;
