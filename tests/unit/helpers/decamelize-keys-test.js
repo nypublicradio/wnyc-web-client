92d2487a182f70773b1b59f7d864aaab9175c6ad
walsh9 <walsh9@gmail.com>
2017-04-17 14:37:48 -0400

    Add decamelize keys helper

---
+++ tests/unit/helpers/decamelize-keys-test.js
@@ -0,0 +1,10 @@
+import { decamelizeKeys } from 'wnyc-web-client/helpers/decamelize-keys';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | decamelize-keys');
+
+// Replace this with your real tests.
+test('it decamelizes keys', function(assert) {
+  let result = decamelizeKeys([{ 'fooBar': true, bleepBloopBlop: true }]);
+  assert.deepEqual(result, {foo_bar: true, bleep_bloop_blop: true});
+});
