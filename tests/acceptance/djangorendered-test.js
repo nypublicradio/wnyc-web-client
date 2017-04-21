cd68c09312d21e298d20026cc5f137dd7beb7635
Brian Whitton <me@brianwhitton.com>
2017-04-21 13:28:30 -0400

    adds assertions for DFP targeting
    on the following routes:
    `djangorendered`
    `playlist`: /streams/<slug>/
    `schedule`: /schedule/<date>/?scheduleStation=<slug>
    `shows`: /shows
    `streams`: /streams
    `channels`: /shows|series|articles|blogs|tags/<slug>

--- tests/acceptance/djangorendered-test.js
+++ tests/acceptance/djangorendered-test.js
@@ -1,5 +1,6 @@
 import config from 'wnyc-web-client/config/environment';
-import { skip, test } from 'qunit';
+import { skip } from 'qunit';
+import test from 'ember-sinon-qunit/test-support/test';
 import { plantBetaTrial } from 'wnyc-web-client/tests/helpers/beta';
 import moduleForAcceptance from 'wnyc-web-client/tests/helpers/module-for-acceptance';
 import djangoPage from 'wnyc-web-client/tests/pages/django-page';
@@ -180,6 +181,19 @@ test('.search is added to search pages', function(assert) {
   andThen(function() {
     assert.equal(find('.django-content').parent('.search').length, 1, 'should have an l-constrained class');
   });
+});
+
+
+test('arbitrary django routes do dfp targeting', function(/*assert*/) {
+  server.create('django-page', {id: 'fake/'});
+
+  this.mock(this.application.__container__.lookup('route:djangorendered').get('googleAds'))
+    .expects('doTargeting')
+    .once();
+  
+  djangoPage
+    .bootstrap({id: 'fake/'})
+    .visit({id: 'fake/'});
 });
 
 moduleForAcceptance('Acceptance | Django Rendered | Beta Trial', {
--- tests/acceptance/playlist-test.js
+++ tests/acceptance/playlist-test.js
@@ -1,4 +1,4 @@
-import { test } from 'qunit';
+import test from 'ember-sinon-qunit/test-support/test';
 import moduleForAcceptance from 'wnyc-web-client/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | playlist');
@@ -24,4 +24,25 @@ test('visiting /streams/wnyc-fm939', function(assert) {
     assert.equal(currentURL(), '/streams/wnyc-fm939');
     assert.equal(find('a[href="http://fooshow.com"]').text().trim(), 'Episode Foo');
   });
+});
+
+test('playlist routes do dfp targeting', function(/*assert*/) {
+  this.mock(this.application.__container__.lookup('route:playlist').get('googleAds'))
+    .expects('doTargeting')
+    .once();
+  server.create('stream', {
+    slug: 'wnyc-fm939', 
+    name: 'WNYC FM',
+  });
+  server.create('whats-on', {
+    slug: 'wnyc-fm939',
+    current_show: {
+      show_title: 'Foo Show',
+      title: 'Episode Foo',
+      url: 'http://fooshow.com',
+      end: "2016-09-15T13:00:15.542Z" // 9 am
+    }
+  });
+
+  visit('/streams/wnyc-fm939');
 });
