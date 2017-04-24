20a89a0ced860af89677f6670cbabc6a1275c5cb
walsh9 <walsh9@gmail.com>
2017-04-24 12:21:17 -0400

    Remove floating queue button

--- app/transitions/player-reveal.js
+++ app/transitions/player-reveal.js
@@ -4,11 +4,9 @@ export default function() {
   this.newElement.css('visibility', '');
   if (!this.newValue) {
     let player = this.newElement.find('.nypr-player');
-    let floatingQueueButton = this.oldElement.find('.nypr-player-queue-button');
     player.css({ transform: 'translateY(80px)' });
     let playerAnimationOptions = {easing: [0.17, 0.89, 0.39, 1.25], duration: 350};
     return Promise.all([
-      animate(floatingQueueButton, {opacity: [0, 1]}, {duration: 125}),
       animate(player, {translateY: [0, '80px']}, playerAnimationOptions)
     ]);
   }
