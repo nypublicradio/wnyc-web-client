0dc298b427e65e88be86ab36d3e239cbf0dc1419
Brian Whitton <me@brianwhitton.com>
2017-04-27 09:53:49 -0400

    add videos template so we can add the leaderboard

--- app/routes/videos.js
+++ app/routes/videos.js
@@ -4,7 +4,6 @@ export default Route.extend({
   titleToken(model) {
     return model.get('title');
   },
-  templateName: 'djangorendered',
   model() {
     return this.store.findRecord('django-page', 'videos/');
   }
