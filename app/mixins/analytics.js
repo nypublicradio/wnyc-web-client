c8ebfc3bd6567ca71a3a8c6381398364e765ca5c
Brian Whitton <me@brianwhitton.com>
2017-05-05 18:12:08 -0400

    use seach query to construct pageview

--- app/mixins/analytics.js
+++ app/mixins/analytics.js
@@ -30,7 +30,7 @@ export default Mixin.create({
   _trackPage() {
     scheduleOnce('afterRender', this, () => {
       const metrics = get(this, 'metrics');
-      const page = document.location.pathname; // e.g. '/shows/bl/'
+      const page = document.location.pathname + document.location.search; // e.g. '/shows/bl/?q=foo'
       const title = document.title; // this should be something dynamic
 
       metrics.trackPage('GoogleAnalytics', { page, title });
