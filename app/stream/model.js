792b4af6030976db3b56704aa285cde717c911b9
Jeff Keen <jeff@keen.me>
2017-05-08 16:26:09 -0500

    Add default argument for data on forListenAction

--- app/stream/model.js
+++ app/stream/model.js
@@ -58,7 +58,7 @@ export default Model.extend({
     return this.get('currentPlaylistItem.catalogEntry.title');
   }),
 
-  forListenAction(data) {
+  forListenAction(data = {}) {
     return this.get('currentStory').then(s => {
       data.current_audio_position = 0; // stream should always report 0
       return Object.assign({
