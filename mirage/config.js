fee2436ad6f009831b372187f5745eddf945fe3d
walsh9 <walsh9@gmail.com>
2017-05-09 16:48:57 -0400

    Disable logging

--- mirage/config.js
+++ mirage/config.js
@@ -7,7 +7,7 @@ import { Response, faker } from 'ember-cli-mirage';
 // Note for future people: schema.modelName.create() doesn't generate attributes in mirage factories. Create the objects using server.create in default.js (for local dev), or in the test
 
 export default function() {
-  this.logging = true;
+  this.logging = false;
   let baseUrl = config.wnycURL;
 
   /*------------------------------------------------------------
