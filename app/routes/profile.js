ab938dc05f3f9e228e1d78f8c2529539996422c8
walsh9 <walsh9@gmail.com>
2017-05-09 11:21:18 -0400

    Move check back to controller, but call it from route

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,10 +1,7 @@
 import Route from 'ember-route';
 import service from 'ember-service/inject';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import config from 'wqxr-web-client/config/environment';
 import get from 'ember-metal/get';
-import { task } from 'ember-concurrency';
-import fetch from 'fetch';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   currentUser: service(),
@@ -16,35 +13,8 @@ export default Route.extend(AuthenticatedRouteMixin, {
   setupController(controller, model) {
     if (this.features.isEnabled('member-center')) {
       controller.set('orders', this.store.findAll('order'));
-      this.get('checkVerificationStatus').perform(controller, model);
+      controller.send('checkEmailStatus', get(model, 'user.email'));
     }
     return this._super(controller, model);
   },
-
-  checkVerificationStatus: task(function * (controller, model) {
-    yield this.get('session').authorize('authorizer:nypr', (_, authorization) => this.get('setVerificationStatus').perform(authorization, controller, model));
-  }).on('init'),
-
-  setVerificationStatus: task(function * (authorization, controller, model) {
-    let email = get(model, 'user.email');
-    let url = `${config.wnycMembershipAPI}/v1/emails/is-verified/?email=${email}`;
-    try {
-      let response = yield fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: authorization
-        }
-      });
-      if (response && response.ok) {
-        let json = yield response.json();
-        // if it's not verified, it's pending
-        let pendingVerification = !json.data.is_verified;
-        controller.set('emailPendingVerification', pendingVerification);
-      }
-    } catch(e) {
-      // if there's a problem with the request, don't change the status
-      // because we don't want to show the pending message and confuse users.
-    }
-  }),
 });
