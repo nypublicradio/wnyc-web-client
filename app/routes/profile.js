9acea2b9d819f862b0a12ce146f14b8cd6f421de
Michael Hearn <git@mikehearn.net>
2017-05-10 11:52:15 -0400

    Removed RSVP hash on profile route

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,14 +1,11 @@
 import Route from 'ember-route';
 import service from 'ember-service/inject';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import RSVP from 'rsvp';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   currentUser: service(),
   model() {
-    return RSVP.hash({
-      user: this.get('currentUser.user'),
-    });
+    return this.get('currentUser.user');
   },
   setupController(controller, model) {
     if (this.features.isEnabled('member-center')) {
