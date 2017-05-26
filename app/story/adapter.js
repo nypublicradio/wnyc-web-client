921d6b21d0d2880a32791264ebfa2719624395e2
Brian Whitton <me@brianwhitton.com>
2017-05-26 06:37:50 -0400

    whip adapter/serializer updates for queries
    need to integrate with related stories

--- app/story/adapter.js
+++ app/story/adapter.js
@@ -8,19 +8,24 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:nypr',
   host: ENV.wnycAPI,
-  namespace: 'api/v2',
-  query(store, type, query) {
-    let url = [this.host, this.namespace, 'related', query.itemId, `?limit=${query.limit}`].join('/');
-    let options = this.ajaxOptions(url, 'GET', {});
-    // Django isn't setup to honor XHR requests at the related stories endpoint,
-    // so just use the jQuery JSONp for now
-    if (ENV.environment === 'production') {
-      options.dataType = 'jsonp';
-      options.jsonpCallback = 'RELATED';
-      options.cache = true;
-    } 
-    return wrapAjax(options);
-  },
+  pathForType: () => 'story/',
+  namespace: 'api/v3',
+  // query(store, type, {itemId, uslug, limit, ordering}) {
+  //   if (itemId) {
+  //     let url = [this.host, this.namespace, 'v2/related', itemId, `?limit=${limit}`].join('/');
+  //     let options = this.ajaxOptions(url, 'GET', {});
+  //     // Django isn't setup to honor XHR requests at the related stories endpoint,
+  //     // so just use the jQuery JSONp for now
+  //     if (ENV.environment === 'production') {
+  //       options.dataType = 'jsonp';
+  //       options.jsonpCallback = 'RELATED';
+  //       options.cache = true;
+  //     } 
+  //     return wrapAjax(options);
+  //   } else if (uslug) {
+  //     let url = `${this.host}/${this.namespace}/v3/story/?uslug=${uslug}&`
+  //   }
+  // },
   findRecord(store, type, id/*, snapshot*/) {
     var url = [this.host, 'api/v3', 'story', 'detail', id].join('/') + '/';
     let options = this.ajaxOptions(url, 'GET', {});
--- app/story/serializer.js
+++ app/story/serializer.js
@@ -1,22 +1,56 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  normalizeQueryResponse(store, primaryModelClass, payload, id, requestType) {
-    let transformed = {
-      data: payload.results.map(result => {
-        let id = result.id;
-        delete result.id;
-        
-        let attributes = {};
-        Object.keys(result).forEach(key => attributes[key.dasherize()] = result[key]);
-        
-        return {
-          id,
-          type: 'story',
-          attributes
-        };
-      })
-    };
-    return this._super(store, primaryModelClass, transformed, id, requestType);
+  // normalizeQueryResponse(store, primaryModelClass, payload, id, requestType) {
+  //   let transformed = {
+  //     data: payload.results.map(result => {
+  //       let id = result.id;
+  //       delete result.id;
+  //       
+  //       let attributes = {};
+  //       Object.keys(result).forEach(key => attributes[key.dasherize()] = result[key]);
+  //       
+  //       return {
+  //         id,
+  //         type: 'story',
+  //         attributes
+  //       };
+  //     })
+  //   };
+  //   return this._super(store, primaryModelClass, transformed, id, requestType);
+  // }
+  normalizeQueryResponse(store, modelClass, payload) {
+    const result = this._super(...arguments);
+    result.meta = result.meta || {};
+
+    if (payload.links) {
+      result.meta.pagination = this.createPageMeta(payload.links);
+    }
+
+    return result;
+  },
+  createPageMeta(data) {
+
+    let meta = {};
+
+    Object.keys(data).forEach(type => {
+      const link = data[type];
+      meta[type] = {};
+      let a = document.createElement('a');
+      a.href = link;
+
+      a.search.slice(1).split('&').forEach(pairs => {
+        const [param, value] = pairs.split('=');
+
+        if (param === 'page') {
+          meta[type].number = parseInt(value);
+        }
+
+      });
+      a = null;
+    });
+
+    return meta;
+
   }
 });
