20ac0b97c9a89a8f5144c09218b2680da8d6f15e
Brian Whitton <me@brianwhitton.com>
2017-05-30 00:21:29 -0400

    just use existing page-numbers component for page numbers

--- app/controllers/archive.js
+++ app/controllers/archive.js
@@ -3,11 +3,5 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['page'],
   page: 1,
-  count: Ember.computed('model.meta.pagination.last.number', 'model.meta.pagination.self.number', function() {
-    let total = this.get('model.meta.pagination.last.number') || this.get('model.meta.pagination.self.number');
-    if (!total) {
-      return [];
     }
-    return new Array(total+1).join('x').split('').map((e,i) => i+1);
-  })
 });
