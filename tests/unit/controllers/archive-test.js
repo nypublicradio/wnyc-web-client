98c05891f4d9ce7b9e53f8ef9ee0ac86c95ab6ce
Brian Whitton <me@brianwhitton.com>
2017-05-30 10:47:43 -0400

    adds needs values for archive controller and route tests

--- tests/unit/controllers/archive-test.js
+++ tests/unit/controllers/archive-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('controller:archive', 'Unit | Controller | archive', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:metrics', 'service:data-pipeline', 'service:session']
 });
 
 // Replace this with your real tests.
