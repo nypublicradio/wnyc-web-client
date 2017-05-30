6e0d129b960020928c5d379c63bfa5cf448aaa43
Brian Whitton <me@brianwhitton.com>
2017-05-30 00:21:29 -0400

    just use existing page-numbers component for page numbers

--- app/story/serializer.js
+++ app/story/serializer.js
@@ -19,38 +19,4 @@ export default DS.JSONAPISerializer.extend({
   //   };
   //   return this._super(store, primaryModelClass, transformed, id, requestType);
   // }
-  normalizeQueryResponse(store, modelClass, payload) {
-    const result = this._super(...arguments);
-    result.meta = result.meta || {};
-
-    if (payload.links) {
-      result.meta.pagination = this.createPageMeta(payload.links);
-    }
-
-    return result;
-  },
-  createPageMeta(data) {
-
-    let meta = {};
-
-    Object.keys(data).forEach(type => {
-      const link = data[type];
-      meta[type] = {};
-      let a = document.createElement('a');
-      a.href = link;
-
-      a.search.slice(1).split('&').forEach(pairs => {
-        const [param, value] = pairs.split('=');
-
-        if (param === 'page') {
-          meta[type].number = parseInt(value);
-        }
-
-      });
-      a = null;
-    });
-
-    return meta;
-
-  }
 });
