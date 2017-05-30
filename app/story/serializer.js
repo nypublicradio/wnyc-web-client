5a2f81c9ffd4d5fad82ad199b7a25872e1c8e39e
Brian Whitton <me@brianwhitton.com>
2017-05-30 10:47:13 -0400

    update adapter and serializer for v3 transition

--- app/story/serializer.js
+++ app/story/serializer.js
@@ -3,18 +3,22 @@ import DS from 'ember-data';
 export default DS.JSONAPISerializer.extend({
   keyForAttribute(key) { return key; },
   payloadKeyFromModelName() { return 'story'; },
-  // normalizeQueryResponse(store, primaryModelClass, payload, id, requestType) {
-  //   let transformed = {
-  //     data: payload.results.map(result => {
-  //       let id = result.id;
-  //       delete result.id;
-  //       return {
-  //         id,
-  //         type: 'story',
-  //         attributes: result
-  //       };
-  //     })
-  //   };
-  //   return this._super(store, primaryModelClass, transformed, id, requestType);
-  // }
+  normalizeQueryResponse(store, primaryModelClass, {results}, id, requestType) {
+    if (results) {
+      let transformed = {
+        data: results.map(result => {
+          let id = result.id;
+          delete result.id;
+          return {
+            id,
+            type: 'story',
+            attributes: result
+          };
+        })
+      };
+      return this._super(store, primaryModelClass, transformed, id, requestType);
+    } else {
+      return this._super(...arguments);
+    }
+  }
 });
