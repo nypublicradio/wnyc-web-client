5f9a2429c56fd51864cf8d52cce02d617737e6b6
Diana Parrington <diana.parrington@gmail.com>
2017-05-31 12:24:11 -0400

    add audio, use segments component

--- app/story/route.js
+++ app/story/route.js
@@ -12,6 +12,7 @@ export default Ember.Route.extend(PlayParamMixin, {
   googleAds:    service(),
   dataPipeline: service(),
   currentUser:  service(),
+  audio:      service(),
   
   titleToken(model) {
     return `${get(model, 'story.title')} - ${get(model, 'story.headers.brand.title')}`;
@@ -43,6 +44,7 @@ export default Ember.Route.extend(PlayParamMixin, {
     controller.set('isMobile', window.Modernizr.touchevents);
     controller.set('session', get(this, 'session'));
     controller.set('user', get(this, 'currentUser.user'));
+    controller.set('audio', get(this, 'audio'));
     return this._super(...arguments);
   },
   
