nypublicradio/wqxr-web-client

Add deauthenticating routes

tests/acceptance/reset-test.js
@@ -2,7 +2,7 @@ import { test } from 'qunit';
 import moduleForAcceptance from 'wqxr-web-client/tests/helpers/module-for-acceptance';
 import { Response } from 'ember-cli-mirage';
 import config from 'wqxr-web-client/config/environment';
-import { currentSession } from 'wqxr-web-client/tests/helpers/ember-simple-auth';
+import { authenticateSession, currentSession } from 'wqxr-web-client/tests/helpers/ember-simple-auth';
 
 moduleForAcceptance('Acceptance | reset');
 
@@ -18,6 +18,18 @@ test('visiting /reset with no params', function(assert) {
 
   andThen(() => {
     assert.equal(currentURL(), '/forgot', 'it should redirect to forgot password page if you go to /reset with no params');
+  });
+});
+
+test('visiting /reset deauthenticates but remains on page', function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+
+  visit(resetUrlWithEmailAndConfirmation);
+
+  return andThen(() => {
+    assert.notOk(currentSession(this.application).get('isAuthenticated'));
+    assert.equal(currentURL(), resetUrlWithEmailAndConfirmation);
   });
 });
 
@@ -64,6 +76,33 @@ test('visiting /reset and resetting password and logging in', function(assert) {
   });
 });
 
+test('visiting /reset and resetting password and logging in still works when starting logged in', function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+
+  visit(resetUrlWithEmailAndConfirmation);
+
+  andThen(() => {
+    assert.equal(currentURL(), resetUrlWithEmailAndConfirmation);
+    assert.equal(find('.account-form-heading').text().trim(), 'Reset your password', 'it should show the reset password form');
+  });
+
+  fillIn('input[name=password]', password);
+  click('button:contains(Reset password)');
+
+  andThen(() => {
+    assert.equal(find('.account-form-heading').text().trim(), 'Log in to WNYC', 'it should show a login form when you click reset password with a good email code and password');
+  });
+
+  fillIn('input[name=email]', email);
+  fillIn('input[name=password]', password);
+
+  click('button[type=submit]:contains(Log in)');
+
+  andThen(() => {
+    assert.ok(currentSession(this.application).get('isAuthenticated'));
+  });
+});
 
 test('visiting /reset and getting a server error when submitting the form', function(assert) {
   server.post(`${config.wnycAuthAPI}/v1/confirm/password-reset`, () => {

tests/acceptance/validate-test.js
@@ -1,6 +1,6 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'wqxr-web-client/tests/helpers/module-for-acceptance';
-import { currentSession } from 'wqxr-web-client/tests/helpers/ember-simple-auth';
+import { authenticateSession, currentSession } from 'wqxr-web-client/tests/helpers/ember-simple-auth';
 // import { Response } from 'ember-cli-mirage';
 // import config from 'wqxr-web-client/config/environment';
 
@@ -12,6 +12,17 @@ test('visiting /validate with no params', function(assert) {
   return andThen(() => {
     assert.equal(currentURL(), '/validate');
     assert.equal(find('h2').text().trim(), 'Oops!');
+  });
+});
+
+test('visiting /validate deauthenticates but remains on page', function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+  visit('/validate');
+
+  return andThen(() => {
+    assert.notOk(currentSession(this.application).get('isAuthenticated'));
+    assert.equal(currentURL(), '/validate');
   });
 });
 
@@ -34,3 +45,25 @@ test('visiting /validate and logging in', function(assert) {
     assert.ok(currentSession(this.application).get('isAuthenticated'));
   });
 });
+
+test('visiting /validate and logging in even when starting logged in', function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+
+  visit('/validate?username=test&confirmation=123');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/validate?username=test&confirmation=123');
+    assert.equal(find('.alert-success').text().trim(), 'Your email has been verified and your online account is now active.');
+    assert.equal(find('h2').text().trim(), 'Log in to WNYC');
+  });
+
+  fillIn('input[name=email]', 'user@example.com');
+  fillIn('input[name=password]', 'password1');
+  click('button[type=submit]:contains(Log in)');
+
+  return andThen(() => {
+    assert.ok(currentSession(this.application).get('isAuthenticated'));
+  });
+});
+

nypublicradio/wnyc-web-client

tests/acceptance/reset-test.js
@@ -2,7 +2,7 @@ import { test } from 'qunit';
 import moduleForAcceptance from 'wnyc-web-client/tests/helpers/module-for-acceptance';
 import { Response } from 'ember-cli-mirage';
 import config from 'wnyc-web-client/config/environment';
-import { currentSession } from 'wnyc-web-client/tests/helpers/ember-simple-auth';
+import { authenticateSession, currentSession } from 'wnyc-web-client/tests/helpers/ember-simple-auth';
 
 moduleForAcceptance('Acceptance | reset');
 
@@ -18,6 +18,18 @@ test('visiting /reset with no params', function(assert) {
 
   andThen(() => {
     assert.equal(currentURL(), '/forgot', 'it should redirect to forgot password page if you go to /reset with no params');
+  });
+});
+
+test('visiting /reset deauthenticates but remains on page', function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+
+  visit(resetUrlWithEmailAndConfirmation);
+
+  return andThen(() => {
+    assert.notOk(currentSession(this.application).get('isAuthenticated'));
+    assert.equal(currentURL(), resetUrlWithEmailAndConfirmation);
   });
 });
 
@@ -63,6 +75,33 @@ test('visiting /reset and resetting password and logging in', function(assert) {
   });
 });
 
+test('visiting /reset and resetting password and logging in still works when starting logged in', function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+
+  visit(resetUrlWithEmailAndConfirmation);
+
+  andThen(() => {
+    assert.equal(currentURL(), resetUrlWithEmailAndConfirmation);
+    assert.equal(find('.account-form-heading').text().trim(), 'Reset your password', 'it should show the reset password form');
+  });
+
+  fillIn('input[name=password]', password);
+  click('button:contains(Reset password)');
+
+  andThen(() => {
+    assert.equal(find('.account-form-heading').text().trim(), 'Log in to WNYC', 'it should show a login form when you click reset password with a good email code and password');
+  });
+
+  fillIn('input[name=email]', email);
+  fillIn('input[name=password]', password);
+
+  click('button[type=submit]:contains(Log in)');
+
+  andThen(() => {
+    assert.ok(currentSession(this.application).get('isAuthenticated'));
+  });
+});
 
 test('visiting /reset and getting a server error when submitting the form', function(assert) {
   server.post(`${config.wnycAuthAPI}/v1/confirm/password-reset`, () => {

tests/acceptance/validate-test.js
@@ -1,6 +1,6 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'wnyc-web-client/tests/helpers/module-for-acceptance';
-import { currentSession } from 'wnyc-web-client/tests/helpers/ember-simple-auth';
+import { authenticateSession, currentSession } from 'wnyc-web-client/tests/helpers/ember-simple-auth';
 // import { Response } from 'ember-cli-mirage';
 // import config from 'wnyc-web-client/config/environment';
 
@@ -12,6 +12,17 @@ test('visiting /validate with no params', function(assert) {
   return andThen(() => {
     assert.equal(currentURL(), '/validate');
     assert.equal(find('h2').text().trim(), 'Oops!');
+  });
+});
+
+test('visiting /validate deauthenticates but remains on page', function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+  visit('/validate');
+
+  return andThen(() => {
+    assert.notOk(currentSession(this.application).get('isAuthenticated'));
+    assert.equal(currentURL(), '/validate');
   });
 });
 
@@ -33,3 +44,25 @@ test('visiting /validate and logging in', function(assert) {
     assert.ok(currentSession(this.application).get('isAuthenticated'));
   });
 });
+
+test('visiting /validate and logging in even when starting logged in', function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+
+  visit('/validate?username=test&confirmation=123');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/validate?username=test&confirmation=123');
+    assert.equal(find('.alert-success').text().trim(), 'Your email has been verified and your online account is now active.');
+    assert.equal(find('h2').text().trim(), 'Log in to WNYC');
+  });
+
+  fillIn('input[name=email]', 'user@example.com');
+  fillIn('input[name=password]', 'password1');
+  click('button[type=submit]:contains(Log in)');
+
+  return andThen(() => {
+    assert.ok(currentSession(this.application).get('isAuthenticated'));
+  });
+});
+