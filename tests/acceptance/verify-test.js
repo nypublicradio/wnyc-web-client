28602c53bd6c4b706827e18fd27670c68a7b3f90
walsh9 <walsh9@gmail.com>
2017-06-05 12:15:32 -0400

    Fix verify tests

--- tests/acceptance/verify-test.js
+++ tests/acceptance/verify-test.js
@@ -2,7 +2,11 @@ import { test } from 'qunit';
 import moduleForAcceptance from 'wqxr-web-client/tests/helpers/module-for-acceptance';
 import { authenticateSession } from 'wqxr-web-client/tests/helpers/ember-simple-auth';
 
-moduleForAcceptance('Acceptance | verify');
+moduleForAcceptance('Acceptance | verify', {
+  beforeEach() {
+    server.create('stream');
+  }
+});
 
 test('visiting /verify unauthenticated shows a login form', function(assert) {
   visit('/verify');
