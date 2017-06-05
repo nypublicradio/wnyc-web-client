nypublicradio/wqxr-web-client

Add tests for /login and /signup when authenticated

tests/acceptance/login-test.js
@@ -3,7 +3,7 @@ import djangoPage from 'wqxr-web-client/tests/pages/django-page';
 import moduleForAcceptance from 'wqxr-web-client/tests/helpers/module-for-acceptance';
 import { Response } from 'ember-cli-mirage';
 import config from 'wqxr-web-client/config/environment';
-import { currentSession } from 'wqxr-web-client/tests/helpers/ember-simple-auth';
+import { authenticateSession, currentSession } from 'wqxr-web-client/tests/helpers/ember-simple-auth';
 import 'wqxr-web-client/tests/helpers/with-feature';
 import dummySuccessProviderFb from 'wqxr-web-client/tests/helpers/torii-dummy-success-provider-fb';
 import dummyFailureProvider from 'wqxr-web-client/tests/helpers/torii-dummy-failure-provider';
@@ -20,6 +20,17 @@ test('visiting /login', function(assert) {
 
   andThen(() => {
     assert.equal(currentURL(), '/login');
+  });
+});
+
+test("can't visit /login when authenticated", function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+
+  visit('/login');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/');
   });
 });
 

tests/acceptance/signup-test.js
@@ -1,7 +1,7 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'wqxr-web-client/tests/helpers/module-for-acceptance';
 import 'wqxr-web-client/tests/helpers/with-feature';
-import { currentSession } from 'wqxr-web-client/tests/helpers/ember-simple-auth';
+import { authenticateSession, currentSession } from 'wqxr-web-client/tests/helpers/ember-simple-auth';
 import dummySuccessProviderFb from 'wqxr-web-client/tests/helpers/torii-dummy-success-provider-fb';
 import dummyFailureProvider from 'wqxr-web-client/tests/helpers/torii-dummy-failure-provider';
 import { registerMockOnInstance } from 'wqxr-web-client/tests/helpers/register-mock';
@@ -17,6 +17,17 @@ test('visiting /signup', function(assert) {
 
   andThen(() => {
     assert.equal(currentURL(), '/signup');
+  });
+});
+
+test("can't visit /signup when authenticated", function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+
+  visit('/signup');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/');
   });
 });
 

nypublicradio/wnyc-web-client

tests/acceptance/login-test.js
@@ -3,7 +3,7 @@ import djangoPage from 'wnyc-web-client/tests/pages/django-page';
 import moduleForAcceptance from 'wnyc-web-client/tests/helpers/module-for-acceptance';
 import { Response } from 'ember-cli-mirage';
 import config from 'wnyc-web-client/config/environment';
-import { currentSession } from 'wnyc-web-client/tests/helpers/ember-simple-auth';
+import { authenticateSession, currentSession } from 'wnyc-web-client/tests/helpers/ember-simple-auth';
 import 'wnyc-web-client/tests/helpers/with-feature';
 import dummySuccessProviderFb from 'wnyc-web-client/tests/helpers/torii-dummy-success-provider-fb';
 import dummyFailureProvider from 'wnyc-web-client/tests/helpers/torii-dummy-failure-provider';
@@ -20,6 +20,17 @@ test('visiting /login', function(assert) {
 
   andThen(() => {
     assert.equal(currentURL(), '/login');
+  });
+});
+
+test("can't visit /login when authenticated", function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+
+  visit('/login');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/');
   });
 });
 

tests/acceptance/signup-test.js
@@ -1,7 +1,7 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'wnyc-web-client/tests/helpers/module-for-acceptance';
 import 'wnyc-web-client/tests/helpers/with-feature';
-import { currentSession } from 'wnyc-web-client/tests/helpers/ember-simple-auth';
+import { authenticateSession, currentSession } from 'wnyc-web-client/tests/helpers/ember-simple-auth';
 import dummySuccessProviderFb from 'wnyc-web-client/tests/helpers/torii-dummy-success-provider-fb';
 import dummyFailureProvider from 'wnyc-web-client/tests/helpers/torii-dummy-failure-provider';
 import { registerMockOnInstance } from 'wnyc-web-client/tests/helpers/register-mock';
@@ -17,6 +17,17 @@ test('visiting /signup', function(assert) {
 
   andThen(() => {
     assert.equal(currentURL(), '/signup');
+  });
+});
+
+test("can't visit /signup when authenticated", function(assert) {
+  server.create('user');
+  authenticateSession(this.application, {access_token: 'foo'});
+
+  visit('/signup');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/');
   });
 });
 