d092ea738c73e705340bf61722d736b759e4b3c3
walsh9 <walsh9@gmail.com>
2017-06-05 13:03:43 -0400

    Update environment

--- config/environment.js
+++ config/environment.js
@@ -113,6 +113,7 @@ module.exports = function(environment) {
     moreShowsAPIKey: "mammoth",
     wnycAdminRoot: process.env.WQXR_ADMIN_ROOT,
     wnycAuthAPI: process.env.AUTH_SERVICE,
+    wnycMembershipAPI: process.env.MEMBERSHIP_SERVICE,
     wnycAccountRoot: process.env.WQXR_ACCOUNT_ROOT,
     wnycEtagAPI: process.env.WQXR_ETAG_API,
     wnycStaticURL: process.env.WQXR_STATIC_URL,
@@ -124,6 +125,7 @@ module.exports = function(environment) {
     wnycBetaURL: process.env.WNYC_BETA_URL,
     featureFlags: {
       'social-auth': process.env.SOCIAL_AUTH,
+      'member-center': process.env.MEMBER_CENTER,
     },
     betaTrials: {
       betaInviteLanding: '#full-page-transforms-wrapper',
