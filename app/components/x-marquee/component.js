f86d63f84be05c5dfb0d64b1e557e965b17ffeb2
Brian Whitton <me@brianwhitton.com>
2017-06-06 18:29:21 -0400

    use a getter on the marquee; might be undefined

--- app/components/x-marquee/component.js
+++ app/components/x-marquee/component.js
@@ -11,7 +11,7 @@ export default Component.extend({
 
     let bgColor = get(this, 'model.bgColor') || 'white';
     let marquee = get(this, 'model.marqueeImage');
-    let urlString = imageTemplate([marquee.template, 1200,  200, marquee.crop, 99]);
+    let urlString = imageTemplate([get(marquee, 'template'), 1200,  200, get(marquee, 'crop'), 99]);
 
     set(this, 'marqueeImageCSS', htmlSafe(`background: url(${urlString}) no-repeat center center ${bgColor};`));
     set(this, 'marqueeGradientCSS', htmlSafe(`background-image: radial-gradient(rgba(255, 255, 255,0) 200px, ${bgColor} 620px);`));
