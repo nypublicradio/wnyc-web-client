54b3f979c4ab55fe0f1c34ba64388ad5d35aa7ad
walsh9 <walsh9@gmail.com>
2017-06-06 15:58:58 -0400

    Rewrite as a task. Promises were getting too tangled.

--- app/authenticators/torii.js
+++ app/authenticators/torii.js
@@ -1,48 +1,54 @@
+/* global FB */
 import Torii from 'ember-simple-auth/authenticators/torii';
 import service from 'ember-service/inject';
 import fetch from 'fetch';
 import RSVP from 'rsvp';
-import config from 'wqxr-web-client/config/environment';
-import { decamelizeKeys } from 'wqxr-web-client/helpers/decamelize-keys';
+import config from 'wnyc-web-client/config/environment';
+import { decamelizeKeys } from 'wnyc-web-client/helpers/decamelize-keys';
+import { task } from 'ember-concurrency';
 
 export default Torii.extend({
   torii: service(),
+
   authenticate() {
     return this._super(...arguments)
     .then((data) => {
-      return RSVP.all([
-        data,
-        this.fbAPI(`/${data.userId}/permissions`)
-      ]);
-    })
-    .then(([data, permissions]) => {
-      if (permissions) {
-        data.permissions = permissions.data.reduce((result, p) => {
-          if (p && p.permission) {
-            result[p.permission] = p.status;
-          }
-          return result;
-        }, {});
-      }
-      return RSVP.all([
-        data,
-        this.getSession(data.provider, data.accessToken)
-      ]);
-    })
-    .then(([data, response]) => {
-      data = decamelizeKeys([data]);
-      if (response && response.ok) {
-        return data;
-      } else {
-        return RSVP.reject(response, data);
-      }
+      return this.get('authTask').perform(data);
     });
   },
 
+  authTask: task(function * (data) {
+    try {
+      let permissions = yield this.fbAPI(`/${data.userId}/permissions`);
+      data = this.attachPermissions(data, permissions);
+      data = decamelizeKeys([data]);
+      let response = this.getSession(data.provider, data.accessToken);
+      if (response && response.ok) {
+        return data;
+      } else {
+        throw { error: 'Unauthorized', data: data};
+      }
+    } catch(e) {
+      throw {error: 'Unauthorized', data: data};
+    }
+  }),
+
   fbAPI(url) {
-    return new RSVP.Promise(function(resolve, reject) {
+    return new RSVP.Promise(function(resolve/*, reject*/) {
       FB.api(url, response => resolve(response));
     });
+  },
+
+  attachPermissions(data, permissions) { // modifies data
+    if (permissions) {
+      data.permissions = permissions.data.reduce((result, p) => {
+        if (p && p.permission) {
+          result[p.permission] = p.status;
+        }
+        return result;
+      }, {});
+    }
+    return data;
   },
 
   getSession(provider, accessToken) {
