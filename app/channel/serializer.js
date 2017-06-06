48975d73f9c2405a0f5dde2568cd408c547f9ac2
Brian Whitton <me@brianwhitton.com>
2017-06-06 18:27:17 -0400

    attempting to use relationships in channel model

--- app/channel/serializer.js
+++ app/channel/serializer.js
@@ -3,11 +3,11 @@ import DS from 'ember-data';
 export default DS.JSONAPISerializer.extend({
   keyForAttribute: key => key,
   keyForRelationship: key => key,
-  normalizeResponse(store, typeClass, payload, id) {
-    let featuredStory = payload.data.attributes.featured;
-    delete payload.data.attributes.featured;
+  normalizeResponse(store, typeClass, payload, id, requestType) {
+    let { logoImage, marqueeImage, featured, about } = payload.data.attributes;
+    // let featuredStory = payload.data.attributes.featured;
+    // delete payload.data.attributes.featured;
     payload.included = payload.included || [];
-
     // id will have a trailing slash because it is derived from the URL and we
     // reliably append a trailing slash via Django
     payload.included.push({
@@ -24,22 +24,47 @@ export default DS.JSONAPISerializer.extend({
     {
       type: 'about-page',
       id: `${id}about`,
-      attributes: payload.data.attributes.about
+      attributes: about
+    },
+    {
+      type: 'image',
+      id: logoImage ? logoImage.id : null,
+      attributes: logoImage
+    },
+    {
+      type: 'image',
+      id: marqueeImage ? marqueeImage.id : null,
+      attributes: marqueeImage
+    },
+    {
+      type: 'story',
+      id: featured ? featured.id : null,
+      attributes: featured
     });
 
-    if (featuredStory) {
-      this.store.push({data: {attributes: featuredStory, type: 'story', id: featuredStory.id}});
-
-      payload.data.relationships = {
-        featured: {
-          data: {
-            type: 'story',
-            id: featuredStory.id
-          }
-        }
+    payload.included = payload.included.map(r => {
+      let { attributes, type, id } = r;
+      let attrs = {};
+      if (attributes) {
+        Object.keys(attributes).forEach(k => attrs[k.dasherize()] = attributes[k]);
+      }
+      return {
+        id,
+        type: type.dasherize(),
+        attributes: attrs 
       };
-    }
-    return payload;
+    });
+
+    return this._super(store, typeClass, payload, id, requestType);
+  },
+  extractRelationships(modelClass, hash) {
+    let { logoImage, marqueeImage, featured } = hash.attributes;
+    let relationshps = {
+      logoImage: {data: {type: 'image', id: String(logoImage.id) }},
+      marqueeImage: {data: {type: 'image', id: String(marqueeImage.id) }},
+      featured: {data: {type: 'story', id: String(featured.id) }},
+      about: {data: {type: 'api-response', id: `${hash.id}about/1`}}
+    };
+    return relationshps;
   }
 });
-
