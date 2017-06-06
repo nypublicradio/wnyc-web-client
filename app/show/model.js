ac9eb67674529559e6807c5dac23415f362edea0
Brian Whitton <me@brianwhitton.com>
2017-06-06 18:24:02 -0400

    migrate from `shows` pod to `show` pod
    and use `show-detail` as abstract route name

--- app/show/model.js
+++ app/show/model.js
@@ -1,7 +1,4 @@
-import DS from 'ember-data';
 import ChannelModel from 'wnyc-web-client/channel/model';
 
-export default ChannelModel.extend({
-  image: DS.belongsTo('image'),
-  producingOrganizations: DS.hasMany('producing-organization')
-});
+export default ChannelModel;
+
