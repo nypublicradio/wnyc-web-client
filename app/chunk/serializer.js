28099a66c6c98a8d4eab3db5d163a777b355ab55
Brian Whitton <me@brianwhitton.com>
2017-06-06 18:26:00 -0400

    serializer clean ups
    trying to move to a place where serializers expect dasherized name
    
    channel model is still manually built, but the rest should be handled by
    the framework

--- app/chunk/serializer.js
+++ app/chunk/serializer.js
@@ -1,7 +1,5 @@
 import JSONAPISerializer from 'ember-data/serializers/json-api';
 
 export default JSONAPISerializer.extend({
-  extractId(modelClass, {attributes}) {
-    return attributes.slug;
-  }
+  extractId: (modelClass, {attributes}) => attributes.slug,
 });
