6575c799012ecabd0eef66c297dc7b8887f34673
Brian Whitton <me@brianwhitton.com>
2017-06-07 17:45:01 -0400

    update listing pages to to conform to dasherized keys from server
    about-page is dasherized as a relationship
    all the included records (usually stories) all need their attribute keys
    transformed. We're not consuming story relationships  (images, producing orgs, etc)
    yet because compensating for that here gets too intense.
    
    ideally when we implement relationships on stories, we should also
    update listing-pages to work with JSON:API output

--- app/api-response/serializer.js
+++ app/api-response/serializer.js
@@ -1,6 +1,17 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  keyForRelationship: key => key,
-  keyForAttribute: key => key
+  normalizeResponse(store, typeClass, payload, id, requestType) {
+    payload.included = payload.included || [];
+    payload.included = payload.included.map(r => {
+      let { attributes, type, id, relationships } = r;
+      // story serializer expects keys dasherized
+      let attrs = {};
+      if (attributes) {
+        Object.keys(attributes).forEach(k => attrs[k.dasherize()] = attributes[k]);
+      }
+      return {type, id, attributes: attrs, relationships};
+    });
+    return this._super(store, typeClass, payload, id, requestType);
+  }
 });
--- app/channel/serializer.js
+++ app/channel/serializer.js
@@ -14,7 +14,7 @@ export default DS.JSONAPISerializer.extend({
       type: 'api-response',
       id: `${id}about/1`,
       relationships: {
-        aboutPage: {
+        'about-page': {
           data: {
             type: 'about-page', id: `${id}about`
           }
@@ -26,9 +26,30 @@ export default DS.JSONAPISerializer.extend({
       id: `${id}about`,
       attributes: payload.data.attributes.about
     });
+    
+    payload.included = payload.included.map(r => {
+      let { attributes, type } = r;
+      if (type === 'api-response') {
+        return r;
+      }
+
+      // story serializer expects keys dasherized
+      if (attributes) {
+        r.attributes = {};
+        Object.keys(attributes).forEach(k => r.attributes[k.dasherize()] = attributes[k]);
+      }
+      return r;
+    });
 
     if (featuredStory) {
-      this.store.push({data: {attributes: featuredStory, type: 'story', id: featuredStory.id}});
+      let story = {
+        type: 'story',
+        id: featuredStory.id,
+        attributes: {}
+      };
+      Object.keys(featuredStory).forEach(k => story.attributes[k.dasherize()] = featuredStory[k]);
+      
+      payload.included.push(story);
 
       payload.data.relationships = {
         featured: {
