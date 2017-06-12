ad2c6cf84c7710d91f309a1b91034273ce8255f1
walsh9 <walsh9@gmail.com>
2017-06-12 16:51:57 -0400

    Update torii authenticator

--- app/authenticators/torii.js
+++ app/authenticators/torii.js
@@ -4,33 +4,32 @@ import fetch from 'fetch';
 import RSVP from 'rsvp';
 import config from 'wnyc-web-client/config/environment';
 import { decamelizeKeys } from 'wnyc-web-client/helpers/decamelize-keys';
-import { task } from 'ember-concurrency';
 
 export default Torii.extend({
   torii: service(),
 
   authenticate() {
-    return this._super(...arguments)
-    .then(providerData => {
-      return new RSVP.Promise(resolve => {
-        let user = this.get('authenticateSession').perform(providerData);
-        resolve(user);
+    return new RSVP.Promise((resolve, reject) => {
+      return this._super(...arguments)
+      .then((data) => {
+        return RSVP.all([
+          data,
+          this.getSession(data.provider, data.accessToken)
+        ]);
+      })
+      .then(([data, response]) => {
+        if (response) {
+          if (response.ok) {
+            resolve(decamelizeKeys([data]));
+          } else if (response.status < 500) {
+            response.json().then(reject);
+          } else {
+            reject({ "errors": {"code": "serverError"} });
+          }
+        }
       });
     });
   },
-
-  authenticateSession: task(function * (data) {
-    let response = yield this.getSession(data.provider, data.accessToken);
-    if (response) {
-      if (response.ok) {
-        return decamelizeKeys([data]);
-      } else if (response.status < 500) {
-        return RSVP.reject(response.json());
-      } else {
-        return RSVP.reject({ "errors": {"code": "serverError"} });
-      }
-    }
-  }),
 
   getSession(provider, accessToken) {
     return fetch(`${config.wnycAuthAPI}/v1/session`, {
