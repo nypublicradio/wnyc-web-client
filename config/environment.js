413dd3c8f358e7042871beee87f3082c63ec06c4
Brian Whitton <me@brianwhitton.com>
2017-06-13 18:08:31 -0400

    no utm for donate

--- config/environment.js
+++ config/environment.js
@@ -123,7 +123,7 @@ module.exports = function(environment) {
     wnycStaticURL: process.env.WNYC_STATIC_URL,
     wnycURL: process.env.WNYC_URL,
     platformEventsAPI: process.env.PLATFORM_EVENTS_SERVICE,
-    wnycDonateURL: 'https://pledge3.wnyc.org/donate/main/onestep/?utm_source=wnyc&utm_medium=wnyc-86x27&utm_content=wnyc-header-beta&utm_campaign=pledge',
+    wnycDonateURL: 'https://pledge3.wnyc.org/donate/main/onestep/',
     wnycSvgURL: '/media/svg/',
     // put beta host at the root so it can be overridden by Django
     wnycBetaURL: process.env.WNYC_BETA_URL,
