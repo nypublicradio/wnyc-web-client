e02ce733d63fb5a3660a3def1106fb93257951d0
Diana Parrington <diana.parrington@gmail.com>
2017-06-22 15:49:02 -0400

    mods for new API, camelize obj helper'

--- app/story/serializer.js
+++ app/story/serializer.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import { camelizeObj } from 'wqxr-web-client/helpers/camelize-object';
 
 export function dasherizeKeys(attributes) {
   let attrs = {};
@@ -8,6 +9,26 @@ export function dasherizeKeys(attributes) {
   return attrs;
 }
 
+//dasherized versions of names in model bc they haven't been processsed yet
+const propertiesWithChildren = [
+'chunks',
+'headers',
+'image-main',
+'playlist',
+'producing-organizations',
+'segments',
+];
+
 export default DS.JSONAPISerializer.extend({
   extractId: (modelClass, {attributes}) => attributes.slug,
+
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+
+    for (var prop of propertiesWithChildren) {
+      payload.data.attributes[prop] = camelizeObj(payload.data.attributes[prop]);
+    }
+
+    return this._super(store, primaryModelClass, payload, id, requestType);
+  },
+
 });
