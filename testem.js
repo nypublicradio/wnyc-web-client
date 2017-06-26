c57a7b6b237d725d2c6e38a17b644adae0f2dbdc
walsh9 <walsh9@gmail.com>
2017-06-26 10:49:56 -0400

    Add xml report output in circle ci to test config

--- testem.js
+++ testem.js
@@ -1,10 +1,25 @@
-/* eslint-env node */
+/*jshint node:true*/
+function reportFile() {
+  if (_circleTestFolder()) {
+    return _circleTestFolder() + '/test.xml';
+  }
+}
+
+function testReporter() {
+  return _circleTestFolder() ? 'xunit' : 'tap';
+}
+
+function _circleTestFolder() {
+  return process.env['CIRCLE_TEST_REPORTS'];
+}
+
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
     'Chrome'
   ],
+<<<<<<< HEAD
   launch_in_dev: [
     'Firefox',
     'Chrome'
@@ -16,5 +31,12 @@ module.exports = {
       '--remote-debugging-port=9222',
       '--window-size=1440,900'
     ]
-  }
+  },
+  "launch_in_dev": [
+    "Chrome",
+    "Firefox"
+  ],
+  "reporter": testReporter(),
+  "report_file": reportFile(),
+  "xunit_intermediate_output": true
 };
