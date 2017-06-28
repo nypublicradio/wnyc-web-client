6d30004362365b5b025339e50af7c8b29eb2ef68
Diana Parrington <diana.parrington@gmail.com>
2017-06-28 12:22:37 -0400

    process slideshow in serializer

--- app/story/serializer.js
+++ app/story/serializer.js
@@ -19,7 +19,8 @@ const propertiesWithChildren = [
 'producing-organizations',
 'segments',
 'series',
-'show-producing-orgs'
+'show-producing-orgs',
+'slideshow'
 ];
 
 export default DS.JSONAPISerializer.extend({
