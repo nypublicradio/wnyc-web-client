ff7eac72f63830bec25115190ba3831265892cc9
Brian Whitton <me@brianwhitton.com>
2017-07-18 10:24:58 -0400

    no more legacy services

--- app/mixins/legacy-support.js
+++ app/mixins/legacy-support.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 const {
   Mixin,
-  inject,
-  get,
   $
 } = Ember;
 
@@ -14,21 +12,10 @@ function legacySelector(target) {
 }
 
 export default Mixin.create({
-  legacyServices: inject.service(),
-
   isLegacyEvent(event) {
     return !!legacySelector(event.target).length;
   },
-  fireLegacyEvent(target) {
-    let legacy = get(this, 'legacyServices');
-    let legacyTarget = legacySelector(target);
-    let { itemPK, itemTitle } = legacyTarget.data('ember-args');
-    if (legacyTarget.hasClass('js-listen')) {
-      return legacy.listen(itemPK, itemTitle);
-    }
-    if (legacyTarget.hasClass('js-queue')) {
-      return legacy.queue(itemPK);
-    }
+  fireLegacyEvent() {
   },
   revealStaffLinks($element, adminRoot) {
     $element.find('.stf').each(function() {
@@ -45,23 +32,9 @@ export default Mixin.create({
     });
   },
   actions: {
-    listen(model, streamSlug) {
-      const id = get(model, 'id');
-      const title = get(model, 'title');
-      const show = get(model, 'headers.brand.title');
-      const legacy = get(this, 'legacyServices');
-
-      if (streamSlug) {
-        legacy.streamStation(streamSlug);
-      } else {
-        legacy.listen(id, title, show);
-      }
+    listen(/*model, streamSlug*/) {
     },
-    queue(model) {
-      const id = get(model, 'id');
-      const legacy = get(this, 'legacyServices');
-
-      legacy.queue(id);
+    queue(/*model*/) {
     }
   }
 });
