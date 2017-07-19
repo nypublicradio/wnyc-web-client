fdca3fee895ca272a99c698660d48c861fdfa3ce
Brian Whitton <me@brianwhitton.com>
2017-07-19 11:29:56 -0400

    pass in background to stream banner

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,7 @@ import DS from 'ember-data';
 import RSVP from 'rsvp';
 import get from 'ember-metal/get';
 
-const carouselBg = 'https://images.unsplash.com/photo-1481462585914-9f695507135e?dpr=2&auto=format&fit=crop&w=1500&h=1500&q=80&cs=tinysrgb';
+const STREAM_BG = '/assets/img/backgrounds/streambanner.jpg';
 
 export default Route.extend(PlayParamMixin, {
   googleAds:  service(),
@@ -35,7 +35,7 @@ export default Route.extend(PlayParamMixin, {
       })
     });
     controller.set('streams', streams);
-    controller.set('carouselBg', carouselBg);
+    controller.set('background', STREAM_BG);
     controller.set('audio', this.get('audio'));
   }
 });
