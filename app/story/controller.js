f7783634a211c98957395a2c08012e8b78e5b0db
Diana Parrington <diana.parrington@gmail.com>
2017-07-27 10:05:12 -0400

    commenting out init fn for now

--- app/story/controller.js
+++ app/story/controller.js
@@ -3,4 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams:  ['tab'],
   tab: 'summary',
+
+  // init(){
+  //   this._super(...arguments);
+  //   if (location.hash.substr(1) === "transcript"){
+  //     this.set("tab", 'transcript');
+  //   }
+  // }
 });
