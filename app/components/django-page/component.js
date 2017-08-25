b85dfd913a8e5b3dd58c49184a971159bbdee9de
Jeff Keen <jeff@keen.me>
2017-08-25 16:22:12 -0500

    More updates for django-for-ember addon

--- app/components/django-page/component.js
+++ app/components/django-page/component.js
@@ -1,83 +1,12 @@
 import Ember from 'ember';
-import service from 'ember-service/inject';
-import config from '../../config/environment';
-import LegacySupportMixin from 'wnyc-web-client/mixins/legacy-support';
+import Component from 'nypr-django-for-ember/components/django-page';
 import BetaActionsMixin from 'wnyc-web-client/mixins/beta-actions';
-import {
-  isInDom,
-  embeddedComponentSetup,
-  clearAlienDom,
-} from '../../lib/alien-dom';
+import service from 'ember-service/inject';
+const { get } = Ember;
 
-const { get, computed } = Ember;
-
-export default Ember.Component.extend(LegacySupportMixin, BetaActionsMixin, {
+export default Component.extend(BetaActionsMixin, {
   session: service(),
   legacyAnalytics: service(),
-  router: service('wnyc-routing'),
-  loadingType: computed('page', function() {
-    let id = get(this, 'page.id') || '';
-    let firstPart = id.split('/')[0];
-
-    switch(firstPart) {
-      case '':
-        return 'index';
-      case 'shows':
-      case 'articles':
-      case 'series':
-      case 'tags':
-      case 'blogs':
-        return 'channel';
-      case 'story':
-        return 'story';
-      default:
-        return 'legacy';
-    }
-  }),
-
-  didReceiveAttrs() {
-    // If we have a new page model, we want to clear any overlaid
-    // content when we rerender.
-    let page = this.get('page');
-    if (page !== this._lastPage) {
-      if (isInDom(page.get('id'))) {
-        embeddedComponentSetup();
-      }
-
-      this.set('showingOverlay', false);
-    }
-  },
-
-  didRender() {
-    let page = this.get('page');
-    if (page !== this._lastPage) {
-      this._lastPage = page;
-      let elt = this.$('.django-content');
-      elt.empty();
-
-      if (isInDom(page.get('id'))) {
-        clearAlienDom();
-      }
-
-      this.get('page').appendTo(elt).then(() => {
-        // After the server-rendered page has been inserted, we
-        // re-enable any overlaid content so that it can wormhole
-        // itself into the server-rendered DOM.
-        this.set('showingOverlay', true);
-
-        if (this.get('session.data.isStaff')) {
-          this.revealStaffLinks(this.$(), config.adminRoot);
-        }
-        this.$().imagesLoaded().progress((i, image) => {
-          Ember.run(() => {
-            image.img.classList.add('is-loaded');
-          });
-        });
-
-      });
-    }
-  },
-
   click(event) {
     let legacyAnalytics = get(this, 'legacyAnalytics');
     legacyAnalytics.dispatch(event);
@@ -85,5 +14,5 @@ export default Ember.Component.extend(LegacySupportMixin, BetaActionsMixin, {
     if (this.isLegacyEvent(event)) {
       return this.fireLegacyEvent(event.target);
     }
-  },
+  }
 });
--- app/components/nav-links/component.js
+++ app/components/nav-links/component.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import config from 'wnyc-web-client/config/environment';
-import { canonicalize } from 'wnyc-web-client/services/script-loader';
+import { canonicalize } from 'nypr-django-for-ember/services/script-loader';
 
 const {
   get,
