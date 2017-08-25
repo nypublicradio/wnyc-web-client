5c697ab0a008c2521423dcc6a19df0cd0c693af4
Jeff Keen <jeff@keen.me>
2017-08-25 16:22:12 -0500

    More updates for django-for-ember addon

--- app/components/nav-links/component.js
+++ app/components/nav-links/component.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import config from 'wqxr-web-client/config/environment';
-import { canonicalize } from 'wqxr-web-client/services/script-loader';
+import { canonicalize } from 'nypr-django-for-ember/services/script-loader';
 
 const {
   get,
