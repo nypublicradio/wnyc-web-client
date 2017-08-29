805a00c49819c962d293b7c9e9454108d32c6ae3
Jeff Keen <jeff@keen.me>
2017-08-29 10:17:39 -0500

    Remove some files that are now in nypr-publisher-lib

--- app/api-response/adapter.js
+++
@@ -1,9 +0,0 @@
-import ApplicationAdapter from '../adapters/application';
-
-export default ApplicationAdapter.extend({
-  pathForType() { return 'channel'; },
-  buildURL() {
-    let url = this._super(...arguments);
-    return decodeURIComponent(url);
-  }
-});
