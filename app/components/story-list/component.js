a1c27e2776c9248c24a48ac457f3b1925d445dbd
Jeff Keen <jeff@keen.me>
2017-08-29 10:17:39 -0500

    Remove some files that are now in nypr-publisher-lib

--- app/components/story-list/component.js
+++
@@ -1,9 +0,0 @@
-import Component from 'ember-component';
-import service from 'ember-service/inject';
-import config from 'wqxr-web-client/config/environment';
-
-export default Component.extend({
-  session: service(),
-  tagName: 'section',
-  adminURL: `${config.adminRoot}/admin`
-});
