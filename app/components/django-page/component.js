62ded6230c5277385734dcf9e735cb9fb4041f52
Jeff Keen <jeff@keen.me>
2017-09-01 17:25:15 -0500

    Be explicit about that template. No funny business.
    later edit: don't need beta actions in wqxr.

--- app/components/django-page/component.js
+++ app/components/django-page/component.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 import Component from 'nypr-django-for-ember/components/django-page';
-import BetaActionsMixin from 'wqxr-web-client/mixins/beta-actions';
+import layout from 'nypr-django-for-ember/templates/components/django-page';
 import service from 'ember-service/inject';
 const { get } = Ember;
 
-export default Component.extend(BetaActionsMixin, {
+export default Component.extend({
+  layout,
   session: service(),
   legacyAnalytics: service(),
   click(event) {
