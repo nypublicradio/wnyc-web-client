e81bf6eb75f76314249662510f1be15573606626
Brian Whitton <me@brianwhitton.com>
2017-09-14 22:29:18 -0400

    this is not used anymore

--- tests/pages/django-page.js
+++ tests/pages/django-page.js
@@ -1,6 +1,5 @@
 import PageObject from 'wnyc-web-client/tests/page-object';
 import { appendHTML } from 'wnyc-web-client/tests/helpers/html';
-import { alienDomClick } from 'nypr-django-for-ember/utils/alien-dom';
 let {
   visitable,
   //clickOnText,
@@ -11,9 +10,6 @@ let {
 
 export default PageObject.create({
   visit: visitable(':id'),
-  alienClick(selector) {
-    return alienDomClick(selector);
-  },
 
   bootstrap({id}) {
     /* eslint-disable */
