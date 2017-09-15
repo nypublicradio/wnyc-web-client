413757de16ecd5fa685dac804af52f76654b70f4
Brian Whitton <me@brianwhitton.com>
2017-09-14 22:45:05 -0400

    these are all provided by addons

--- app/bucket/model.js
+++
@@ -1,8 +0,0 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
-
-export default Model.extend({
-  slug: attr('string'),
-  title: attr('string'),
-  bucketItems: attr()
-});
