68b3da13962bc9dafee0cfeae2919c9635e8992a
Brian Whitton <me@brianwhitton.com>
2017-09-20 16:34:22 -0400

    finishe errored out proxy requests

--- server/proxies/media.js
+++ server/proxies/media.js
@@ -5,8 +5,10 @@ module.exports = function(app) {
   // For options, see:
   // https://github.com/nodejitsu/node-http-proxy
   var proxy = require('http-proxy').createProxyServer({});
-  proxy.on('error', function(err, req) {
+  proxy.on('error', function(err, req, res) {
     console.error(err, req.url);
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.end(err.Error);
   });
 
   app.use(proxyPath, function(req, res/*, next*/){
