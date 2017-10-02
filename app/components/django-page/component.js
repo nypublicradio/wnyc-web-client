4b39b4a4f7b842424431930dd201a0876c3c5452
Brian Whitton <me@brianwhitton.com>
2017-10-02 17:44:35 -0400

    don't need to import the layout

--- app/components/django-page/component.js
+++ app/components/django-page/component.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
 import Component from 'nypr-django-for-ember/components/django-page';
-import layout from 'nypr-django-for-ember/templates/components/django-page';
 import service from 'ember-service/inject';
 const { get } = Ember;
 
 export default Component.extend({
-  layout,
   session: service(),
   legacyAnalytics: service(),
   click(event) {
