3a970aa7fc6830bea705199813cdb0aa21e9a13c
Brian Whitton <me@brianwhitton.com>
2017-12-14 18:06:56 -0500

    adds failing acceptance test to assert stream order

--- tests/acceptance/streams-test.js
+++ tests/acceptance/streams-test.js
@@ -4,14 +4,24 @@ import moduleForAcceptance from 'wqxr-web-client/tests/helpers/module-for-accept
 moduleForAcceptance('Acceptance | streams');
 
 test('visiting /streams', function(assert) {
-  server.createList('stream', 7);
+  let streams = server.createList('stream', 7);
   server.createList('whats-on', 7);
   
+  let wqxrStreams = streams.filter(stream => stream.attrs.source_tags.includes('wqxr_site'));
+  let wnycStreams = streams.filter(stream => stream.attrs.source_tags.includes('wnyc_site'));
+  let allStreams = wqxrStreams.sort((a, b) => a.site_priority - b.site_priority)
+    .concat(wnycStreams.sort((a, b) => a.site_priority - b.site_priority)).uniq();
+
   visit('/streams');
 
   andThen(function() {
     assert.equal(currentURL(), '/streams');
-    assert.equal(find('.stream-list li').length, 6, 'should display a list of streams');
+    assert.equal(find('.stream-list li').length, 7, 'should display a list of streams');
+    assert.deepEqual(
+      allStreams.map(stream => stream.attrs.name),
+      find('.stream-list li .streamitem-header .streamitem-link').map((i, el) => el.textContent.trim()).get(),
+      'should order by site and priority'
+    );
   });
 });
 
