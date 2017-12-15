63c4e4edd18c171bb96d9ab15163bb4a874f4db9
Brian Whitton <me@brianwhitton.com>
2017-12-15 11:51:58 -0500

    force reload streams on the stream page

--- app/stream/route.js
+++ app/stream/route.js
@@ -7,7 +7,7 @@ export default Route.extend({
   titleToken: 'Listen Live to WNYC, WQXR, Q2, Operavore, NJPR, and the Jonathan Channel',
 
   model() {
-    return this.store.findAll('stream').then(streams => {
+    return this.store.findAll('stream', {reload: true}).then(streams => {
       return streams.filterBy('isWNYC').sortBy('sitePriority')
         .concat(streams.filterBy('isWQXR').sortBy('sitePriority')).uniq();
     });
