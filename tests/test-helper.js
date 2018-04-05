16f0618d576e5ce4db07d862cf80e30b7cbcdfd2
Michael Hearn <git@mikehearn.net>
2018-04-05 12:47:33 -0400

    Make ember tests parallel via ember exam

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,14 @@
+import resolver from './helpers/resolver';
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
+import { start, setResolver } from 'ember-qunit';
 import './helpers/flash-message';
+import loadEmberExam from 'ember-exam/test-support/load';
 
 setApplication(Application.create(config.APP));
 
+loadEmberExam();
+
+setResolver(resolver);
 start();
