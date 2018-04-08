63db14590353cbfdd1f90099fba0998d51e785e9
Brian Whitton <me@brianwhitton.com>
2018-04-08 16:58:49 -0400

    inflector deprecation

--- app/channel/route.js
+++ app/channel/route.js
@@ -2,14 +2,12 @@ import { hash as waitFor } from 'rsvp';
 import { set, get } from '@ember/object';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import Ember from 'ember';
-const {
-  Inflector
-} = Ember;
-const inflector = new Inflector(Inflector.defaultRules);
 import { beforeTeardown } from 'nypr-django-for-ember/utils/compat-hooks';
 import PlayParamMixin from 'wnyc-web-client/mixins/play-param';
 import config from 'wnyc-web-client/config/environment';
+import Inflector from 'ember-inflector';
+
+const inflector = new Inflector(Inflector.defaultRules);
 
 export default Route.extend(PlayParamMixin, {
   session:      service(),
