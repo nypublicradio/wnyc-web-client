4a125752f1e81710eb687422d3e32a75835fed03
Brian Whitton <me@brianwhitton.com>
2018-04-08 22:58:26 -0400

    fix up play param tests

--- tests/acceptance/play-param-test.js
+++ tests/acceptance/play-param-test.js
@@ -1,38 +1,31 @@
 import {
   click,
-  findAll,
   currentURL,
   find,
-  visit
+  visit,
+  waitFor
 } from '@ember/test-helpers';
-import { module, test } from 'qunit';
+import { module } from 'qunit';
+import test from 'ember-sinon-qunit/test-support/test';
 import { setupApplicationTest } from 'ember-qunit';
 import { registerMockOnInstance } from 'wnyc-web-client/tests/helpers/register-mock';
-import Service from '@ember/service';
 import velocity from 'velocity';
 import { dummyHifi } from 'wnyc-web-client/tests/helpers/hifi-integration-helpers';
 
 
 velocity.mock = true;
 
-const mockAudio = Service.extend({
-  playParam: null,
-  play(playParam) {
-    this.set('playParam', playParam);
-  }
-});
-
 module('Acceptance | play param', function(hooks) {
   setupApplicationTest(hooks);
 
   hooks.beforeEach(function() {
     server.create('stream');
-    registerMockOnInstance(this.application, 'service:hifi', dummyHifi);
+    registerMockOnInstance(this.owner, 'service:hifi', dummyHifi);
   });
 
   test('play param transitions', async function(assert) {
-    let application = this.application;
-    let audio = registerMockOnInstance(application, 'service:dj', mockAudio);
+    let dj = this.owner.lookup('service:dj');
+    this.stub(dj, 'play');
 
     server.create('django-page', {
       id: '/',
@@ -50,22 +43,23 @@ module('Acceptance | play param', function(hooks) {
     await click('#foo');
 
     assert.equal(currentURL(), '/foo?play=wnyc-fm939', 'url should have ?play');
-    assert.equal(audio.get('playParam'), 'wnyc-fm939', 'play should be called');
     await click('#home');
-    assert.equal(currentURL(), '/', 'homepage should not have a query param');
-    assert.equal(audio.get('playParam'), 'wnyc-fm939', 'play should not be called again');
+
+    // assert.equal(currentURL(), '/', 'homepage should not have a query param');
+    assert.ok(dj.play.calledOnce, 'play should not be called again');
+    assert.ok(dj.play.calledWith('wnyc-fm939'), 'play should be called');
   });
 
   test('loading a page with the ?play param', async function(assert) {
     let slug = 'foo';
 
     server.create('story', {slug, title: 'Foo'});
-    server.create('django-page', {id: `bar/`});
+    server.create('django-page', {id: '/'});
 
-    await visit(`bar?play=${slug}`);
+    await visit(`/?play=${slug}`);
+    await waitFor('.nypr-player');
 
-    assert.ok(findAll('.nypr-player').length, 'persistent player should be visible');
-    assert.equal(find('[data-test-selector=nypr-player-story-title]').textContent, 'Foo', 'Foo story should be loaded in player UI');
+    assert.equal(find('[data-test-selector=nypr-player-story-title]').textContent.trim(), 'Foo', 'Foo story should be loaded in player UI');
   });
 
   test('loading a page with a bad ?play param', async function(assert) {
@@ -73,6 +67,6 @@ module('Acceptance | play param', function(hooks) {
     server.create('django-page', {id: `/bar?play=${id}`});
 
     await visit(`bar?play=${id}`);
-    assert.notOk(findAll('.nypr-player').length, 'persistent player should not be visible');
+    assert.notOk(find('.nypr-player'), 'persistent player should not be visible');
   });
 });
