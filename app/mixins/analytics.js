3fb622a88f3833435187ab29b9eeb1c2799f3e99
Brian Whitton <me@brianwhitton.com>
2018-04-25 18:32:12 -0400

    get rid of metrics in this mixin

--- app/mixins/analytics.js
+++ app/mixins/analytics.js
@@ -1,12 +1,10 @@
 import Mixin from '@ember/object/mixin';
 import { scheduleOnce } from '@ember/runloop';
-import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 const DETAIL_ROUTES = new RegExp(/story|(show|article|series|tag|blog)-detail\./);
 
 export default Mixin.create({
-  metrics: service(),
   dataPipeline: service(),
 
   willTransition() {
@@ -29,19 +27,6 @@ export default Mixin.create({
 
   _trackPage() {
     scheduleOnce('afterRender', this, () => {
-      let page;
-      let is404 = this.currentRouteName === 'missing';
-      let title = document.title;
-      const metrics = get(this, 'metrics');
-      if (is404) {
-        page = this.get('dataPipeline.currentReferrer').replace(/http:\/\/[^/]+/, '');
-      } else {
-        page = document.location.pathname + document.location.search; // e.g. '/shows/bl/?q=foo'
-      }
-
-      metrics.trackPage('GoogleAnalytics', { page, title });
-      metrics.trackPage('GoogleTagManager', { page, title });
-
       if (!DETAIL_ROUTES.test(this.currentRouteName) && !this.currentRouteName.match(/loading/)) {
         this.get('dataPipeline').reportItemView();
       }
