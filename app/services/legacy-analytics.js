183a303d24316641580a0bb02fa01c0c60fd8520
Brian Whitton <me@brianwhitton.com>
2018-04-26 19:20:51 -0400

    all these are moved into GTM

--- app/services/legacy-analytics.js
+++ app/services/legacy-analytics.js
@@ -1,10 +1,7 @@
 import { get } from '@ember/object';
 import Service, { inject as service } from '@ember/service';
 
-const MENU_SELECTORS = '#navigation-menu';
-const HOMEPAGE_SELECTORS = '#wnyc_home a[href^=http], #wnyc_home a.external-link';
 const SHARE_SELECTORS = '.js-share';
-const HEADER_SELECTORS = '#brand-logo, .header-wide-button, #header .user-logout, #header .user-login';
 
 function contains(selector, target) {
   let results = target.closest(selector);
@@ -18,45 +15,9 @@ export default Service.extend({
   dispatch(e) {
     let target = e.target || e.currentTarget;
 
-    if (contains(MENU_SELECTORS, target)) {
-      this._trackLinkWithText(e, 'WNYC Menu');
-    } else if (contains(HEADER_SELECTORS, target)) {
-      this._trackLinkWithText(e, 'WNYC Header');
-    } else if (contains(HOMEPAGE_SELECTORS, target)) {
-      this._trackHomepage(e);
-    } else if (contains(SHARE_SELECTORS, target)) {
+    if (contains(SHARE_SELECTORS, target)) {
       this._trackShare(e);
     }
-  },
-
-  _trackLinkWithText({target}, category) {
-    target = document.querySelector(target);
-    const title = target.textContent.trim();
-    const destinationUrl = target.getAttribute('href') !== '#' ? target.getAttribute('href') : false;
-    const metrics = get(this, 'metrics');
-
-    metrics.trackEvent('GoogleAnalytics', {
-      category: category,
-      action: `Clicked "${title}"`,
-      label: `${destinationUrl || 'no URL'}`
-    });
-  },
-
-  _trackHomepage({target}) {
-    let li = target.closest('li');
-    let index = Array.from(li.parentElement.children).indexOf(li);
-    let bucket = target.closest('.bucket, #damost');
-    let position = bucket.getAttribute('data-position');
-    let title = bucket.querySelector('.bucket-title, #damost-nav .active');
-    title = title.textContent.trim();
-    let metrics = get(this, 'metrics');
-
-    metrics.trackEvent('GoogleAnalytics', {
-      category: 'Homepage Bucket',
-      action : `Clicked story in bucket ${position} with title "${title}"`,
-      label : `Headline clicked: "${target.textContent.trim()}", url: ${target.getAttribute('href')}`,
-      value : index + 1  // zero-indexing is for computers, 1-indexing is for people
-    });
   },
 
   _trackShare({target}) {
