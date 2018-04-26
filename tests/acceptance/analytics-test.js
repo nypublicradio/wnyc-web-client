91a3231ef616415cfebee7148f293ec49ac9a615
Brian Whitton <me@brianwhitton.com>
2018-04-26 19:21:08 -0400

    don't need to test this anymore

--- tests/acceptance/analytics-test.js
+++ tests/acceptance/analytics-test.js
@@ -1,8 +1,7 @@
-import { click, find, currentURL, visit } from '@ember/test-helpers';
+import { click, find, visit } from '@ember/test-helpers';
 import { module } from 'qunit';
 import test from 'ember-sinon-qunit/test-support/test';
 import { setupApplicationTest } from 'ember-qunit';
-import config from 'wnyc-web-client/config/environment';
 import velocity from 'velocity';
 
 module('Acceptance | Analytics', function(hooks) {
@@ -32,33 +31,6 @@ module('Acceptance | Analytics', function(hooks) {
 
     assert.notOk(find('.l-sliding-modal'), 'modal is closed');
     assert.equal(trackPage.callCount, 1, 'trackPageView was only called once after opening and closing queue');
-  });
-
-  test('it logs a homepage bucket event when you click a story on the home page', async function(assert) {
-    let metrics = this.owner.lookup('service:metrics');
-    let trackSpy = this.spy(metrics, 'trackEvent');
-
-    let story = server.create('story');
-    let id = `story/${story.slug}/`;
-    let testMarkup = `
-      <div id="wnyc_home">
-        <div class="top-stories bucket" data-position="top">
-          <h4 class="bucket-title">Top Stories</h4>
-          <ul id="home-primary">
-            <li class="first last">
-              <a href="${config.webRoot}/${id}" id="test-link">story link</a>
-            </li>
-          </ul>
-        </div>
-      </div>`;
-    server.create('django-page', {id});
-    server.create('django-page', {id: '/', testMarkup});
-
-    await visit('/');
-    await click('#test-link');
-
-    assert.equal(currentURL(), `/story/${story.slug}`, 'opened story page');
-    assert.equal(trackSpy.firstCall.args[1].category, 'Homepage Bucket', 'bucket event was triggered once after clicking link');
   });
 
   test('it registers the browser id with the dj service', async function() {
