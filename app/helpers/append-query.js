aa518261bcbd92b511cd81eca861cffe3a8c470e
Brian Whitton <me@brianwhitton.com>
2018-05-03 18:43:58 -0400

    add query string helper

---
+++ app/helpers/append-query.js
@@ -0,0 +1,34 @@
+import { helper } from '@ember/component/helper';
+
+export function appendQuery([url = '', queryToAdd = '']/*, hash*/) {
+  if (typeof url !== 'string' || typeof queryToAdd !== 'string') {
+    return url;
+  }
+
+  // string any beginning ? characters
+  queryToAdd = queryToAdd.replace(/^\?/, '');
+
+  if (/\?$/.test(url)) {
+    // incoming url ends with a ?, just jam on the query string
+    return `${url}${queryToAdd}`;
+  }
+
+  // does a query string exist?
+  let [address, foundQuery] = url.split('?');
+
+  if (/\?/.test(address) || /\?/.test(foundQuery)) {
+    // found a query string in the url or the query params
+    // weird case, just bail out
+    return url;
+  }
+
+  if (foundQuery) {
+    // there's already a query string on the url, add it to the params
+    return `${url}&${queryToAdd}`;
+  } else {
+    // no query string, make one
+    return `${url}?${queryToAdd}`;
+  }
+}
+
+export default helper(appendQuery);
