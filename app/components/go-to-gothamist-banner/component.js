b7a2e8f10f700ea01f333d422e481eca75f6defc
Michael Hearn <git@mikehearn.net>
2018-05-04 14:52:42 -0400

    Add gtm click tracking data attrs

--- app/components/go-to-gothamist-banner/component.js
+++ app/components/go-to-gothamist-banner/component.js
@@ -3,7 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: 'a',
   classNames: ['go-to-gothamist-banner'],
-  attributeBindings: ['href', 'target'],
+  attributeBindings: ['href', 'target', 'data-category', 'data-action', 'data-label'],
   href: 'http://gothamist.com?utm_medium=partnersite&utm_source=wnyc&utm_campaign=banner',
   target: '_blank'
 });
