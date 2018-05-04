69d3f8e7550bc99bb46bce29ed9d28b120b5b4f8
Michael Hearn <git@mikehearn.net>
2018-05-04 13:16:09 -0400

    Add utm codes to link, use ember-responsive 3.0

--- app/components/go-to-gothamist-banner/component.js
+++ app/components/go-to-gothamist-banner/component.js
@@ -4,6 +4,6 @@ export default Component.extend({
   tagName: 'a',
   classNames: ['go-to-gothamist-banner'],
   attributeBindings: ['href', 'target'],
-  href: 'http://gothamist.com/',
+  href: 'http://gothamist.com?utm_medium=partnersite&utm_source=wnyc&utm_campaign=banner',
   target: '_blank'
 });
