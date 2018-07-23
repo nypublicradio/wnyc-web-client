f71cf72b648dd211c43d92bb762f59f26e25e6a2
Brian Whitton <me@brianwhitton.com>
2018-07-23 18:20:39 -0400

    replace dupe label for value

--- app/components/discover-topic/component.js
+++ app/components/discover-topic/component.js
@@ -3,6 +3,6 @@ import Component from '@ember/component';
 export default Component.extend({
   classNameBindings:['isSelected'],
   classNames: ['discover-topic', 'discover-topic-bubble'],
-  attributeBindings: ['data-category', 'data-action', 'data-label', 'data-label'],
+  attributeBindings: ['data-category', 'data-action', 'data-label', 'data-value'],
   tagName: 'label'
 });
