6984f0ce9547bbcbe82acb1cc5c26b3bbbe1ab62
Brian Whitton <me@brianwhitton.com>
2018-08-30 15:07:04 -0400

    need to include timezones for the countdown widget

--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,9 @@ module.exports = function(environment) {
     'ember-cli-mirage': {
       autostart: true // https://github.com/samselikoff/ember-cli-mirage/blob/master/CHANGELOG.md#how-it-works-in-different-types-of-tests
     },
+    moment: {
+      includeTimezone: 'all',
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
