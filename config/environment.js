364a788134fe85db260f7cd12b780a95977cdd81
Brian Whitton <me@brianwhitton.com>
2019-02-14 13:43:28 -0500

    don't clear out hotjar iframe from an alien dom

--- config/environment.js
+++ config/environment.js
@@ -100,7 +100,7 @@ module.exports = function(environment) {
       }
     },
     alienDom: {
-      toRemove: `${ALIEN_DOM_ROOT} > :not(.ember-view):not(#fb-root), ${ALIEN_DOM_ROOT} > head > link[rel=stylesheet]:not([href*=assets])`
+      toRemove: `${ALIEN_DOM_ROOT} > :not(.ember-view):not(#fb-root):not(#_hjRemoteVarsFrame), ${ALIEN_DOM_ROOT} > head > link[rel=stylesheet]:not([href*=assets])`
     },
     hotjar: {
       id: 491820,
