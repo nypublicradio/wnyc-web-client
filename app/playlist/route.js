185f9e91a02826b9277480fb57b317185b7c60de
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-03-01 15:22:13 -0500

    remove playlist template, add redirect on route. fix tests.

--- app/playlist/route.js
+++ app/playlist/route.js
@@ -1,17 +1,13 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import { get } from '@ember/object';
+import moment from 'moment';
 
 export default Route.extend({
-  googleAds: service(),
-  
-  titleToken(model) {
-    return `Playlist for ${get(model, 'name')}`;
-  },
-  model({ slug }) {
-    return this.store.findRecord('stream', slug);
-  },
-  afterModel() {
-    get(this, 'googleAds').doTargeting();
+  redirect(model, { params }) {
+    let year  = moment().format('YYYY');
+    let month = moment().format('MMM').toLowerCase();
+    let day   = moment().format('DD');
+    this.transitionTo(`djangorendered`,`playlist-daily/${year}/${month}/${day}`, {
+      queryParams: { scheduleStation: params.playlist.slug }
+    });
   }
 });
