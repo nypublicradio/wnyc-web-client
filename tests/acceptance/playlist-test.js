4c8ea7b9eb95bdac1d589b77b21626fa757376dd
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-03-01 15:22:13 -0500

    remove playlist template, add redirect on route. fix tests.

--- tests/acceptance/playlist-test.js
+++ tests/acceptance/playlist-test.js
@@ -1,7 +1,8 @@
-import { currentURL, find, visit } from '@ember/test-helpers';
+import { currentURL, visit } from '@ember/test-helpers';
 import test from 'ember-sinon-qunit/test-support/test';
 import { setupApplicationTest } from 'ember-qunit';
 import { module } from 'qunit';
+import moment from 'moment';
 
 module('Acceptance | playlist', function(hooks) {
   setupApplicationTest(hooks);
@@ -20,36 +21,10 @@ module('Acceptance | playlist', function(hooks) {
         end: "2016-09-15T13:00:15.542Z" // 9 am
       }
     });
+    let date = moment().format('YYYY/MMM/DD').toLowerCase();
+    server.create('django-page', {id: `playlist-daily/${date}/?scheduleStation=wnyc-fm939`});
 
     await visit('/streams/wnyc-fm939');
-
-    assert.equal(currentURL(), '/streams/wnyc-fm939');
-    assert.equal(find('a[href="http://fooshow.com"]').textContent.trim(), 'Episode Foo');
-  });
-
-  test('playlist routes do dfp targeting', async function() /*assert*/{
-    // https://github.com/emberjs/ember.js/issues/14716#issuecomment-267976803
-    server.create('django-page', {id: 'foo/'});
-    await visit('/foo');
-
-    server.create('stream', {
-      slug: 'wnyc-fm939',
-      name: 'WNYC FM',
-    });
-    server.create('whats-on', {
-      slug: 'wnyc-fm939',
-      current_show: {
-        show_title: 'Foo Show',
-        title: 'Episode Foo',
-        url: 'http://fooshow.com',
-        end: "2016-09-15T13:00:15.542Z" // 9 am
-      }
-    });
-
-    this.mock(this.owner.lookup('route:playlist').get('googleAds'))
-      .expects('doTargeting')
-      .once();
-
-    await visit('/streams/wnyc-fm939');
+    assert.equal(currentURL(), `/playlist-daily/${date}`);
   });
 });
