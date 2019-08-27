b2e9d067dba1991f2b49029e15cfb9978a81084f
Brian Whitton <me@brianwhitton.com>
2019-08-27 18:11:20 -0400

    comment out gothamist row on homepage

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,8 @@ import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
 import PlayParamMixin from 'wnyc-web-client/mixins/play-param';
 import rsvp from 'rsvp';
-import config from 'wnyc-web-client/config/environment';
-import fetch from 'fetch';
+// import config from 'wnyc-web-client/config/environment';
+// import fetch from 'fetch';
 import { beforeTeardown } from 'nypr-django-for-ember/utils/compat-hooks';
 import DS from 'ember-data';
 const { hash } = rsvp;
@@ -20,11 +20,11 @@ export default Route.extend(PlayParamMixin, {
     let page = this.store.findRecord('django-page', '/');
     // let streams = this.store.findAll('stream', {reload: true});
     let featuredStream = this.store.findRecord('stream', 'wnyc-fm939');
-    let gothamist = fetch(config.gothamistStories)
-      .then(r => r.json()).then(({entries = []}) => entries.slice(0, 5))
-      .catch(() => []);
+    // let gothamist = fetch(config.gothamistStories)
+    //   .then(r => r.json()).then(({entries = []}) => entries.slice(0, 5))
+    //   .catch(() => []);
 
-    return hash({page, featuredStream, gothamist});
+    return hash({page, featuredStream/*, gothamist*/});
   },
   afterModel() {
     get(this, 'googleAds').doTargeting();
