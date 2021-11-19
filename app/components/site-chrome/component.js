b17f983746c9d7ccbf23724b4b52e984d1166516
Josh Gitlin <jgitlin@nypublicradio.org>
2021-11-19 16:43:40 -0500

    Remove tumblr links from wnyc left nav and bottom nav

--- app/components/site-chrome/component.js
+++ app/components/site-chrome/component.js
@@ -4,29 +4,28 @@ import config from 'wnyc-web-client/config/environment';
 import { or } from '@ember/object/computed';
 
 export default Component.extend({
-  session: service(),
-  router: service(),
-  currentUser: service(),
-  donateURL: config.wnycDonateURL,
-  classNameBindings: ['showPlayer:sitechrome--player-open'],
-  preferredStream: or('session.data.user-prefs-active-stream', 'defaultStream'),
+    session: service(),
+    router: service(),
+    currentUser: service(),
+    donateURL: config.wnycDonateURL,
+    classNameBindings: ['showPlayer:sitechrome--player-open'],
+    preferredStream: or('session.data.user-prefs-active-stream', 'defaultStream'),
 
-  init() {
-    this._super(...arguments);
-    this.setProperties({
-      defaultStream:  {slug: 'wnyc-fm939', name: 'WNYC 93.9 FM'},
-      socialIcons: [
-        {url: 'http://www.twitter.com/wnyc', icon: 'twitter'},
-        {url: 'http://www.facebook.com/wnyc', icon: 'facebook'},
-        {url: 'https://www.instagram.com/wnyc', icon: 'instagram'},
-        {url: 'http://wnyc.tumblr.com/', icon: 'tumblr'},
-      ]
-    });
-  },
-
-  actions: {
-    routeSearch(val) {
-      this.get('router').transitionTo('djangorendered', 'search/', {queryParams: {q: val}});
+    init() {
+        this._super(...arguments);
+        this.setProperties({
+            defaultStream: { slug: 'wnyc-fm939', name: 'WNYC 93.9 FM' },
+            socialIcons: [
+                { url: 'http://www.twitter.com/wnyc', icon: 'twitter' },
+                { url: 'http://www.facebook.com/wnyc', icon: 'facebook' },
+                { url: 'https://www.instagram.com/wnyc', icon: 'instagram' }
+            ]
+        });
     },
-  }
+
+    actions: {
+        routeSearch(val) {
+            this.get('router').transitionTo('djangorendered', 'search/', { queryParams: { q: val } });
+        },
+    }
 });
