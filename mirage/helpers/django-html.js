9226a3cc05ef004ed6a94f0b40ff7b0a4a44c4f7
walsh9 <walsh9@gmail.com>
2023-08-02 20:23:30 -0400

    update nypr-ads and google publisher tag script url

--- mirage/helpers/django-html.js
+++ mirage/helpers/django-html.js
@@ -1074,7 +1074,7 @@ var HTML = `<html>
         var gads = document.createElement('script');
         gads.async = true; gads.type = 'text/javascript';
         var useSSL = 'https:' == document.location.protocol;
-        gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
+        gads.src = (useSSL ? 'https:' : 'http:') + '//securepubads.g.doubleclick.net/tag/js/gpt.js';
         var node = document.getElementsByTagName('script')[0];
         node.parentNode.insertBefore(gads, node);
       })();
