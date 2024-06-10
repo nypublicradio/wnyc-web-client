9ba3b2dc16d3e764520bc983ab185ab9c82d3daa
walsh9 <walsh9@gmail.com>
2024-06-10 18:16:14 -0400

    implement initial splash page

--- app/components/centennial/centennial-header/component.js
+++ app/components/centennial/centennial-header/component.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  init() {
+    this._super(...arguments);
+    this.set('socialIcons', [
+          { url: 'https://www.instagram.com/wnyc', icon: 'instagram' },
+          { url: 'http://www.twitter.com/wnyc', icon: 'twitter' },
+          { url: 'https://www.youtube.com/channel/UCbysmY4hyViQAAYEzOR-uCQ', icon: 'youtube' },
+          { url: 'http://www.facebook.com/wnyc', icon: 'facebook' }
+        ]);
+},
 });
--- app/routes/timeline.js
+++ app/routes/timeline.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  actions: {
+    didTransition() {
+      window.scrollTo(0, 0);
+    }
+  }
 });
