a673e1ce365338e3bb643f0dda8552cc0e8bd374
walsh9 <walsh9@gmail.com>
2024-06-11 18:40:21 -0400

    update timeline embed

--- app/routes/timeline.js
+++ app/routes/timeline.js
@@ -1,6 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  init() {
+    this._super(...arguments);
+    this.set('cssPath', 'assets/css/timeline.css')
+  },
   actions: {
     didTransition() {
       window.scrollTo(0, 0);
