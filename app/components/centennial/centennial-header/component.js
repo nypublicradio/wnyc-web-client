c1b17229e18613cfe13f9c143e8ca5633596b788
walsh9 <walsh9@gmail.com>
2024-06-12 12:07:46 -0400

    add x icon

--- app/components/centennial/centennial-header/component.js
+++ app/components/centennial/centennial-header/component.js
@@ -5,7 +5,7 @@ export default Component.extend({
     this._super(...arguments);
     this.set('socialIcons', [
           { url: 'https://www.instagram.com/wnyc', icon: 'instagram' },
-          { url: 'http://www.twitter.com/wnyc', icon: 'twitter' },
+          { url: 'http://x.com/wnyc', icon: 'x' },
           { url: 'https://www.youtube.com/channel/UCbysmY4hyViQAAYEzOR-uCQ', icon: 'youtube' },
           { url: 'http://www.facebook.com/wnyc', icon: 'facebook' }
         ]);
