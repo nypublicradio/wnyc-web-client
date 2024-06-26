b9548a05d8daff8bfeb4f4adf9648c25e0a9820d
walsh9 <walsh9@gmail.com>
2024-06-26 12:38:15 -0400

    add events section

--- app/routes/centennial.js
+++ app/routes/centennial.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  model() {
+    let events = this.store.findRecord('bucket', 'wnyc-centennial-events')
+    .then(b => b.get('bucketItems'))
+    return RSVP.hash({
+      events
+    })
+  }
 });
