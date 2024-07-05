e458ef37174fb5fffc979f83522a9654b299102a
walsh9 <walsh9@gmail.com>
2024-07-05 17:58:24 -0400

    more card updates

--- app/components/centennial/centennial-share-button/component.js
+++ app/components/centennial/centennial-share-button/component.js
@@ -3,6 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
   init() {
     this._super(...arguments)
+    this.set('copyText', 'Copy Link')
     this.set('mailtoUrl', `mailto:?subject=${this.shareTitle}&body=${this.shareTitle}%0A%0A${this.shareUrl}`)
   },
   actions: {
@@ -15,7 +16,17 @@ export default Component.extend({
     updateClipboard() {
       navigator.permissions.query({name: "clipboard-write"}).then(result => {
         if (result.state == "granted" || result.state == "prompt") {
-          navigator.clipboard.writeText(this.shareUrl)
+          navigator.clipboard.writeText(this.shareUrl).then(() => {
+            this.set('copyText', 'Link Copied!')
+            setTimeout(() => {
+              this.set('copyText', 'Copy Link')
+            }, 2500)
+          }).catch(() => {
+            this.set('copyText', 'Error Copying Link')
+            setTimeout(() => {
+              this.set('copyText', 'Copy Link')
+            }, 2500)
+          })
         }
       })
     }
